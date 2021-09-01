dbcd7d20b8cc805013dd436336c13135d762df75
f1shl3gs <fishlegs.engerman@gmail.com>
2021-09-01 20:04:33 +0800

    init

--- src/sources/node/cpufreq.rs
+++ src/sources/node/cpufreq.rs
@@ -1,5 +1,49 @@
 use crate::event::Metric;
+use crate::sources::node::errors::Error;
 
-pub async fn gather() -> Result<Vec<Metric>, ()> {
+use std::io;
+
+pub async fn gather(proc_path: &str) -> Result<Vec<Metric>, ()> {
     todo!()
+}
+
+struct Stat {
+    name: String,
+
+}
+
+async fn get_cpu_freq_stat(sys_path: &str) -> Result<Vec<Stat>, Error> {
+    let cpus = glob::glob(&format!("{}/devices/system/cpu/cpu[0-9]*", sys_path))
+        .map_err(|err| {
+            let inner = io::Error::new(io::ErrorKind::InvalidInput, err);
+
+            Error::from(inner).with_message("No cpu files were found")
+        })?;
+
+    let stats = Vec::new();
+
+    for entry in cpus {
+        match entry {
+            Ok(path) => {
+                println!("find {:?}", path)
+            },
+
+            Err(err) => {
+                println!("err {}", err)
+            }
+        }
+    }
+
+    Ok(stats)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_cpu_freq_stat() {
+        let sys_path = "testdata/sys";
+        get_cpu_freq_stat(sys_path).await.unwrap();
+    }
 }
