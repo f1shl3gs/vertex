2901e7e60569c618dd184899e356f46a459fccd1
f1shl3gs <fishleg.engerman@gmail.com>
2021-10-09 01:33:13 +0800

    add bench

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 SRC=$(find ./src -name "*.rs" -print0 |xargs -0 cat| wc -l)
 LIB=$(find ./lib -name "*.rs" -print0 |xargs -0 cat| wc -l)
+BENCH=$(find ./benches -name "*.rs" -print0 | xargs -0 cat | wc -l)
 DOC=$(find ./ -name "*.md" -not -path "./target/*" -print0 | xargs -0 cat | wc -l)
 
 echo "doc:   ${DOC}"
 echo ""
 echo "src:   ${SRC}"
 echo "lib:   ${LIB}"
-echo "total: $(expr ${SRC} + ${LIB})"
+echo "bench: ${BENCH}"
+echo "total: $(expr ${SRC} + ${LIB} + ${BENCH})"
