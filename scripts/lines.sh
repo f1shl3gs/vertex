956c2ed886280ad6c32a4a793ec9bb18242deb3d
f1shl3gs <fishleg.engerman@gmail.com>
2021-10-09 01:26:10 +0800

    simplify and add doc

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -2,9 +2,12 @@
 
 set -euo pipefail
 
-SRC=$(find ./src -name "*.rs" |xargs cat|grep -vc ^$$)
-LIB=$(find ./lib -name "*.rs" |xargs cat|grep -vc ^$$)
+SRC=$(find ./src -name "*.rs" |xargs cat| wc -l)
+LIB=$(find ./lib -name "*.rs" |xargs cat| wc -l)
+DOC=$(find ./ -name "*.md" -not -path "./target/*" | xargs cat | wc -l)
 
+echo "doc:   ${DOC}"
+echo ""
 echo "src:   ${SRC}"
 echo "lib:   ${LIB}"
 echo "total: $(expr ${SRC} + ${LIB})"
