16f784191c02e50f38a6975da1b3425d7357e0fd
f1shl3gs <fishlegs.engerman@gmail.com>
2021-10-28 10:39:30 +0800

    count files under tests

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -6,10 +6,12 @@ SRC=$(find ./src -name "*.rs" -print0 |xargs -0 grep -v '^$' | wc -l)
 LIB=$(find ./lib -name "*.rs" -print0 |xargs -0 grep -v '^$' | wc -l)
 BENCH=$(find ./benches -name "*.rs" -print0 | xargs -0 grep -v '^$' | wc -l)
 DOC=$(find ./ -name "*.md" -not -path "./target/*" -print0 | xargs -0 grep -v '^$' | wc -l)
+TESTS=$(find ./tests -name "*.rs" -print0 | xargs -0 grep -v '^$' | wc -l)
 
 echo "doc:   ${DOC}"
 echo ""
 echo "src:   ${SRC}"
 echo "lib:   ${LIB}"
+echo "tests: ${TESTS}"
 echo "bench: ${BENCH}"
-echo "total: $(expr ${SRC} + ${LIB} + ${BENCH})"
+echo "total: $(expr ${SRC} + ${LIB} + ${BENCH} + ${TESTS})"
