1bc2eda4d05a39a0c07a51353085e40cff437402
f1shl3gs <fishlegs.engerman@gmail.com>
2021-10-29 17:08:52 +0800

    add bench duration_to_string

--- benches/duration.rs
+++ benches/duration.rs
@@ -1,12 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-
-use vertex::duration::parse_duration;
 use criterion::measurement::WallTime;
+
+use vertex::duration::{parse_duration, duration_to_string};
 
 pub fn parse_duration_benchmark(c: &mut Criterion) -> &mut Criterion<WallTime> {
     c.bench_function("parse_duration", |b| {
         b.iter(|| {
             parse_duration("3m20s").unwrap();
+        })
+    });
+
+    c.bench_function("duration_to_string", |b| {
+        let d = parse_duration("1h20m30s40ms").unwrap();
+
+        b.iter(|| {
+            duration_to_string(&d)
         })
     })
 }
