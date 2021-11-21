a9ebe106a4ae3aa4a9278d6cce1c9382bd59559e
f1shl3gs <fishleg.engerman@gmail.com>
2021-11-21 23:08:34 +0800

    update

--- tests/http_config.rs
+++ tests/http_config.rs
@@ -1,5 +1,40 @@
-[tokio::test]
+use http::{Request, Response, StatusCode};
+use hyper::{Body, Server};
+use hyper::service::{make_service_fn, service_fn};
+use tokio_util::codec::{BytesCodec, FramedRead};
+
+async fn send_file(_req: Request<Body>) -> hyper::Result<Response<Body>> {
+    // serve a file by asynchronously reading it by chunks using tokio-util crate
+
+    if let Ok(file) = tokio::fs::File::open("").await {
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let body = Body::wrap_stream(stream);
+        return Ok(Response::new(body))
+    }
+
+    Ok(Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(Body::empty())
+        .unwrap()
+    )
+}
+
+#[tokio::test]
 async fn http_config() {
     let addr = testify::next_addr();
+    let service = make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(send_file))
+    });
 
+    let server = Server::bind(&addr)
+        .serve(service);
+
+    println!("Listening on http://{}", addr);
+    tokio::spawn(async move {
+        if let Err(err) = server.await {
+            eprintln!("server error: {}", err);
+        }
+    });
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
