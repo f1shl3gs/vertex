f7d4fd936143138684b7c4f3771cc0a5bc6c9cfd
f1shl3gs <fishlegs.engerman@gmail.com>
2021-12-01 17:08:07 +0800

    fix prepare scripts

--- scripts/environment/bootstrap-ubuntu-20.04.sh
+++ scripts/environment/bootstrap-ubuntu-20.04.sh
@@ -17,24 +17,17 @@ apt upgrade --yes
 
 # Deps
 apt install --yes \
-    awscli \
     build-essential \
     ca-certificates \
     cmake \
     cmark-gfm \
     curl \
     gawk \
-    gnupg2 \
-    gnupg-agent \
-    gnuplot \
-    jq \
     libclang-dev \
     libsasl2-dev \
     libssl-dev \
     llvm \
     locales \
-    nodejs \
-    npm \
     pkg-config \
     python3-pip \
     rename \
@@ -42,29 +35,8 @@ apt install --yes \
     ruby-bundler \
     shellcheck \
     sudo \
-    wget \
-    yarn
+    wget
 
-# Cue
-TEMP=$(mktemp -d)
-curl \
-    -L https://github.com/cue-lang/cue/releases/download/v0.4.0/cue_v0.4.0_linux_amd64.tar.gz \
-    -o "${TEMP}/cue_v0.4.0_linux_amd64.tar.gz"
-tar \
-    -xvf "${TEMP}/cue_v0.4.0_linux_amd64.tar.gz" \
-    -C "${TEMP}"
-cp "${TEMP}/cue" /usr/bin/cue
-
-# Grease
-# Grease is used for the `make release-github` task.
-TEMP=$(mktemp -d)
-curl \
-    -L https://github.com/timberio/grease/releases/download/v1.0.1/grease-1.0.1-linux-amd64.tar.gz \
-    -o "${TEMP}/grease-1.0.1-linux-amd64.tar.gz"
-tar \
-    -xvf "${TEMP}/grease-1.0.1-linux-amd64.tar.gz" \
-    -C "${TEMP}"
-cp "${TEMP}/grease/bin/grease" /usr/bin/grease
 
 # Locales
 locale-gen en_US.UTF-8
@@ -82,21 +54,6 @@ if [ -n "${CI-}" ] ; then
     echo "${HOME}/.cargo/bin" >> "${GITHUB_PATH}"
 else
     echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> "${HOME}/.bash_profile"
-fi
-
-# Docker.
-if ! [ -x "$(command -v docker)" ]; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository \
-        "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu \
-        xenial \
-        stable"
-    # Install those new things
-    apt update --yes
-    apt install --yes docker-ce docker-ce-cli containerd.io
-
-    # ubuntu user doesn't exist in scripts/environment/Dockerfile which runs this
-    usermod --append --groups docker ubuntu || true
 fi
 
 # Apt cleanup
