684326acaf06dd2133a0d60ba9f2101df4d2fd75
github-action-benchmark <github@users.noreply.github.com>
2021-12-15 09:39:23 +0000

    add humanize (customSmallerIsBetter) benchmark result for edd8ca6ac9a1f0f7fc23a8abee710af289165e05

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639560586238,
+  "lastUpdate": 1639561163164,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -24,6 +24,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1001100,
+            "unit": "ns/op"
+          }
+        ]
+      }
+    ],
+    "humanize": [
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "edd8ca6ac9a1f0f7fc23a8abee710af289165e05",
+          "message": "Bench workflow",
+          "timestamp": "2021-12-15T08:25:33Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/52/commits/edd8ca6ac9a1f0f7fc23a8abee710af289165e05"
+        },
+        "date": 1639561162448,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 42.725,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 68.93,
             "unit": "ns/op"
           }
         ]
