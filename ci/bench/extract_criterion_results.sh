baa1a395d2c1893ef6820c301c799b620fab048e
f1shl3gs <fishlegs.engerman@gmail.com>
2021-12-15 16:24:04 +0800

    fix shell_check

--- ci/bench/extract_criterion_results.sh
+++ ci/bench/extract_criterion_results.sh
@@ -5,28 +5,26 @@ set -euo pipefail
 OUTPUT=bench.json
 CONTENT="["
 
-IFS=$'\n\n';
-for LINE in $(grep -v ignore bench.txt | grep time: | grep -v %); do
+while IFS= read -r LINE; do
   [[ $LINE =~ ([a-zA-Z0-9/_]*).*\[[0-9.]*\ (us|ns|ms|s)\ ([0-9.]*)\ (us|ns|ms|s) ]]
-  BENCH=${BASH_REMATCH[1]}
-  VALUE=${BASH_REMATCH[3]}
-  UNIT=${BASH_REMATCH[4]}
+    BENCH=${BASH_REMATCH[1]}
+    VALUE=${BASH_REMATCH[3]}
+    UNIT=${BASH_REMATCH[4]}
 
-  # convert to ns
-  case $UNIT in
-  s)
-    VALUE=$(echo "${VALUE} * 1000.0 * 1000.0 * 1000.0" | bc -l)
-    ;;
-  ms)
-    VALUE=$(echo "${VALUE} * 1000.0 * 1000.0" | bc -l)
-    ;;
-  us)
-    VALUE=$(echo "${VALUE} * 1000.0" | bc -l)
-    ;;
-  esac
+    # convert to ns
+    case $UNIT in
+    s)
+      VALUE=$(echo "${VALUE} * 1000.0 * 1000.0 * 1000.0" | bc -l)
+      ;;
+    ms)
+      VALUE=$(echo "${VALUE} * 1000.0 * 1000.0" | bc -l)
+      ;;
+    us)
+      VALUE=$(echo "${VALUE} * 1000.0" | bc -l)
+      ;;
+    esac
 
-  CONTENT="${CONTENT}{\"name\": \"${BENCH}\",\"unit\":\"ns/op\",\"value\": ${VALUE}},"
-  # echo "${BENCH}: ${VALUE} ns/op"
-done
+    CONTENT="${CONTENT}{\"name\": \"${BENCH}\",\"unit\":\"ns/op\",\"value\": ${VALUE}},"
+done < <(grep -v ignore bench.txt | grep time: | grep -v %)
 
 echo "${CONTENT%,} ]" > ${OUTPUT}
