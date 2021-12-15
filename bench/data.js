293a7357ee0ce506ee6748e711665377561a57c5
github-action-benchmark <github@users.noreply.github.com>
2021-12-15 09:29:46 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 1409f6b480ccf124a155b1e63efd53e975d8c158

---
+++ bench/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1639560586238,
+  "repoUrl": "https://github.com/f1shl3gs/vertex",
+  "entries": {
+    "prometheus": [
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "1409f6b480ccf124a155b1e63efd53e975d8c158",
+          "message": "Bench workflow",
+          "timestamp": "2021-12-15T08:25:33Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/52/commits/1409f6b480ccf124a155b1e63efd53e975d8c158"
+        },
+        "date": 1639560585342,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1001100,
+            "unit": "ns/op"
+          }
+        ]
+      }
+    ]
+  }
+}
