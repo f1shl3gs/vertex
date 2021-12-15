4a038b3f0d9beed48e3d2171aab0671cdfbc30a6
github-action-benchmark <github@users.noreply.github.com>
2021-12-15 09:00:37 +0000

    add bench (customSmallerIsBetter) benchmark result for 9578a816d272dd7ba1e634b8b57754464df199b9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639557585365,
+  "lastUpdate": 1639558837741,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "bench-prometheus": [
@@ -28,6 +28,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/1",
             "value": 964930,
+            "unit": "ns/op"
+          }
+        ]
+      }
+    ],
+    "bench": [
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "distinct": true,
+          "id": "9578a816d272dd7ba1e634b8b57754464df199b9",
+          "message": "add paths filter and fix tool name",
+          "timestamp": "2021-12-15T16:32:34+08:00",
+          "tree_id": "200115fdce804167796040b541e4b008cd79f604",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9578a816d272dd7ba1e634b8b57754464df199b9"
+        },
+        "date": 1639558836654,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 69127000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1417900,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 29.025,
             "unit": "ns/op"
           }
         ]
