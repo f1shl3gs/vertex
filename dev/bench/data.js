7e978e8f5b7360828dd18960a45749df19a99398
github-action-benchmark <github@users.noreply.github.com>
2021-12-15 08:39:45 +0000

    add bench-prometheus (customSmallerIsBetter) benchmark result for 9578a816d272dd7ba1e634b8b57754464df199b9

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1639557585365,
+  "repoUrl": "https://github.com/f1shl3gs/vertex",
+  "entries": {
+    "bench-prometheus": [
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "distinct": true,
+          "id": "9578a816d272dd7ba1e634b8b57754464df199b9",
+          "message": "add paths filter and fix tool name",
+          "timestamp": "2021-12-15T16:32:34+08:00",
+          "tree_id": "200115fdce804167796040b541e4b008cd79f604",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9578a816d272dd7ba1e634b8b57754464df199b9"
+        },
+        "date": 1639557584112,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/1",
+            "value": 964930,
+            "unit": "ns/op"
+          }
+        ]
+      }
+    ]
+  }
+}
