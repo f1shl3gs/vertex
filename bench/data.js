1e5b0b18d2e78c74c31cd3dec00f7219f55ec684
github-action-benchmark <github@users.noreply.github.com>
2021-12-16 04:45:13 +0000

    add humanize (customSmallerIsBetter) benchmark result for 353bcc0239fdc0a353f53e63e204b1c79a4374fd

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639629770601,
+  "lastUpdate": 1639629913373,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -136,6 +136,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 65.799,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "353bcc0239fdc0a353f53e63e204b1c79a4374fd",
+          "message": "chore(deps): bump tokio from 1.14.0 to 1.15.0",
+          "timestamp": "2021-12-15T10:06:53Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/56/commits/353bcc0239fdc0a353f53e63e204b1c79a4374fd"
+        },
+        "date": 1639629912360,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 48.59,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 78.597,
             "unit": "ns/op"
           }
         ]
