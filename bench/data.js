5b32e5665293440f2cf5c837a4264e4781a36ecc
github-action-benchmark <github@users.noreply.github.com>
2021-12-20 05:05:00 +0000

    add vertex (customSmallerIsBetter) benchmark result for 2a1327022f0988fd1f041445ba31e9ba0004fa16

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639976091227,
+  "lastUpdate": 1639976700439,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -1980,6 +1980,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 26.642,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "2a1327022f0988fd1f041445ba31e9ba0004fa16",
+          "message": "chore(deps): bump futures from 0.3.18 to 0.3.19",
+          "timestamp": "2021-12-19T13:46:32Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/70/commits/2a1327022f0988fd1f041445ba31e9ba0004fa16"
+        },
+        "date": 1639976699300,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 81971000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1577200,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 30.079,
             "unit": "ns/op"
           }
         ]
