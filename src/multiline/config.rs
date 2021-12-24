68513f2fdf84382864fb22e42fa2a86ca3324910
f1shl3gs <fishlegs.engerman@gmail.com>
2021-12-24 16:58:27 +0800

    add some example

--- src/multiline/config.rs
+++ src/multiline/config.rs
@@ -1,7 +1,9 @@
+use std::fmt::{Formatter, Write};
 use std::time::Duration;
 
 use regex::bytes::Regex;
-use serde::{Deserialize, Serialize};
+use serde::de::{Error, MapAccess};
+use serde::{de, Deserialize, Deserializer, Serialize};
 use snafu::{ResultExt, Snafu};
 
 use super::aggregate::{self, Mode};
@@ -64,3 +66,76 @@ impl TryFrom<&MultilineConfig> for aggregate::Config {
     }
 }
 */
+
+#[derive(Debug)]
+struct DockerRule {}
+
+
+///
+/// With type
+/// ```yaml
+/// multiline:
+///     type: java
+///     timeout: 5s # this should be default
+/// ```
+///
+/// Without type
+/// ```yaml
+/// multiline:
+///     type: custom
+///     timeout: 5s
+///     start_pattern:  ^[^\s]
+///     condition_pattern: ^[\s]+
+///     mode: continue_through
+/// ```
+#[derive(Debug)]
+pub enum MultilineConfig {
+    Cri,
+    Docker,
+    Go,
+    Java,
+
+    Custom,
+}
+
+impl<'de> serde::de::Deserialize<'de> for MultilineConfig {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct StringOrMap;
+
+        impl<'de> serde::de::Visitor<'de> for StringOrMap {
+            type Value = MultilineConfig;
+
+            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                formatter.write_str("string or map")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                match v {
+                    "docker" => Ok(MultilineConfig::Docker),
+                    "cri" => Ok(MultilineConfig::Cri),
+                    "go" => Ok(MultilineConfig::Go),
+                    "java" => Ok(MultilineConfig::Java),
+                    _ => Err(de::Error::invalid_value(
+                        de::Unexpected::Str(v),
+                        &r#"cri, docker, go or java"#,
+                    )),
+                }
+            }
+
+            fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
+            where
+                A: MapAccess<'de>,
+            {
+                todo!()
+            }
+        }
+
+        deserializer.deserialize_any(StringOrMap)
+    }
+}
