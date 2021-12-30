7e04f64c75f388d1136892818a3a61907fc7317a
f1shl3gs <fishlegs.engerman@gmail.com>
2021-12-30 19:05:09 +0800

    init

--- src/sources/libvirt.rs
+++ src/sources/libvirt.rs
@@ -1,1 +1,31 @@
+use crate::config::{DataType, GenerateConfig, SourceConfig, SourceContext};
+use crate::sources::Source;
 
+#[derive(Debug, Deserialize, Serialize)]
+struct LibvirtSourceConfig {}
+
+impl GenerateConfig for LibvirtSourceConfig {
+    fn generate_config() -> serde_yaml::Value {
+        todo!()
+    }
+}
+
+inventory::submit! {
+    SourceDescription::new::<LibvirtSourceConfig>("libvirt")
+}
+
+#[async_trait::async_trait]
+#[typetag::serde(name = "libvirt")]
+impl SourceConfig for LibvirtSourceConfig {
+    async fn build(&self, ctx: SourceContext) -> crate::Result<Source> {
+        todo!()
+    }
+
+    fn output_type(&self) -> DataType {
+        DataType::Metric
+    }
+
+    fn source_type(&self) -> &'static str {
+        "libvirt"
+    }
+}
