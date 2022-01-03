69ba5f79dc5470954d36e4a392a76aaa86c8f672
github-action-benchmark <github@users.noreply.github.com>
2022-01-03 04:35:26 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 9236391f8c9197ec9be9a3f4f9e2db02fa0f2175

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641184520861,
+  "lastUpdate": 1641184526808,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -949,6 +949,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1129200,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "9236391f8c9197ec9be9a3f4f9e2db02fa0f2175",
+          "message": "chore(deps): bump serde from 1.0.132 to 1.0.133",
+          "timestamp": "2022-01-01T10:41:13Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/87/commits/9236391f8c9197ec9be9a3f4f9e2db02fa0f2175"
+        },
+        "date": 1641184526219,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1323800,
             "unit": "ns/op"
           }
         ]
