075b01721283523f1add25219d79c68f352c4f95
github-action-benchmark <github@users.noreply.github.com>
2022-01-05 10:51:42 +0000

    add prometheus (customSmallerIsBetter) benchmark result for abcde6fe57d8c53f0b16a8e9413b119cc30f6444

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641371813517,
+  "lastUpdate": 1641379902149,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -1149,6 +1149,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1133300,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "id": "abcde6fe57d8c53f0b16a8e9413b119cc30f6444",
+          "message": "Libvirt",
+          "timestamp": "2022-01-04T07:50:55Z",
+          "url": "https://github.com/f1shl3gs/vertex/pull/92/commits/abcde6fe57d8c53f0b16a8e9413b119cc30f6444"
+        },
+        "date": 1641379900508,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1139500,
             "unit": "ns/op"
           }
         ]
