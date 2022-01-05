c9d9823bce4acf68a477b599803591e93f950b5e
f1shl3gs <fishlegs.engerman@gmail.com>
2022-01-05 17:19:12 +0800

    fix clippy warings

--- src/multiline/preset/java.rs
+++ src/multiline/preset/java.rs
@@ -16,13 +16,13 @@ mod tests {
         ];
 
         let want = [
-            "Exception in thread \"main\" java.lang.IllegalStateException: ..null property\n"
-                + "     at com.example.myproject.Author.getBookIds(xx.java:38)\n"
-                + "     at com.example.myproject.Bootstrap.main(Bootstrap.java:14)\n"
-                + "Caused by: java.lang.NullPointerException\n"
-                + "     at com.example.myproject.Book.getId(Book.java:22)\n"
-                + "     at com.example.myproject.Author.getBookIds(Author.java:35)\n"
-                + "     ... 1 more\n",
+            "Exception in thread \"main\" java.lang.IllegalStateException: ..null property\n\
+     at com.example.myproject.Author.getBookIds(xx.java:38)\n\
+     at com.example.myproject.Bootstrap.main(Bootstrap.java:14)\n\
+Caused by: java.lang.NullPointerException\n\
+     at com.example.myproject.Book.getId(Book.java:22)\n\
+     at com.example.myproject.Author.getBookIds(Author.java:35)\n\
+     ... 1 more\n",
             "single line\n",
         ];
     }
