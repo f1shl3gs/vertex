23501cb9867f8194c9e302d6a98b5f8504a9fa78
github-action-benchmark <github@users.noreply.github.com>
2022-01-19 08:18:29 +0000

    add humanize (customSmallerIsBetter) benchmark result for ff8acad1ebc969951ca32d1d1175bdfe94cf3858

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642511616438,
+  "lastUpdate": 1642580309664,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -3490,6 +3490,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 55.754,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff8acad1ebc969951ca32d1d1175bdfe94cf3858",
+          "message": "Merge pull request #133 from f1shl3gs/dependabot/cargo/console-subscriber-0.1.1\n\nchore(deps): bump console-subscriber from 0.1.0 to 0.1.1",
+          "timestamp": "2022-01-19T16:09:55+08:00",
+          "tree_id": "17a85144d3723df17fc5db67a6ea4d33278bf3af",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ff8acad1ebc969951ca32d1d1175bdfe94cf3858"
+        },
+        "date": 1642580308923,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 20.419,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 51.608,
             "unit": "ns/op"
           }
         ]
