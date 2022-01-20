36e201fc2d7ce1ca12cddd139e83174128bd8102
github-action-benchmark <github@users.noreply.github.com>
2022-01-20 08:56:17 +0000

    add humanize (customSmallerIsBetter) benchmark result for 9c6e6cc8cde5ec25b9480764544a9798a55840b3

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642595306369,
+  "lastUpdate": 1642668977468,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -3616,6 +3616,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 69.403,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c6e6cc8cde5ec25b9480764544a9798a55840b3",
+          "message": "Merge pull request #135 from f1shl3gs/more_build_info\n\nadd more build info to 'version' output",
+          "timestamp": "2022-01-20T16:50:35+08:00",
+          "tree_id": "eab2d910a580d90c352f0778f0ec50de181aaeb7",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9c6e6cc8cde5ec25b9480764544a9798a55840b3"
+        },
+        "date": 1642668976552,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 22.277,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 62.148,
             "unit": "ns/op"
           }
         ]
