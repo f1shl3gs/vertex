93b453d792dbbcef2c3196345e2643a8b3a150bb
github-action-benchmark <github@users.noreply.github.com>
2022-01-24 14:23:34 +0000

    add Build (customSmallerIsBetter) benchmark result for 2e88e3f9653f23786bae680701278863a95e5f47

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643033396310,
+  "lastUpdate": 1643034214417,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -11065,6 +11065,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1065,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e88e3f9653f23786bae680701278863a95e5f47",
+          "message": "Merge pull request #146 from f1shl3gs/prometheus_remote_write_source\n\nAdd prometheus remote write source",
+          "timestamp": "2022-01-24T22:02:51+08:00",
+          "tree_id": "690b3d1d00deca1458c74016eb4b1d020249a681",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2e88e3f9653f23786bae680701278863a95e5f47"
+        },
+        "date": 1643034213260,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1054,
             "unit": "s"
           }
         ]
