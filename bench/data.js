6014c7f19bbe920a182bd6497ff34caa9e6627f4
github-action-benchmark <github@users.noreply.github.com>
2022-01-26 09:45:00 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 0c92b4b77908163d59355974092615480ef7f70e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643190255434,
+  "lastUpdate": 1643190300161,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2050,6 +2050,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1120800,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c92b4b77908163d59355974092615480ef7f70e",
+          "message": "Merge pull request #154 from f1shl3gs/ensure_fanout_flushed_on_idle\n\nensure fanout is flushed on idle",
+          "timestamp": "2022-01-26T17:37:22+08:00",
+          "tree_id": "b51c97806df426e61cd04bfe47f731cbcc7e6d32",
+          "url": "https://github.com/f1shl3gs/vertex/commit/0c92b4b77908163d59355974092615480ef7f70e"
+        },
+        "date": 1643190299044,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1127200,
             "unit": "ns/op"
           }
         ]
