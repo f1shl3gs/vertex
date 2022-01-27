f3df8c698d24f5a0e0b04ff8049191b5096c8f6a
github-action-benchmark <github@users.noreply.github.com>
2022-01-27 19:40:01 +0000

    add humanize (customSmallerIsBetter) benchmark result for 15bb40b9434ad8a75bd554cb7cbd9be4681a5ef0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643310587911,
+  "lastUpdate": 1643312401005,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -4687,6 +4687,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 58.01,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "15bb40b9434ad8a75bd554cb7cbd9be4681a5ef0",
+          "message": "Merge pull request #157 from f1shl3gs/switch_to_tokio_channel_for_pipeline\n\nSwitch to tokio channel for pipeline",
+          "timestamp": "2022-01-28T03:34:43+08:00",
+          "tree_id": "eb7bd1cbaaa3c0da7d35d4cdb3877783146440b0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/15bb40b9434ad8a75bd554cb7cbd9be4681a5ef0"
+        },
+        "date": 1643312400187,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 21.206,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 57.575,
             "unit": "ns/op"
           }
         ]
