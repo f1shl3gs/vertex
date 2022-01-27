3b5ea139f1adee1295a1eb5790e1d142fb5a925a
github-action-benchmark <github@users.noreply.github.com>
2022-01-27 17:47:40 +0000

    add Build (customSmallerIsBetter) benchmark result for bfc557664387bdd107f9450b7789161d5ee3d796

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643304834420,
+  "lastUpdate": 1643305660062,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -11965,6 +11965,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1110,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bfc557664387bdd107f9450b7789161d5ee3d796",
+          "message": "Merge pull request #155 from f1shl3gs/dependabot/cargo/rkyv-0.7.31\n\nchore(deps): bump rkyv from 0.7.30 to 0.7.31",
+          "timestamp": "2022-01-28T01:27:27+08:00",
+          "tree_id": "d92854964024bb76411da163bce358c8f459f600",
+          "url": "https://github.com/f1shl3gs/vertex/commit/bfc557664387bdd107f9450b7789161d5ee3d796"
+        },
+        "date": 1643305658838,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 939,
             "unit": "s"
           }
         ]
