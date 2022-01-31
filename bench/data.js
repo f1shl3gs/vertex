9c6dc55eb80666bfd90f10fe6ce4f7d5e9950d0f
github-action-benchmark <github@users.noreply.github.com>
2022-01-31 19:03:15 +0000

    add humanize (customSmallerIsBetter) benchmark result for fbbe5c31976fe52232170f9250542f2c01a5d074

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643655780027,
+  "lastUpdate": 1643655795929,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5157,6 +5157,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 68.943,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fbbe5c31976fe52232170f9250542f2c01a5d074",
+          "message": "Merge pull request #162 from f1shl3gs/dependabot/cargo/libc-0.2.116\n\nchore(deps): bump libc from 0.2.115 to 0.2.116",
+          "timestamp": "2022-02-01T02:56:22+08:00",
+          "tree_id": "afd07cfecd33be5cc6fd9f3f5569b1afef57e065",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fbbe5c31976fe52232170f9250542f2c01a5d074"
+        },
+        "date": 1643655794807,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 28.119,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 72.78,
             "unit": "ns/op"
           }
         ]
