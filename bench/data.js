04bc4b13afa2ef68bceba3866e849421445ed459
github-action-benchmark <github@users.noreply.github.com>
2022-02-07 06:13:13 +0000

    add humanize (customSmallerIsBetter) benchmark result for e03069fbb764ce9d8e3734e18bbe65b7dc343862

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644214390004,
+  "lastUpdate": 1644214393426,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5792,6 +5792,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 71.902,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e03069fbb764ce9d8e3734e18bbe65b7dc343862",
+          "message": "Merge pull request #175 from f1shl3gs/dependabot/cargo/dashmap-5.1.0\n\nchore(deps): bump dashmap from 5.0.0 to 5.1.0",
+          "timestamp": "2022-02-07T14:07:28+08:00",
+          "tree_id": "6b22e749c1719cbedba04b43904b1d4c6eb009aa",
+          "url": "https://github.com/f1shl3gs/vertex/commit/e03069fbb764ce9d8e3734e18bbe65b7dc343862"
+        },
+        "date": 1644214392692,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 23.626,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 63.15,
             "unit": "ns/op"
           }
         ]
