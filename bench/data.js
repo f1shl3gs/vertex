0acae5e76f037d632acf1b9db3f1dc74a54f9da2
github-action-benchmark <github@users.noreply.github.com>
2022-02-09 15:34:56 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 1931ef3c5cf1ea330627c91d15bbd4a5fe874d78

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644420876594,
+  "lastUpdate": 1644420896003,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2804,6 +2804,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1145500,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1931ef3c5cf1ea330627c91d15bbd4a5fe874d78",
+          "message": "Merge pull request #182 from f1shl3gs/kafka_source_integration_test\n\nKafka source integration test",
+          "timestamp": "2022-02-09T23:27:15+08:00",
+          "tree_id": "8ed89daf4e44736aba63fc499534216015f8275e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1931ef3c5cf1ea330627c91d15bbd4a5fe874d78"
+        },
+        "date": 1644420894352,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1464500,
             "unit": "ns/op"
           }
         ]
