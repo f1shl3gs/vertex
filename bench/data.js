5afbce7c994a35b131d503c98e0358016168c185
github-action-benchmark <github@users.noreply.github.com>
2022-02-09 17:25:32 +0000

    add Build (customSmallerIsBetter) benchmark result for 0c4d4669262f9a29d6ca8a86305cea637e2d50cb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644426768468,
+  "lastUpdate": 1644427532145,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -15893,6 +15893,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1067,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c4d4669262f9a29d6ca8a86305cea637e2d50cb",
+          "message": "Merge pull request #183 from f1shl3gs/implement_histogram_for_aggregate_transform\n\nImplement histogram for aggregate transform",
+          "timestamp": "2022-02-10T01:05:14+08:00",
+          "tree_id": "70ea4bf63f6df5040b92c9fbf95c3763f0f4765e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/0c4d4669262f9a29d6ca8a86305cea637e2d50cb"
+        },
+        "date": 1644427530843,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 980,
             "unit": "s"
           }
         ]
