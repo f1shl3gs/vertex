abed5703d68f38b29121ae699c4463830d1ba520
github-action-benchmark <github@users.noreply.github.com>
2022-02-22 13:17:28 +0000

    add humanize (customSmallerIsBetter) benchmark result for 99b996ee0e9338d0b088cc6915285ed4290f8c8e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645499726411,
+  "lastUpdate": 1645535848664,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7207,6 +7207,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 57.337,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "distinct": true,
+          "id": "99b996ee0e9338d0b088cc6915285ed4290f8c8e",
+          "message": "new decoding config & impl exec source",
+          "timestamp": "2022-02-22T21:10:04+08:00",
+          "tree_id": "07c6b3a98ca26d628f4a1db649a879cbeb7adec4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/99b996ee0e9338d0b088cc6915285ed4290f8c8e"
+        },
+        "date": 1645535847799,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 24.954,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 74.118,
             "unit": "ns/op"
           }
         ]
