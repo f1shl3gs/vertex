2b10c155166f34f3c95d48a37b37159a80d86262
github-action-benchmark <github@users.noreply.github.com>
2022-02-23 03:01:44 +0000

    add Build (customSmallerIsBetter) benchmark result for 1e42e7ddaf5bc6761bdc5df06adc49f392d34454

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645585296564,
+  "lastUpdate": 1645585304572,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -19452,6 +19452,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1284,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e42e7ddaf5bc6761bdc5df06adc49f392d34454",
+          "message": "Merge pull request #205 from f1shl3gs/dependabot/cargo/mimalloc-0.1.28\n\nchore(deps): bump mimalloc from 0.1.27 to 0.1.28",
+          "timestamp": "2022-02-23T10:36:00+08:00",
+          "tree_id": "98de4ffad1f6529d80bb43a173d5ef4ab4402584",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1e42e7ddaf5bc6761bdc5df06adc49f392d34454"
+        },
+        "date": 1645585302694,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1281,
             "unit": "s"
           }
         ]
