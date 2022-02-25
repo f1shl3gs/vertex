ce1bd2b9aa7517a9cceddee3ec83ff1b0da2eab6
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 09:09:04 +0000

    add Build (customSmallerIsBetter) benchmark result for 5c2d2adbf993d8f8fd5ef34bd74fb643be95de4b

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645779089603,
+  "lastUpdate": 1645780143984,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -19949,6 +19949,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1235,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5c2d2adbf993d8f8fd5ef34bd74fb643be95de4b",
+          "message": "Merge pull request #214 from f1shl3gs/move_encoding_to_framework\n\nmove encoding to framework",
+          "timestamp": "2022-02-25T16:44:01+08:00",
+          "tree_id": "e89bdaa071f0503f4eaa522ffe967e9c1b3a2152",
+          "url": "https://github.com/f1shl3gs/vertex/commit/5c2d2adbf993d8f8fd5ef34bd74fb643be95de4b"
+        },
+        "date": 1645780142371,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1246,
             "unit": "s"
           }
         ]
