36bba418dab52e693b3e63f301905fc744a97864
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 08:34:34 +0000

    add Build (customSmallerIsBetter) benchmark result for 2bfbb7281c2c47dbfaf94789f05344304a557d91

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645777065036,
+  "lastUpdate": 1645778074484,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -19793,6 +19793,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1079,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2bfbb7281c2c47dbfaf94789f05344304a557d91",
+          "message": "Merge pull request #212 from f1shl3gs/bump_rsntp\n\nbump rsntp from 2.1.0 to 3.0.0",
+          "timestamp": "2022-02-25T16:09:37+08:00",
+          "tree_id": "d2df245640b1a7da7ba9d0ad2c71594c2089ebcf",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2bfbb7281c2c47dbfaf94789f05344304a557d91"
+        },
+        "date": 1645778072946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1235,
             "unit": "s"
           }
         ]
