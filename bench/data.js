cd463a1e53202a613286c0d61245fb5e9f408b72
github-action-benchmark <github@users.noreply.github.com>
2022-03-22 04:31:12 +0000

    add Build (customSmallerIsBetter) benchmark result for 8e358da4e9ddb9d7e89ecba29817cda4d4c98ef7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647923449936,
+  "lastUpdate": 1647923471980,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -25308,6 +25308,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1557,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8e358da4e9ddb9d7e89ecba29817cda4d4c98ef7",
+          "message": "Merge pull request #248 from f1shl3gs/events\n\nPassing events between components",
+          "timestamp": "2022-03-22T11:26:01+08:00",
+          "tree_id": "391c03bb6e7f8341d524215e5d13e6ccf7ea2481",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8e358da4e9ddb9d7e89ecba29817cda4d4c98ef7"
+        },
+        "date": 1647923470153,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1754,
             "unit": "s"
           }
         ]
