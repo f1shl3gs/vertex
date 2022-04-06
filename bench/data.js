10f932455e86ac157d18ae6e86e07abe6e3ff01c
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 10:41:49 +0000

    add Build (customSmallerIsBetter) benchmark result for 07b9f482dd8f4e209b3423ae8de6b98d9c1b8b0a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649240613159,
+  "lastUpdate": 1649241709808,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -29011,6 +29011,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/f1shl3gs/vertex/commit/065904cb408e5f2e18fdba4eb95ad391e2ec3fd4"
         },
         "date": 1649225840335,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1701,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07b9f482dd8f4e209b3423ae8de6b98d9c1b8b0a",
+          "message": "Merge pull request #268 from f1shl3gs/dependabot/cargo/tracing-subscriber-0.3.10\n\nchore(deps): bump tracing-subscriber from 0.3.9 to 0.3.10",
+          "timestamp": "2022-04-06T18:09:43+08:00",
+          "tree_id": "1bf5566ca0e030994a18b9a342d932fe909f7fa3",
+          "url": "https://github.com/f1shl3gs/vertex/commit/07b9f482dd8f4e209b3423ae8de6b98d9c1b8b0a"
+        },
+        "date": 1649241708054,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
