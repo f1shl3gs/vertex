2d95e6811f8c2eed5b6b162cae528b669bb33c19
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 14:04:51 +0000

    add Build (customSmallerIsBetter) benchmark result for c06f41511be7add090397f161f36034f59fc4be6

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649253621255,
+  "lastUpdate": 1649253891703,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -29290,6 +29290,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1701,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c06f41511be7add090397f161f36034f59fc4be6",
+          "message": "Merge pull request #274 from f1shl3gs/transform_coerce\n\nimplement transform coercer",
+          "timestamp": "2022-04-06T21:28:29+08:00",
+          "tree_id": "f7f13812e959c03b6211e5b9bea8f4a74e1d2db9",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c06f41511be7add090397f161f36034f59fc4be6"
+        },
+        "date": 1649253889650,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1976,
             "unit": "s"
           }
         ]
