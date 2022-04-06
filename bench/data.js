cc9e2a8bda42576de48ed6fe08ecee2ae32c15d2
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 03:24:48 +0000

    add humanize (customSmallerIsBetter) benchmark result for 2c965d566ccb68b810d0d417c2524f517f45529b

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649215455121,
+  "lastUpdate": 1649215488851,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -10420,6 +10420,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 70.679,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2c965d566ccb68b810d0d417c2524f517f45529b",
+          "message": "Merge pull request #271 from f1shl3gs/dependabot/cargo/tonic-0.7.1\n\nchore(deps): bump tonic from 0.7.0 to 0.7.1",
+          "timestamp": "2022-04-06T11:18:00+08:00",
+          "tree_id": "a50886b8c3c1c38a008fd1a4fd9ea74e3b1fafda",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2c965d566ccb68b810d0d417c2524f517f45529b"
+        },
+        "date": 1649215487150,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 27.79,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 75.255,
             "unit": "ns/op"
           }
         ]
