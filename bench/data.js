3407eebfa04f1e1baca0c3e8a39f257e0bcc8f72
github-action-benchmark <github@users.noreply.github.com>
2022-04-11 04:28:45 +0000

    add prometheus (customSmallerIsBetter) benchmark result for cd28eb5bd65c0fc9624ac40007bf0622e851568f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649651046042,
+  "lastUpdate": 1649651325730,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5008,6 +5008,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1525200,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cd28eb5bd65c0fc9624ac40007bf0622e851568f",
+          "message": "Merge pull request #281 from f1shl3gs/download_flamegraph_directly\n\ndownload the flamegraph directly",
+          "timestamp": "2022-04-11T12:16:57+08:00",
+          "tree_id": "00d599d2cd8cf46c31daf2959431ce46cd6095af",
+          "url": "https://github.com/f1shl3gs/vertex/commit/cd28eb5bd65c0fc9624ac40007bf0622e851568f"
+        },
+        "date": 1649651324100,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1163600,
             "unit": "ns/op"
           }
         ]
