7a4d4cfc8f3ae58dfb704464409f4f07d9cb4e36
github-action-benchmark <github@users.noreply.github.com>
2022-04-11 04:56:34 +0000

    add Build (customSmallerIsBetter) benchmark result for f11f24016e7a72c831b49bb55493d79667fabc18

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649652876385,
+  "lastUpdate": 1649652994339,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -30365,6 +30365,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1924,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f11f24016e7a72c831b49bb55493d79667fabc18",
+          "message": "Merge pull request #280 from f1shl3gs/demo_logs_replace_interval_with_rate\n\nuse rate rather than interval, it's easy to understand",
+          "timestamp": "2022-04-11T12:16:41+08:00",
+          "tree_id": "e904e661c09a9f74b5ebf8af4378c8ff6b8614a8",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f11f24016e7a72c831b49bb55493d79667fabc18"
+        },
+        "date": 1649652991170,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 2173,
             "unit": "s"
           }
         ]
