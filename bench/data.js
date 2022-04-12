02dd8133bb3cdd0f574e9be035c24edf81fd2883
github-action-benchmark <github@users.noreply.github.com>
2022-04-12 04:34:54 +0000

    add prometheus (customSmallerIsBetter) benchmark result for f927e4374c8431e2a062203b986abdb7a4f95859

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649737747409,
+  "lastUpdate": 1649738094798,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5124,6 +5124,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1151400,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f927e4374c8431e2a062203b986abdb7a4f95859",
+          "message": "Merge pull request #285 from f1shl3gs/dependabot/cargo/enumflags2-0.7.5\n\nchore(deps): bump enumflags2 from 0.7.4 to 0.7.5",
+          "timestamp": "2022-04-12T12:20:22+08:00",
+          "tree_id": "73145ba6cae7dab558c280978fb00434e22e6f4b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f927e4374c8431e2a062203b986abdb7a4f95859"
+        },
+        "date": 1649738092526,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1327200,
             "unit": "ns/op"
           }
         ]
