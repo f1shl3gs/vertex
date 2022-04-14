de1a7ad449fbf31205b19e11bd1a2c36c46fb49d
github-action-benchmark <github@users.noreply.github.com>
2022-04-14 12:42:48 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 2e3eec1e44527cd3f0495fd2681943aeec1da058

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649940156465,
+  "lastUpdate": 1649940168871,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5298,6 +5298,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1200000,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e3eec1e44527cd3f0495fd2681943aeec1da058",
+          "message": "Merge pull request #293 from f1shl3gs/dependabot/cargo/kube-0.71.0\n\nchore(deps): bump kube from 0.70.0 to 0.71.0",
+          "timestamp": "2022-04-14T20:28:35+08:00",
+          "tree_id": "0d26914f33c7438bc18e1358c8c29bd072573c7d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2e3eec1e44527cd3f0495fd2681943aeec1da058"
+        },
+        "date": 1649940166871,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1343100,
             "unit": "ns/op"
           }
         ]
