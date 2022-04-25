fc5a42f5f06a96e6e7b89972ad4e3d1689d0645c
github-action-benchmark <github@users.noreply.github.com>
2022-04-25 01:38:53 +0000

    add Build (customSmallerIsBetter) benchmark result for 7cfce6198702c199f29362645561ffc13ba7bb2d

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1650850717470,
+  "lastUpdate": 1650850733020,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -34020,6 +34020,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1795,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7cfce6198702c199f29362645561ffc13ba7bb2d",
+          "message": "Merge pull request #300 from f1shl3gs/dependabot/cargo/uuid-1.0.0\n\nchore(deps): bump uuid from 0.8.2 to 1.0.0",
+          "timestamp": "2022-04-25T09:01:03+08:00",
+          "tree_id": "103f3a1ae17205dd27aeffd3131d93e897bd0cc1",
+          "url": "https://github.com/f1shl3gs/vertex/commit/7cfce6198702c199f29362645561ffc13ba7bb2d"
+        },
+        "date": 1650850730870,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1991,
             "unit": "s"
           }
         ]
