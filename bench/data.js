f49474127df0b7324aae455e3ef6062ff067b7a2
github-action-benchmark <github@users.noreply.github.com>
2022-04-25 05:53:53 +0000

    add Build (customSmallerIsBetter) benchmark result for 0483488d40092eae9be8af1265ca467f67fb839a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1650864926443,
+  "lastUpdate": 1650866033259,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -34176,6 +34176,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1991,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0483488d40092eae9be8af1265ca467f67fb839a",
+          "message": "Merge pull request #302 from f1shl3gs/dependabot/cargo/nix-0.24.1\n\nchore(deps): bump nix from 0.24.0 to 0.24.1",
+          "timestamp": "2022-04-25T13:20:17+08:00",
+          "tree_id": "5b22fb9f9a4910f7672af7e8875323fb39f9e672",
+          "url": "https://github.com/f1shl3gs/vertex/commit/0483488d40092eae9be8af1265ca467f67fb839a"
+        },
+        "date": 1650866031391,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1774,
             "unit": "s"
           }
         ]
