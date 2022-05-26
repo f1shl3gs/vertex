94fc63cc75471a6b67bc5c8b0de5c80aaedbff13
github-action-benchmark <github@users.noreply.github.com>
2022-05-26 14:00:56 +0000

    add humanize (customSmallerIsBetter) benchmark result for b3b10d8fe695103ef97de48633bd1f322235f1ad

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653570179921,
+  "lastUpdate": 1653573656593,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -14229,6 +14229,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 63.47,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3b10d8fe695103ef97de48633bd1f322235f1ad",
+          "message": "Merge pull request #341 from f1shl3gs/dependabot/cargo/regex-1.5.6\n\nchore(deps): bump regex from 1.5.5 to 1.5.6",
+          "timestamp": "2022-05-26T21:53:46+08:00",
+          "tree_id": "0404d8d21887e49271b624b7fef6ccb236879560",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b3b10d8fe695103ef97de48633bd1f322235f1ad"
+        },
+        "date": 1653573654364,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 20.006,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 62.389,
             "unit": "ns/op"
           }
         ]
