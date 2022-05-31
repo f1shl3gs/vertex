63bf2033e9df4532a49f712055ee80967cb183fd
github-action-benchmark <github@users.noreply.github.com>
2022-05-31 02:09:10 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 946134adea761c6b5cfc48c55452033dd7b1f818

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653962728920,
+  "lastUpdate": 1653962950858,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6835,6 +6835,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 976040,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "946134adea761c6b5cfc48c55452033dd7b1f818",
+          "message": "Merge pull request #351 from f1shl3gs/dependabot/cargo/indexmap-1.8.2\n\nchore(deps): bump indexmap from 1.8.1 to 1.8.2",
+          "timestamp": "2022-05-31T09:57:12+08:00",
+          "tree_id": "f2b93448fcd388533f038798a7ed0d243a599a75",
+          "url": "https://github.com/f1shl3gs/vertex/commit/946134adea761c6b5cfc48c55452033dd7b1f818"
+        },
+        "date": 1653962947194,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1403700,
             "unit": "ns/op"
           }
         ]
