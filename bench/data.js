2ea3e41fc49f0e71a1df5246c3208281ef404b32
github-action-benchmark <github@users.noreply.github.com>
2022-06-13 16:20:51 +0000

    add Build (customSmallerIsBetter) benchmark result for 829eb7e60fa5451d5db2e4a4badaf804b8401d88

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655135868997,
+  "lastUpdate": 1655137251537,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -44897,6 +44897,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1379,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "829eb7e60fa5451d5db2e4a4badaf804b8401d88",
+          "message": "Merge pull request #360 from f1shl3gs/fix_metric_register\n\nreplace metrics with our implementation",
+          "timestamp": "2022-06-13T23:48:38+08:00",
+          "tree_id": "0021eb6b49c4e508207dba8ee14c874fd921fb68",
+          "url": "https://github.com/f1shl3gs/vertex/commit/829eb7e60fa5451d5db2e4a4badaf804b8401d88"
+        },
+        "date": 1655137248646,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1655,
             "unit": "s"
           }
         ]
