2cf122e72feab1239973a0cf7fa3cb86d6628fdf
github-action-benchmark <github@users.noreply.github.com>
2022-06-14 12:39:34 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 39dc9f636df2ad92de9524609662961d50d587b7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655210372726,
+  "lastUpdate": 1655210374261,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7183,6 +7183,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1141600,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "39dc9f636df2ad92de9524609662961d50d587b7",
+          "message": "Merge pull request #367 from f1shl3gs/dependabot/cargo/tracing-test-0.2.2\n\nchore(deps): bump tracing-test from 0.2.1 to 0.2.2",
+          "timestamp": "2022-06-14T20:30:32+08:00",
+          "tree_id": "9ded73d40a65780c4b691875a408665f41833010",
+          "url": "https://github.com/f1shl3gs/vertex/commit/39dc9f636df2ad92de9524609662961d50d587b7"
+        },
+        "date": 1655210370613,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1144200,
             "unit": "ns/op"
           }
         ]
