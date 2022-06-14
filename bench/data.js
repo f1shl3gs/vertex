7af73fbe622587a9bde9e25608752a7921256e28
github-action-benchmark <github@users.noreply.github.com>
2022-06-14 14:06:45 +0000

    add vertex (customSmallerIsBetter) benchmark result for 3aac8c7955769d616cd2b85863a3ed5a56d2db25

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655215558363,
+  "lastUpdate": 1655215605630,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -41012,6 +41012,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 26.313,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3aac8c7955769d616cd2b85863a3ed5a56d2db25",
+          "message": "Merge pull request #370 from f1shl3gs/dependabot/cargo/uuid-1.1.2\n\nchore(deps): bump uuid from 1.1.0 to 1.1.2",
+          "timestamp": "2022-06-14T21:33:40+08:00",
+          "tree_id": "0759e951ab3291d3f796c4dbf832ca2de3e6e3f8",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3aac8c7955769d616cd2b85863a3ed5a56d2db25"
+        },
+        "date": 1655215602695,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "attributes/insert",
+            "value": 66.454,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 225.76,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 504.78,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 1374.4,
+            "unit": "ns/op"
+          },
+          {
+            "name": "haproxy/parse_csv",
+            "value": 40616000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1097200,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 25.294,
             "unit": "ns/op"
           }
         ]
