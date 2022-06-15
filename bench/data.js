099a992cc4b735bf64141f458734facdf9164b19
github-action-benchmark <github@users.noreply.github.com>
2022-06-15 14:36:06 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 3d6b4687e663f271e10c92ae78484406e3f252a4

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655303699751,
+  "lastUpdate": 1655303766728,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7357,6 +7357,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1390200,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3d6b4687e663f271e10c92ae78484406e3f252a4",
+          "message": "Merge pull request #372 from f1shl3gs/f64_gauge\n\nf64 gauge",
+          "timestamp": "2022-06-15T22:25:37+08:00",
+          "tree_id": "d5294fe0722c9df1084cfbe7ead787722f2f64a0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3d6b4687e663f271e10c92ae78484406e3f252a4"
+        },
+        "date": 1655303762249,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1271700,
             "unit": "ns/op"
           }
         ]
