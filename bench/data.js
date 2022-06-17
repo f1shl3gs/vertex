985603b637cedff5f66b173bb5b024b8c6982691
github-action-benchmark <github@users.noreply.github.com>
2022-06-17 15:42:21 +0000

    add Build (customSmallerIsBetter) benchmark result for 4e6ff419b0fbdbda7b7ff2c131e1b33d0981ead2

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655480516357,
+  "lastUpdate": 1655480541251,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -48552,6 +48552,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1964,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4e6ff419b0fbdbda7b7ff2c131e1b33d0981ead2",
+          "message": "Merge pull request #357 from f1shl3gs/dependabot/cargo/testcontainers-0.14.0\n\nchore(deps): bump testcontainers from 0.12.0 to 0.14.0",
+          "timestamp": "2022-06-17T23:11:18+08:00",
+          "tree_id": "bc8b7e24147080a444c41b33184193a8cf9c60b9",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4e6ff419b0fbdbda7b7ff2c131e1b33d0981ead2"
+        },
+        "date": 1655480538055,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1609,
             "unit": "s"
           }
         ]
