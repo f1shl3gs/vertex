d40657859fdff3ffa0d91972ee1efa13d70abff8
github-action-benchmark <github@users.noreply.github.com>
2022-06-22 09:29:29 +0000

    add prometheus (customSmallerIsBetter) benchmark result for ff1d900ee2dad2fb88968f7fb6104083b22e3cf5

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655890005477,
+  "lastUpdate": 1655890169054,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7792,6 +7792,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1150100,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff1d900ee2dad2fb88968f7fb6104083b22e3cf5",
+          "message": "Merge pull request #385 from f1shl3gs/replace_xdrgen_with_fork\n\nupdate xdrgen",
+          "timestamp": "2022-06-22T17:18:50+08:00",
+          "tree_id": "7cf4911ddf54a293814f2275e30bee29e6dfc03c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ff1d900ee2dad2fb88968f7fb6104083b22e3cf5"
+        },
+        "date": 1655890164429,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1271500,
             "unit": "ns/op"
           }
         ]
