d05e3707edcdd81fb49f4db97ec6908f60abb797
github-action-benchmark <github@users.noreply.github.com>
2022-06-25 13:57:54 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 2380cef1530ee118a0d2995f40b0065a20816d34

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656165411216,
+  "lastUpdate": 1656165474222,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7937,6 +7937,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1419500,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2380cef1530ee118a0d2995f40b0065a20816d34",
+          "message": "Merge pull request #391 from f1shl3gs/transform/geoip\n\nadd transform/geoip",
+          "timestamp": "2022-06-25T21:49:27+08:00",
+          "tree_id": "2338438a92b8e758092aa17936dd28c5d391460e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2380cef1530ee118a0d2995f40b0065a20816d34"
+        },
+        "date": 1656165470899,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1104800,
             "unit": "ns/op"
           }
         ]
