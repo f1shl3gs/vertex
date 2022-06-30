0be2bd68e7c947dbd6d53693a81d5b13364d0cc7
github-action-benchmark <github@users.noreply.github.com>
2022-06-30 10:27:26 +0000

    add tracing-limit (customSmallerIsBetter) benchmark result for d67d424427caeccba3e767f12b91eb26dc6f6b91

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656584805264,
+  "lastUpdate": 1656584846007,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -35807,6 +35807,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "tracing-limit/5s/1000",
             "value": 262110,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d67d424427caeccba3e767f12b91eb26dc6f6b91",
+          "message": "Merge pull request #399 from f1shl3gs/dependabot/cargo/smallvec-1.9.0\n\nchore(deps): bump smallvec from 1.8.1 to 1.9.0",
+          "timestamp": "2022-06-30T18:19:50+08:00",
+          "tree_id": "caccb2901248f147f5264f61c05e0d27535a07d7",
+          "url": "https://github.com/f1shl3gs/vertex/commit/d67d424427caeccba3e767f12b91eb26dc6f6b91"
+        },
+        "date": 1656584843655,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tracing-limit/none/1",
+            "value": 738.07,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/100",
+            "value": 73746,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/500",
+            "value": 326170,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/1000",
+            "value": 651130,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1",
+            "value": 230.16,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/100",
+            "value": 22959,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/500",
+            "value": 130100,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1000",
+            "value": 229510,
             "unit": "ns/op"
           }
         ]
