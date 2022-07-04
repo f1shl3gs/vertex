44905a23f9e5d9cd3f8216631ce59d1f8bdc287b
github-action-benchmark <github@users.noreply.github.com>
2022-07-04 19:16:31 +0000

    add Build (customSmallerIsBetter) benchmark result for 03c0c0b94dce7f59e5fbc32016db390832f5d3ad

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656962148949,
+  "lastUpdate": 1656962191380,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -55700,6 +55700,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1294,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03c0c0b94dce7f59e5fbc32016db390832f5d3ad",
+          "message": "Merge pull request #410 from f1shl3gs/dependabot/cargo/thrift-0.16.0\n\nchore(deps): bump thrift from 0.15.0 to 0.16.0",
+          "timestamp": "2022-07-05T02:49:26+08:00",
+          "tree_id": "2962d0313889d476ec9087686c1dc99830cb6d01",
+          "url": "https://github.com/f1shl3gs/vertex/commit/03c0c0b94dce7f59e5fbc32016db390832f5d3ad"
+        },
+        "date": 1656962188203,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1413,
             "unit": "s"
           }
         ]
