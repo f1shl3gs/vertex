9f6e57c28507ac6f6221f0da8dfd33211aae59d8
github-action-benchmark <github@users.noreply.github.com>
2022-07-06 17:31:19 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 1d198dec443f0473aa5b1bbec9dcd495ae6df5f2

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1657128565096,
+  "lastUpdate": 1657128679156,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -8575,6 +8575,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 1297200,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d198dec443f0473aa5b1bbec9dcd495ae6df5f2",
+          "message": "Merge pull request #415 from f1shl3gs/sinks/elasticsearch\n\nSinks/elasticsearch",
+          "timestamp": "2022-07-07T01:21:48+08:00",
+          "tree_id": "790b3361eb71c656a3d6d92591af92efff5b8b9c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1d198dec443f0473aa5b1bbec9dcd495ae6df5f2"
+        },
+        "date": 1657128674781,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 1381500,
             "unit": "ns/op"
           }
         ]
