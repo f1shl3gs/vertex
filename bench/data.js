e907cf887d9497ead1627d3833307b72386d4cd5
github-action-benchmark <github@users.noreply.github.com>
2022-07-07 03:51:31 +0000

    add humanize (customSmallerIsBetter) benchmark result for 7aa43d3060a6ae991883f9af28bac7986f5ec449

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1657165884492,
+  "lastUpdate": 1657165891610,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -18731,6 +18731,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 67.356,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7aa43d3060a6ae991883f9af28bac7986f5ec449",
+          "message": "Merge pull request #417 from f1shl3gs/dependabot/cargo/openssl-src-111.22.01.1.1q\n\nchore(deps): bump openssl-src from 111.20.0+1.1.1o to 111.22.0+1.1.1q",
+          "timestamp": "2022-07-07T11:44:24+08:00",
+          "tree_id": "75f0ac12effa867751d675d421f6ecc4c18f2f35",
+          "url": "https://github.com/f1shl3gs/vertex/commit/7aa43d3060a6ae991883f9af28bac7986f5ec449"
+        },
+        "date": 1657165886848,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 23.042,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 55.843,
             "unit": "ns/op"
           }
         ]
