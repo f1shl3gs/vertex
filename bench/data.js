5cf1b973e62536733853250afca309e34f38c155
github-action-benchmark <github@users.noreply.github.com>
2022-07-26 14:25:46 +0000

    add Build (customSmallerIsBetter) benchmark result for 7829856a41ccf18c243ff1e068539ed67a1d6430

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1658843909939,
+  "lastUpdate": 1658845546011,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -59355,6 +59355,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1452,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7829856a41ccf18c243ff1e068539ed67a1d6430",
+          "message": "Merge pull request #452 from f1shl3gs/dependabot/cargo/crossbeam-queue-0.3.6\n\nchore(deps): bump crossbeam-queue from 0.3.5 to 0.3.6",
+          "timestamp": "2022-07-26T21:54:17+08:00",
+          "tree_id": "f02e9970898fea9c721e91a36a103a901b9e64c4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/7829856a41ccf18c243ff1e068539ed67a1d6430"
+        },
+        "date": 1658845539977,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1575,
             "unit": "s"
           }
         ]
