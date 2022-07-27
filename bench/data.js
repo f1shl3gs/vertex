7f32750f3594c3554c982bca4533da5d9b2c4125
github-action-benchmark <github@users.noreply.github.com>
2022-07-27 09:47:27 +0000

    add humanize (customSmallerIsBetter) benchmark result for 4513130e54bb9e1d14bc39d75220b0606fe155bc

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1658866233375,
+  "lastUpdate": 1658915247295,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -20129,6 +20129,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "duration_to_string",
             "value": 74.529,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4513130e54bb9e1d14bc39d75220b0606fe155bc",
+          "message": "Merge pull request #456 from f1shl3gs/transforms/filter\n\nadd transforms/filter",
+          "timestamp": "2022-07-27T17:43:23+08:00",
+          "tree_id": "494c385202fddf404d1693ec944dad6b28fe5504",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4513130e54bb9e1d14bc39d75220b0606fe155bc"
+        },
+        "date": 1658915243278,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "parse_duration",
+            "value": 18.733,
+            "unit": "ns/op"
+          },
+          {
+            "name": "duration_to_string",
+            "value": 63.725,
             "unit": "ns/op"
           }
         ]
