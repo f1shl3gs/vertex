4b677353c8cc54ae70659d280b151836ce4a0d01
github-action-benchmark <github@users.noreply.github.com>
2022-08-06 07:08:22 +0000

    add Build (customSmallerIsBetter) benchmark result for 90af60ba58dfbd8b37337d8125ccb967a820c775

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659769614850,
+  "lastUpdate": 1659769702848,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -61438,6 +61438,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1513,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "90af60ba58dfbd8b37337d8125ccb967a820c775",
+          "message": "Merge pull request #499 from f1shl3gs/upgrade_deps\n\nUpgrade deps",
+          "timestamp": "2022-08-06T14:42:47+08:00",
+          "tree_id": "8c989bd259cd6d95351e316cbcbbea3f1e51e87a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/90af60ba58dfbd8b37337d8125ccb967a820c775"
+        },
+        "date": 1659769698360,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1319,
             "unit": "s"
           }
         ]
