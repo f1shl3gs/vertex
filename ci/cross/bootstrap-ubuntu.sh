64917d51b487aff0971e6b1472ddada9b3681b83
f1shl3gs <fishlegs.engerman@gmail.com>
2022-08-06 14:06:36 +0800

    move protoc to /usr/local/bin

--- ci/cross/bootstrap-ubuntu.sh
+++ ci/cross/bootstrap-ubuntu.sh
@@ -37,6 +37,7 @@ apt install --yes \
 PROTOC_VERSION=3.20.1
 curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip" -o protoc-${PROTOC_VERSION}-linux-x86_64.zip
 unzip protoc-${PROTOC_VERSION}-linux-x86_64.zip
+cp bin/protoc /usr/local/bin
 cp -r include/google /usr/local/include/
 rm protoc-${PROTOC_VERSION}-linux-x86_64.zip
 
