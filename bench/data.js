2b024cdbaca26bd7d90e3f003ad1908ce69e871d
github-action-benchmark <github@users.noreply.github.com>
2022-08-18 14:42:28 +0000

    add Build (customSmallerIsBetter) benchmark result for 4d56bc8b29211dc45cb5cf9f6a556970827ac7e7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660832606042,
+  "lastUpdate": 1660833748274,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -64819,6 +64819,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1458,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4d56bc8b29211dc45cb5cf9f6a556970827ac7e7",
+          "message": "Merge pull request #506 from f1shl3gs/dependabot/cargo/ahash-0.8.0\n\nchore(deps): bump ahash from 0.7.6 to 0.8.0",
+          "timestamp": "2022-08-18T22:16:04+08:00",
+          "tree_id": "6e42759a05e08b6a8f37b0ad621ff41534dcc81f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4d56bc8b29211dc45cb5cf9f6a556970827ac7e7"
+        },
+        "date": 1660833743944,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1346,
             "unit": "s"
           }
         ]
