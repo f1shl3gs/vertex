4d14f00009782f7cf8a2346912b07b94425f0951
github-action-benchmark <github@users.noreply.github.com>
2022-09-21 04:15:13 +0000

    add tracing-limit (customSmallerIsBetter) benchmark result for e03767359dc666a99263a07698b6540c68896cef

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663733662786,
+  "lastUpdate": 1663733713108,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -45396,6 +45396,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "tracing-limit/5s/1000",
             "value": 305030,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e03767359dc666a99263a07698b6540c68896cef",
+          "message": "Merge pull request #559 from f1shl3gs/dependabot/cargo/governor-0.5.0\n\nchore(deps): bump governor from 0.4.2 to 0.5.0",
+          "timestamp": "2022-09-21T12:08:12+08:00",
+          "tree_id": "a4eb8d41167386686b4b49ef9ce744a1811fd58e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/e03767359dc666a99263a07698b6540c68896cef"
+        },
+        "date": 1663733706982,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tracing-limit/none/1",
+            "value": 648.96,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/100",
+            "value": 64871,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/500",
+            "value": 324400,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/1000",
+            "value": 648880,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1",
+            "value": 311.18,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/100",
+            "value": 31082,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/500",
+            "value": 155480,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1000",
+            "value": 310820,
             "unit": "ns/op"
           }
         ]
