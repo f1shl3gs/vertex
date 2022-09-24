79406414dc8a89af321568442ce8bc59d0ba61b5
github-action-benchmark <github@users.noreply.github.com>
2022-09-24 01:18:35 +0000

    add Build (customSmallerIsBetter) benchmark result for a95f3234ad7489b7b0425d631ce67eed592a3bde

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663981811664,
+  "lastUpdate": 1663982315894,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -71386,6 +71386,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1322,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a95f3234ad7489b7b0425d631ce67eed592a3bde",
+          "message": "Merge pull request #563 from f1shl3gs/dependabot/cargo/argh-0.1.9\n\nchore(deps): bump argh from 0.1.8 to 0.1.9",
+          "timestamp": "2022-09-24T08:45:17+08:00",
+          "tree_id": "d2437d4d08d33a2ff363e07bfc67e5c751137085",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a95f3234ad7489b7b0425d631ce67eed592a3bde"
+        },
+        "date": 1663982309193,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1649,
             "unit": "s"
           }
         ]
