3f8eb9ad18dd1636c28c25e3f6ec3f3a7c089fb1
github-action-benchmark <github@users.noreply.github.com>
2022-09-26 09:18:55 +0000

    add prometheus (customSmallerIsBetter) benchmark result for c3ad6bc3df690c50b4f59d93b1a934eafa525cec

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664183919872,
+  "lastUpdate": 1664183935178,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -10431,6 +10431,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 915660,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "distinct": true,
+          "id": "c3ad6bc3df690c50b4f59d93b1a934eafa525cec",
+          "message": "update toolchain to 1.64.0",
+          "timestamp": "2022-09-26T17:12:46+08:00",
+          "tree_id": "408bd6f274c2ad066a81cf83fb792efc3eaf0858",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c3ad6bc3df690c50b4f59d93b1a934eafa525cec"
+        },
+        "date": 1664183927566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 853030,
             "unit": "ns/op"
           }
         ]
