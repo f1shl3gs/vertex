b5b7148e01c8fc9259867eafda7d8cdb77fb612a
github-action-benchmark <github@users.noreply.github.com>
2022-09-30 16:33:54 +0000

    add prometheus (customSmallerIsBetter) benchmark result for b815431ab4a29ac4b097df73754b8ded560dc24a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664555620647,
+  "lastUpdate": 1664555634731,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -10605,6 +10605,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 851800,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b815431ab4a29ac4b097df73754b8ded560dc24a",
+          "message": "Merge pull request #570 from f1shl3gs/dependabot/cargo/tonic-build-0.8.2\n\nchore(deps): bump tonic-build from 0.8.0 to 0.8.2",
+          "timestamp": "2022-10-01T00:26:52+08:00",
+          "tree_id": "9b234e69e4e6a93c53e215371ff067a7ea0b6954",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b815431ab4a29ac4b097df73754b8ded560dc24a"
+        },
+        "date": 1664555627495,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 841780,
             "unit": "ns/op"
           }
         ]
