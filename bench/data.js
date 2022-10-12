7052a0fb1ce5d45e7f1c2391cdf6d6687aef1d2e
github-action-benchmark <github@users.noreply.github.com>
2022-10-12 07:13:40 +0000

    add condition (customSmallerIsBetter) benchmark result for b5746bade7315f08ce86d81c501996533245baab

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665558793179,
+  "lastUpdate": 1665558820532,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -79468,6 +79468,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 52.53,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5746bade7315f08ce86d81c501996533245baab",
+          "message": "Merge pull request #584 from f1shl3gs/remove_redis_dep\n\nremove redis_dep",
+          "timestamp": "2022-10-12T15:08:07+08:00",
+          "tree_id": "f50e114cd07f8e31665509c7458a50c562a42535",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b5746bade7315f08ce86d81c501996533245baab"
+        },
+        "date": 1665558815183,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 22.072,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 22.661,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 21.795,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.943,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 48.991,
             "unit": "ns/op"
           }
         ]
