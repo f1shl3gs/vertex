7eafafb6fe68f4842ac244d001267343fbf993b1
github-action-benchmark <github@users.noreply.github.com>
2022-10-13 06:30:57 +0000

    add vertex (customSmallerIsBetter) benchmark result for b9f38ab67a358dddcb7ba8e2a80543e788973946

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665641766311,
+  "lastUpdate": 1665642657779,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -65650,6 +65650,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 19.977,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9f38ab67a358dddcb7ba8e2a80543e788973946",
+          "message": "Merge pull request #586 from f1shl3gs/dependabot/github_actions/styfle/cancel-workflow-action-0.11.0\n\nchore(ci): bump styfle/cancel-workflow-action from 0.10.1 to 0.11.0",
+          "timestamp": "2022-10-13T14:08:09+08:00",
+          "tree_id": "5caae3248c197cb57e7e10bbc01ff1ab7ba34d6d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b9f38ab67a358dddcb7ba8e2a80543e788973946"
+        },
+        "date": 1665642652142,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "attributes/insert",
+            "value": 49.333,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 170.02,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 393.02,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 1098.1,
+            "unit": "ns/op"
+          },
+          {
+            "name": "haproxy/parse_csv",
+            "value": 25891000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1064300,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 20.024,
             "unit": "ns/op"
           }
         ]
