c93292760878a8cd531ac6a5daccf739012b8b70
github-action-benchmark <github@users.noreply.github.com>
2022-10-13 06:40:18 +0000

    add Build (customSmallerIsBetter) benchmark result for 4220d9a16a25902a1ee628400f64e04b0501a5ab

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665643142869,
+  "lastUpdate": 1665643218401,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -76819,6 +76819,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1520,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4220d9a16a25902a1ee628400f64e04b0501a5ab",
+          "message": "Merge pull request #587 from f1shl3gs/dependabot/cargo/atoi-2.0.0\n\nchore(deps): bump atoi from 1.0.0 to 2.0.0",
+          "timestamp": "2022-10-13T14:08:39+08:00",
+          "tree_id": "0545c716f4dda69ef39cb5b3c2ca1d6a8326294e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4220d9a16a25902a1ee628400f64e04b0501a5ab"
+        },
+        "date": 1665643211268,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1565,
             "unit": "s"
           }
         ]
