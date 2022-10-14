d327e06b12a76297af8ee4004a13b9717397c629
github-action-benchmark <github@users.noreply.github.com>
2022-10-14 13:48:20 +0000

    add Build (customSmallerIsBetter) benchmark result for bac20d2e6d16e9c76db16e433ffdba43e1e43f73

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665754939209,
+  "lastUpdate": 1665755300191,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -77249,6 +77249,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1279,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bac20d2e6d16e9c76db16e433ffdba43e1e43f73",
+          "message": "Merge pull request #589 from f1shl3gs/optimize_dependencies\n\noptimize dependencies",
+          "timestamp": "2022-10-14T21:15:24+08:00",
+          "tree_id": "c5bf1aeb77eab7ef58c075ddd02bee82c5aa4761",
+          "url": "https://github.com/f1shl3gs/vertex/commit/bac20d2e6d16e9c76db16e433ffdba43e1e43f73"
+        },
+        "date": 1665755292020,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1614,
             "unit": "s"
           }
         ]
