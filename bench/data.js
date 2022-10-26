25935c9c53763c94d023082a84960a88ebc8f9b5
github-action-benchmark <github@users.noreply.github.com>
2022-10-26 18:09:43 +0000

    add condition (customSmallerIsBetter) benchmark result for a6a0041c89b3631db516f4c4f9dca24d732ae2e7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666807754028,
+  "lastUpdate": 1666807783590,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -82792,6 +82792,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 54.25,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6a0041c89b3631db516f4c4f9dca24d732ae2e7",
+          "message": "Merge pull request #597 from f1shl3gs/dependabot/cargo/base64-0.13.1\n\nchore(deps): bump base64 from 0.13.0 to 0.13.1",
+          "timestamp": "2022-10-27T02:03:24+08:00",
+          "tree_id": "d87832c85a6529447f648fe27200542d15ade9cc",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a6a0041c89b3631db516f4c4f9dca24d732ae2e7"
+        },
+        "date": 1666807776055,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 32.636,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 31.629,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 31.854,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 45.063,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 70.76,
             "unit": "ns/op"
           }
         ]
