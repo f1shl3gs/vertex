d2045e6cf3125f78af76cc45c4da4edb6aa61b63
github-action-benchmark <github@users.noreply.github.com>
2022-10-26 18:08:29 +0000

    add prometheus (customSmallerIsBetter) benchmark result for a6a0041c89b3631db516f4c4f9dca24d732ae2e7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666807689672,
+  "lastUpdate": 1666807708928,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -11330,6 +11330,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 716770,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6a0041c89b3631db516f4c4f9dca24d732ae2e7",
+          "message": "Merge pull request #597 from f1shl3gs/dependabot/cargo/base64-0.13.1\n\nchore(deps): bump base64 from 0.13.0 to 0.13.1",
+          "timestamp": "2022-10-27T02:03:24+08:00",
+          "tree_id": "d87832c85a6529447f648fe27200542d15ade9cc",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a6a0041c89b3631db516f4c4f9dca24d732ae2e7"
+        },
+        "date": 1666807703513,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 634810,
             "unit": "ns/op"
           }
         ]
