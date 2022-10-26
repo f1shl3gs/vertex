6d5fe324a09825cfefdc85c03aa561df5b033910
github-action-benchmark <github@users.noreply.github.com>
2022-10-26 18:18:45 +0000

    add tracing-limit (customSmallerIsBetter) benchmark result for a6a0041c89b3631db516f4c4f9dca24d732ae2e7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666807900380,
+  "lastUpdate": 1666808325516,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -50286,6 +50286,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "tracing-limit/5s/1000",
             "value": 317120,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6a0041c89b3631db516f4c4f9dca24d732ae2e7",
+          "message": "Merge pull request #597 from f1shl3gs/dependabot/cargo/base64-0.13.1\n\nchore(deps): bump base64 from 0.13.0 to 0.13.1",
+          "timestamp": "2022-10-27T02:03:24+08:00",
+          "tree_id": "d87832c85a6529447f648fe27200542d15ade9cc",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a6a0041c89b3631db516f4c4f9dca24d732ae2e7"
+        },
+        "date": 1666808317763,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tracing-limit/none/1",
+            "value": 601.26,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/100",
+            "value": 60500,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/500",
+            "value": 363140,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/none/1000",
+            "value": 601170,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1",
+            "value": 315.59,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/100",
+            "value": 31149,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/500",
+            "value": 156310,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tracing-limit/5s/1000",
+            "value": 314460,
             "unit": "ns/op"
           }
         ]
