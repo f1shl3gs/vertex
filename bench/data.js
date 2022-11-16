a2579ac92a9072a9712bbb91af3d8ce1815f7928
github-action-benchmark <github@users.noreply.github.com>
2022-11-16 08:17:29 +0000

    add vertex (customSmallerIsBetter) benchmark result for 3dd2f2b7ec70531281b4f9d35c384cd910ae4764

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668585115612,
+  "lastUpdate": 1668586649082,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -56497,6 +56497,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 21.989,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3dd2f2b7ec70531281b4f9d35c384cd910ae4764",
+          "message": "Merge pull request #625 from f1shl3gs/dependabot/cargo/mimalloc-0.1.32\n\nchore(deps): bump mimalloc from 0.1.31 to 0.1.32",
+          "timestamp": "2022-11-16T15:34:18+08:00",
+          "tree_id": "d10aa4baeec9d4daccf8071aa9361056e87423d6",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3dd2f2b7ec70531281b4f9d35c384cd910ae4764"
+        },
+        "date": 1668586642597,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "attributes/insert",
+            "value": 46.786,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 153.44,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 355.51,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 1027.6,
+            "unit": "ns/op"
+          },
+          {
+            "name": "haproxy/parse_csv",
+            "value": 36936000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1044100,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 19.372,
             "unit": "ns/op"
           }
         ]
