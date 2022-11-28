08b9a639f27b37b3e32ff723ca16c88929c346c0
github-action-benchmark <github@users.noreply.github.com>
2022-11-28 14:24:33 +0000

    add vertex (customSmallerIsBetter) benchmark result for 9e94db188fa32f901ae2a96aad0a9d2493827246

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669645424280,
+  "lastUpdate": 1669645473562,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -58321,6 +58321,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 21.322,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9e94db188fa32f901ae2a96aad0a9d2493827246",
+          "message": "Merge pull request #638 from f1shl3gs/dependabot/cargo/serde-1.0.148\n\nchore(deps): bump serde from 1.0.147 to 1.0.148",
+          "timestamp": "2022-11-28T22:00:03+08:00",
+          "tree_id": "15aa0f495de4331bcc576d922d23872d1b4450de",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9e94db188fa32f901ae2a96aad0a9d2493827246"
+        },
+        "date": 1669645466692,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "attributes/insert",
+            "value": 53.455,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 177.1,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 409.13,
+            "unit": "ns/op"
+          },
+          {
+            "name": "attributes/insert",
+            "value": 1170.8,
+            "unit": "ns/op"
+          },
+          {
+            "name": "haproxy/parse_csv",
+            "value": 38169000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1115800,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 21.245,
             "unit": "ns/op"
           }
         ]
