c06a5fe4595122eb4f535bc0e8f7083b90e41fc4
github-action-benchmark <github@users.noreply.github.com>
2022-11-30 09:34:50 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 929538c9ed6ac21a69ae77b5c36c8ec8f18d1c64

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669800885858,
+  "lastUpdate": 1669800890112,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -12490,6 +12490,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 718850,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "929538c9ed6ac21a69ae77b5c36c8ec8f18d1c64",
+          "message": "Merge pull request #639 from f1shl3gs/dependabot/cargo/prost-build-0.11.3\n\nchore(deps): bump prost-build from 0.11.2 to 0.11.3",
+          "timestamp": "2022-11-30T17:30:04+08:00",
+          "tree_id": "d4915f8185569ab97edf4f710dc1f03f046388f9",
+          "url": "https://github.com/f1shl3gs/vertex/commit/929538c9ed6ac21a69ae77b5c36c8ec8f18d1c64"
+        },
+        "date": 1669800882781,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 718980,
             "unit": "ns/op"
           }
         ]
