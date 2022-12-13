2b6b0c4a3ff9f17fdb308da2bc60462d8b23c5d1
f1shl3gs <fishlegs.engerman@gmail.com>
2022-12-13 23:03:51 +0800

    add prepare.sh

--- ci/environment/bootstrap-ubuntu-20.04.sh
+++ ci/environment/bootstrap-ubuntu-20.04.sh
@@ -54,27 +54,3 @@ else
     echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> "${HOME}/.bash_profile"
 fi
 
-# Setup cargo-cross
-source "$HOME/.cargo/env"
-rustup show # causes installation of version from rust-toolchain.toml
-rustup default "$(rustup show active-toolchain | awk '{print $1;}')"
-
-if [[ "$(cross --version | grep cross)" != "cross 0.2.4" ]]; then
-  rustup run stable cargo install cross --version 0.2.4 --force
-fi
-
-# Make sure our release build settings are present.
-#
-# We want to ensure we're building using "full" release capabilities when possible, which
-# means full LTO and a single codegen unit.  This maximizes performance of the resulting
-# code, but increases compilation time.  We only set this if we're in CI _and_ we haven't
-# been instructed to use the debug profile (via PROFILE environment variable).
-if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
-  {
-    echo "CARGO_PROFILE_RELEASE_LTO=fat";
-    echo "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1";
-    echo "CARGO_PROFILE_RELEASE_DEBUG=false";
-  } >> "${GITHUB_ENV}"
-fi
-
-bash ci/install_protoc.sh
---
+++ ci/prepare.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -e -o verbose
+
+rustup show # causes installation of version from rust-toolchain.toml
+rustup default "$(rustup show active-toolchain | awk '{print $1;}')"
+
+# Setup cargo-cross
+if [[ "$(cross --version | grep cross)" != "cross 0.2.4" ]]; then
+  rustup run stable cargo install cross --version 0.2.4 --force
+fi
+
+# Make sure our release build settings are present.
+#
+# We want to ensure we're building using "full" release capabilities when possible, which
+# means full LTO and a single codegen unit.  This maximizes performance of the resulting
+# code, but increases compilation time.  We only set this if we're in CI _and_ we haven't
+# been instructed to use the debug profile (via PROFILE environment variable).
+if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
+  {
+    echo "CARGO_PROFILE_RELEASE_LTO=fat";
+    echo "CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1";
+    echo "CARGO_PROFILE_RELEASE_DEBUG=false";
+  } >> "${GITHUB_ENV}"
+fi
