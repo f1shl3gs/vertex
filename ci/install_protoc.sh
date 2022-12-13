d3c4453b565ddd34063443ba9285f7d516fb5257
f1shl3gs <fishlegs.engerman@gmail.com>
2022-12-13 23:08:56 +0800

    mkdir before cp

--- ci/install_protoc.sh
+++ ci/install_protoc.sh
@@ -5,6 +5,9 @@ PROTOC_VERSION=3.20.1
 
 curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip" -o protoc-${PROTOC_VERSION}-linux-x86_64.zip
 unzip protoc-${PROTOC_VERSION}-linux-x86_64.zip
+
 cp bin/protoc /usr/local/bin
+
+mkdir -p /usr/local/include/google
 cp -r include/google /usr/local/include/
 rm protoc-${PROTOC_VERSION}-linux-x86_64.zip
