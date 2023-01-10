20b26c56acae4ae0c64b199cd0c2717557c4a8bb
github-action-benchmark <github@users.noreply.github.com>
2023-01-10 12:02:43 +0000

    add prometheus (customSmallerIsBetter) benchmark result for c914d84368eeadf55836cfbef160bfb1d8bb7911

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673348180702,
+  "lastUpdate": 1673352163087,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13998,6 +13998,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 718850,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c914d84368eeadf55836cfbef160bfb1d8bb7911",
+          "message": "Merge pull request #698 from f1shl3gs/blocking_thread_keepalive\n\nadd blocking_thread_keepalive",
+          "timestamp": "2023-01-10T19:58:43+08:00",
+          "tree_id": "25f295589d7e9a6640f29d35bf2a31e36093c86f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c914d84368eeadf55836cfbef160bfb1d8bb7911"
+        },
+        "date": 1673352155691,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 717410,
             "unit": "ns/op"
           }
         ]
