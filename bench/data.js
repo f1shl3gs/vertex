55981380b4e11e4173b73cce843eeb5f0b368877
github-action-benchmark <github@users.noreply.github.com>
2023-01-11 13:43:20 +0000

    add condition (customSmallerIsBetter) benchmark result for 802f13da0d85551c7aa8b2e190c8f438940915a4

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673444592278,
+  "lastUpdate": 1673444600403,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -87620,6 +87620,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 51.176,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "802f13da0d85551c7aa8b2e190c8f438940915a4",
+          "message": "Merge pull request #700 from f1shl3gs/remove_strip_ansi_escapes\n\nremove strip-ansi-escapes",
+          "timestamp": "2023-01-11T21:38:42+08:00",
+          "tree_id": "012afe649e113aa4fe5847184189f43a4dbf12b5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/802f13da0d85551c7aa8b2e190c8f438940915a4"
+        },
+        "date": 1673444591732,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 24.521,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.462,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 24.505,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 32.912,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 51.938,
             "unit": "ns/op"
           }
         ]
