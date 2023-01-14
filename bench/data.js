73c6ce024df5dddde9635e0f599cf87849c2534d
github-action-benchmark <github@users.noreply.github.com>
2023-01-14 15:19:53 +0000

    add Build (customSmallerIsBetter) benchmark result for 49dfea83db53aeefe58e59116dfd484c0852e188

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673708631621,
+  "lastUpdate": 1673709593510,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -187,6 +187,42 @@ window.BENCHMARK_DATA = {
             "name": "tracing-limit/5s/1000",
             "value": 264440,
             "unit": "ns/op"
+          }
+        ]
+      }
+    ],
+    "Build": [
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "49dfea83db53aeefe58e59116dfd484c0852e188",
+          "message": "Merge pull request #710 from f1shl3gs/fix_bench_charts_name\n\nfix bench name",
+          "timestamp": "2023-01-14T22:59:04+08:00",
+          "tree_id": "7abdd153c16fac584f3033b1748d455a947b0c3c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/49dfea83db53aeefe58e59116dfd484c0852e188"
+        },
+        "date": 1673709585299,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1075,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 56930896,
+            "unit": "bytes"
           }
         ]
       }
