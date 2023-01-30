8132227feeb1ea561bb9fcae4018ca2c2079c37e
github-action-benchmark <github@users.noreply.github.com>
2023-01-30 20:02:38 +0000

    add Build (customSmallerIsBetter) benchmark result for 4198c1ca299e06df3f10a48f88d3d4104ef681da

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675108055195,
+  "lastUpdate": 1675108958948,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -3198,6 +3198,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 56927280,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4198c1ca299e06df3f10a48f88d3d4104ef681da",
+          "message": "Merge pull request #722 from f1shl3gs/dependabot/cargo/notify-5.1.0\n\nchore(deps): bump notify from 5.0.0 to 5.1.0",
+          "timestamp": "2023-01-31T03:42:03+08:00",
+          "tree_id": "1a1c5c263e18de22de23a34793cfe37956314126",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4198c1ca299e06df3f10a48f88d3d4104ef681da"
+        },
+        "date": 1675108949793,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1035,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 56929496,
             "unit": "bytes"
           }
         ]
