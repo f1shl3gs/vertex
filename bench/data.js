f265f8433dc2abfce753741e2529465cb6847890
github-action-benchmark <github@users.noreply.github.com>
2023-02-07 05:17:54 +0000

    add Build (customSmallerIsBetter) benchmark result for eca3fb2ac99c3989e28433c3b84f2379af651b44

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675747046294,
+  "lastUpdate": 1675747074469,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6090,6 +6090,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1057,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 56915704,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eca3fb2ac99c3989e28433c3b84f2379af651b44",
+          "message": "Merge pull request #743 from f1shl3gs/dependabot/cargo/tikv-jemalloc-sys-0.5.35.3.0-patched\n\nchore(deps): bump tikv-jemalloc-sys from 0.5.2+5.3.0-patched to 0.5.3+5.3.0-patched",
+          "timestamp": "2023-02-07T12:57:51+08:00",
+          "tree_id": "d64ae472c32ee3253ccc81923629d2a0de58fd59",
+          "url": "https://github.com/f1shl3gs/vertex/commit/eca3fb2ac99c3989e28433c3b84f2379af651b44"
+        },
+        "date": 1675747065777,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1023,
             "unit": "s"
           },
           {
