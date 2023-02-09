0b1a098bfc5349824145c9b3de58b37c08d943d2
github-action-benchmark <github@users.noreply.github.com>
2023-02-09 14:00:38 +0000

    add metrics (customSmallerIsBetter) benchmark result for e89e00086db989d461f75d93cbb1535c0d3154b5

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675951230685,
+  "lastUpdate": 1675951238752,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -1973,6 +1973,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 304.85,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e89e00086db989d461f75d93cbb1535c0d3154b5",
+          "message": "Merge pull request #750 from f1shl3gs/migrate_sources_to_configurable\n\nMigrate sources to configurable",
+          "timestamp": "2023-02-09T21:55:12+08:00",
+          "tree_id": "94f1d0599f8f9787521f98b7c59af64b0dc0163b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/e89e00086db989d461f75d93cbb1535c0d3154b5"
+        },
+        "date": 1675951230607,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 91.03,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 188.68,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 305.93,
             "unit": "ns/op"
           }
         ]
