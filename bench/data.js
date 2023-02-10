98a9f67a0047168aa3f2fb61d3da81bfe1ce563d
github-action-benchmark <github@users.noreply.github.com>
2023-02-10 14:24:33 +0000

    add vertex (customSmallerIsBetter) benchmark result for 4ca35e91a552411e3429e4c035d64f42f97924ac

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676038756689,
+  "lastUpdate": 1676039073476,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -10371,6 +10371,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1170.5,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ca35e91a552411e3429e4c035d64f42f97924ac",
+          "message": "Merge pull request #756 from f1shl3gs/remove_generate_config\n\nremove Trait GenerateConfig",
+          "timestamp": "2023-02-10T21:58:01+08:00",
+          "tree_id": "ff00453f70b0144a42bcf071408f6f107cd0a849",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4ca35e91a552411e3429e4c035d64f42f97924ac"
+        },
+        "date": 1676039062789,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 43027000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1399000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 25.311,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/1",
+            "value": 65.162,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 213.69,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 502.8,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1420.3,
             "unit": "ns/op"
           }
         ]
