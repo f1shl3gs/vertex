6b08743fe21febf2a962897f378f41ca39571ff6
github-action-benchmark <github@users.noreply.github.com>
2023-02-11 10:11:23 +0000

    add metrics (customSmallerIsBetter) benchmark result for 423ccd5d5325e5a2d16043ddc49dda62e5f06cd7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676105751891,
+  "lastUpdate": 1676110283644,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2721,6 +2721,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 285.72,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "423ccd5d5325e5a2d16043ddc49dda62e5f06cd7",
+          "message": "Merge pull request #759 from f1shl3gs/remove_provider_type\n\nreplace provider_type with NamedComponent",
+          "timestamp": "2023-02-11T18:07:28+08:00",
+          "tree_id": "47d553ed5328bd1e7402e72fa6302ac2b291a4ef",
+          "url": "https://github.com/f1shl3gs/vertex/commit/423ccd5d5325e5a2d16043ddc49dda62e5f06cd7"
+        },
+        "date": 1676110274800,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 98.231,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 176.2,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 285.83,
             "unit": "ns/op"
           }
         ]
