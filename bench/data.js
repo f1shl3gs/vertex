2e9dbb119314802ee7171ff64018ca1b59df4625
github-action-benchmark <github@users.noreply.github.com>
2023-02-11 08:36:10 +0000

    add condition (customSmallerIsBetter) benchmark result for c34259ce8a4c97d42897c9567bc370fc79c9e0b8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676104531027,
+  "lastUpdate": 1676104570573,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -4634,6 +4634,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.715,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c34259ce8a4c97d42897c9567bc370fc79c9e0b8",
+          "message": "Merge pull request #758 from f1shl3gs/style\n\nStyle",
+          "timestamp": "2023-02-11T16:31:27+08:00",
+          "tree_id": "9908a962c8bdda4d9b29233be420feac2d929ea1",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c34259ce8a4c97d42897c9567bc370fc79c9e0b8"
+        },
+        "date": 1676104562547,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.124,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.332,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.755,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.369,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 53.745,
             "unit": "ns/op"
           }
         ]
