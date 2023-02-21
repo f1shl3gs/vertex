e7a288421837267b35e17a433c72c9bb0fc7e4b6
github-action-benchmark <github@users.noreply.github.com>
2023-02-21 11:15:52 +0000

    add condition (customSmallerIsBetter) benchmark result for 0dbaa6e682e424ffbf3b3060670e7afd750bdb57

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676978150982,
+  "lastUpdate": 1676978152210,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6057,6 +6057,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 62.043,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0dbaa6e682e424ffbf3b3060670e7afd750bdb57",
+          "message": "Merge pull request #775 from f1shl3gs/dependabot/cargo/tokio-stream-0.1.12\n\nchore(deps): bump tokio-stream from 0.1.11 to 0.1.12",
+          "timestamp": "2023-02-21T19:11:23+08:00",
+          "tree_id": "fc727c05739c2eec856a9a74e7ae4f726ade9bc6",
+          "url": "https://github.com/f1shl3gs/vertex/commit/0dbaa6e682e424ffbf3b3060670e7afd750bdb57"
+        },
+        "date": 1676978142747,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 24.2,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.412,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.578,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.704,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 52.604,
             "unit": "ns/op"
           }
         ]
