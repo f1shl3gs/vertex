2b93798a11d04b52f69ac79db599be3c5ff3337d
github-action-benchmark <github@users.noreply.github.com>
2023-02-26 09:02:07 +0000

    add Build (customSmallerIsBetter) benchmark result for 07132a399a2ad9a171637b8ddf36c2bdcf2261f3

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677401186791,
+  "lastUpdate": 1677402127112,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -14729,6 +14729,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 56367.4,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07132a399a2ad9a171637b8ddf36c2bdcf2261f3",
+          "message": "update humanize to revision bf61d1d7a8cf282ce95017e98bb0b29c4dd0d98a (#789)",
+          "timestamp": "2023-02-26T16:40:54+08:00",
+          "tree_id": "6258575697fad4862f2e7651c44810184ff76d9a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/07132a399a2ad9a171637b8ddf36c2bdcf2261f3"
+        },
+        "date": 1677402118106,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1051,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 56364.9,
             "unit": "KiB"
           }
         ]
