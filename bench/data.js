989e234e126b9b319864bb5b5a39f7d9d924adec
github-action-benchmark <github@users.noreply.github.com>
2023-02-28 16:43:20 +0000

    add metrics (customSmallerIsBetter) benchmark result for 44050604cdce1315dfd7cd2002bd273e3ef3c448

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677602587013,
+  "lastUpdate": 1677602600791,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -4722,6 +4722,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 368.27,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "44050604cdce1315dfd7cd2002bd273e3ef3c448",
+          "message": "Merge pull request #795 from f1shl3gs/dependabot/cargo/prost-types-0.11.8\n\nchore(deps): bump prost-types from 0.11.6 to 0.11.8",
+          "timestamp": "2023-03-01T00:39:21+08:00",
+          "tree_id": "9cd6730b27e349084ad7cf340ee0ca43b1a27a74",
+          "url": "https://github.com/f1shl3gs/vertex/commit/44050604cdce1315dfd7cd2002bd273e3ef3c448"
+        },
+        "date": 1677602592104,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 91.153,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 186.33,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 310.39,
             "unit": "ns/op"
           }
         ]
