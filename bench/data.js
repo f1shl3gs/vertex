eb77561b5a78a3f78561eb5dc01de00c16dd46ec
github-action-benchmark <github@users.noreply.github.com>
2023-03-02 10:43:44 +0000

    add metrics (customSmallerIsBetter) benchmark result for 5396ccc75f5efa2ac49f8e7a405a56b569273181

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677701795314,
+  "lastUpdate": 1677753824514,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5101,6 +5101,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 315.63,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5396ccc75f5efa2ac49f8e7a405a56b569273181",
+          "message": "chore(deps): bump tokio from 1.25.0 to 1.26.0 (#802)\n\nBumps [tokio](https://github.com/tokio-rs/tokio) from 1.25.0 to 1.26.0.\r\n- [Release notes](https://github.com/tokio-rs/tokio/releases)\r\n- [Commits](https://github.com/tokio-rs/tokio/compare/tokio-1.25.0...tokio-1.26.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tokio\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-02T18:40:00+08:00",
+          "tree_id": "79b168821f326758a8d7ccb014b3346ae261c504",
+          "url": "https://github.com/f1shl3gs/vertex/commit/5396ccc75f5efa2ac49f8e7a405a56b569273181"
+        },
+        "date": 1677753815946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 91.875,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 186.33,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 312.22,
             "unit": "ns/op"
           }
         ]
