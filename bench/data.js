67d58a87a07b3ace7c80f26b240970745b0b6ea1
github-action-benchmark <github@users.noreply.github.com>
2023-03-02 17:38:52 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 4a263b2336237aa7bf27f40feb9e4cb145c578ce

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677778690951,
+  "lastUpdate": 1677778732647,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2261,6 +2261,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 718600,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4a263b2336237aa7bf27f40feb9e4cb145c578ce",
+          "message": "add grpc_check source (#805)\n\n* add grpc_check source",
+          "timestamp": "2023-03-03T01:34:24+08:00",
+          "tree_id": "2c83ad75eab3329ec1089d40cea9847498b4487f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4a263b2336237aa7bf27f40feb9e4cb145c578ce"
+        },
+        "date": 1677778721903,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 887530,
             "unit": "ns/op"
           }
         ]
