33c02abd847d66b7c6ab6ffec0e02fdd2b105d15
github-action-benchmark <github@users.noreply.github.com>
2023-03-05 14:17:46 +0000

    add Build (customSmallerIsBetter) benchmark result for 6736545290fe426c42cd2a3afe5b076939136fbe

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678024877929,
+  "lastUpdate": 1678025865988,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -17875,6 +17875,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 57496.4,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6736545290fe426c42cd2a3afe5b076939136fbe",
+          "message": "Merge pull request #809 from f1shl3gs/fix_kafka_sink_config\n\nremove non-support option for rdkafka",
+          "timestamp": "2023-03-05T21:56:27+08:00",
+          "tree_id": "35555aee57c56838ce3111c1725f36bd0ee785c0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/6736545290fe426c42cd2a3afe5b076939136fbe"
+        },
+        "date": 1678025856252,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1063,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 57492.4,
             "unit": "KiB"
           }
         ]
