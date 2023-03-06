1342a555225f837ef301a449f43149c3f4ac4202
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 07:09:28 +0000

    add metrics (customSmallerIsBetter) benchmark result for 1cc5f654fdb753e1103f0f30a1cc1ead1812632e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678086542862,
+  "lastUpdate": 1678086568043,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6014,6 +6014,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 371.58,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1cc5f654fdb753e1103f0f30a1cc1ead1812632e",
+          "message": "Merge pull request #810 from f1shl3gs/dependabot/cargo/typetag-0.2.6\n\nchore(deps): bump typetag from 0.2.5 to 0.2.6",
+          "timestamp": "2023-03-06T14:56:56+08:00",
+          "tree_id": "35f91eca78f0524ac9fadfebe02ce03be4db1095",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1cc5f654fdb753e1103f0f30a1cc1ead1812632e"
+        },
+        "date": 1678086557629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 103.18,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 231.87,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 371.56,
             "unit": "ns/op"
           }
         ]
