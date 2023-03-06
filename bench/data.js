a19996d985234cfb81266518c56e0dae88e61245
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 21:18:52 +0000

    add vertex (customSmallerIsBetter) benchmark result for 1eff6f7890a7d69c8be0638112ae8d3695931efb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678136418554,
+  "lastUpdate": 1678137532210,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -25611,6 +25611,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 992.09,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1eff6f7890a7d69c8be0638112ae8d3695931efb",
+          "message": "Merge pull request #820 from f1shl3gs/kafka_metrics\n\nKafka metrics",
+          "timestamp": "2023-03-07T04:54:40+08:00",
+          "tree_id": "29a5747a78fb2e4f7ccda9ccfc1ca9c4d3bf2d7e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1eff6f7890a7d69c8be0638112ae8d3695931efb"
+        },
+        "date": 1678137520850,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 34492000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1075100,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 21.703,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/1",
+            "value": 58.192,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 189.78,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 435.8,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1202.9,
             "unit": "ns/op"
           }
         ]
