32dc048240e0857282f0515098c968bd4f8c9dfe
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 07:43:42 +0000

    add metrics (customSmallerIsBetter) benchmark result for 18b796ba3189cb4a3434cbb7e4213a1641029538

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678088578117,
+  "lastUpdate": 1678088622364,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6082,6 +6082,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 371.56,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "18b796ba3189cb4a3434cbb7e4213a1641029538",
+          "message": "Merge pull request #812 from f1shl3gs/dependabot/cargo/serde_path_to_error-0.1.10\n\nchore(deps): bump serde_path_to_error from 0.1.9 to 0.1.10",
+          "timestamp": "2023-03-06T15:39:04+08:00",
+          "tree_id": "6cccdd080d9b03de242389b5ac1eee265ae70697",
+          "url": "https://github.com/f1shl3gs/vertex/commit/18b796ba3189cb4a3434cbb7e4213a1641029538"
+        },
+        "date": 1678088611625,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 103.19,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 220.29,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 359.22,
             "unit": "ns/op"
           }
         ]
