035eb8806ae0d0613fcfe2aaf42e7ca362479989
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 06:58:07 +0000

    add metrics (customSmallerIsBetter) benchmark result for 31536571cea1e5172b94da6bb33b94f977f3a2ac

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678026040645,
+  "lastUpdate": 1678085887618,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5577,6 +5577,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 380.91,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "31536571cea1e5172b94da6bb33b94f977f3a2ac",
+          "message": "Merge pull request #818 from f1shl3gs/dependabot/cargo/inventory-0.3.4\n\nchore(deps): bump inventory from 0.3.3 to 0.3.4",
+          "timestamp": "2023-03-06T14:54:10+08:00",
+          "tree_id": "f6ac622cf5404d65b39e187252e0d69887b40ccd",
+          "url": "https://github.com/f1shl3gs/vertex/commit/31536571cea1e5172b94da6bb33b94f977f3a2ac"
+        },
+        "date": 1678085878178,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 95.95,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 177.38,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 290.72,
             "unit": "ns/op"
           }
         ]
