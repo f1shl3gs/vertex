14c31c78fe8633888af5f7f4ee4e275ea18e5ec1
github-action-benchmark <github@users.noreply.github.com>
2023-03-10 18:33:23 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 4e251377c30f67396d087c40cf3c8c559b7b7761

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678473141764,
+  "lastUpdate": 1678473203090,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2870,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 718280,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4e251377c30f67396d087c40cf3c8c559b7b7761",
+          "message": "Merge pull request #829 from f1shl3gs/batch_spans\n\nbatch spans",
+          "timestamp": "2023-03-11T02:28:14+08:00",
+          "tree_id": "1db2d41472b0dc5bebc731a813f4c017e7ff68e5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4e251377c30f67396d087c40cf3c8c559b7b7761"
+        },
+        "date": 1678473191873,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 861370,
             "unit": "ns/op"
           }
         ]
