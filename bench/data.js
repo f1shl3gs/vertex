a2ec58bb8d225fd811f6b2d989eea0cae6867761
github-action-benchmark <github@users.noreply.github.com>
2023-03-10 05:45:59 +0000

    add vertex (customSmallerIsBetter) benchmark result for 42668a253ebe1ccc762938caa31c5349a3f1b488

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678426187752,
+  "lastUpdate": 1678427159592,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -26981,6 +26981,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1160.9,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "42668a253ebe1ccc762938caa31c5349a3f1b488",
+          "message": "Merge pull request #827 from f1shl3gs/dependabot/cargo/libc-0.2.140\n\nchore(deps): bump libc from 0.2.139 to 0.2.140",
+          "timestamp": "2023-03-10T13:24:50+08:00",
+          "tree_id": "1b052d768bbbf595ab27c8dbf07d2f7c7a39af90",
+          "url": "https://github.com/f1shl3gs/vertex/commit/42668a253ebe1ccc762938caa31c5349a3f1b488"
+        },
+        "date": 1678427151090,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 32367000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1038800,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 21.625,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/1",
+            "value": 59.751,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 180.97,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 420.35,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1221.7,
             "unit": "ns/op"
           }
         ]
