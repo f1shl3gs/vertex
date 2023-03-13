88827887e08c5d68b0f96f3b0a3fb8e9aa1f50aa
github-action-benchmark <github@users.noreply.github.com>
2023-03-13 16:34:10 +0000

    add metrics (customSmallerIsBetter) benchmark result for 614f904831ee93e487a9bf901cfe42aa5f7ffca8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678725214479,
+  "lastUpdate": 1678725250606,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -7481,6 +7481,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 410.1,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "614f904831ee93e487a9bf901cfe42aa5f7ffca8",
+          "message": "Misc (#839)\n\n* refine tracing limit",
+          "timestamp": "2023-03-14T00:29:17+08:00",
+          "tree_id": "a312b4425ab1d5f48a40eb40342ed84ab4a84252",
+          "url": "https://github.com/f1shl3gs/vertex/commit/614f904831ee93e487a9bf901cfe42aa5f7ffca8"
+        },
+        "date": 1678725239384,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 109.04,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 236.53,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 375.26,
             "unit": "ns/op"
           }
         ]
