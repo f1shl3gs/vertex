330fbe7116b7ccd87dcb1932b30b3636fbdf3b1c
github-action-benchmark <github@users.noreply.github.com>
2023-03-16 16:39:17 +0000

    add Build (customSmallerIsBetter) benchmark result for 961bc6cdbf871df94636fce4ae67cb8bcadacd00

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678984304818,
+  "lastUpdate": 1678984757896,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -25185,6 +25185,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 57359.3,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "961bc6cdbf871df94636fce4ae67cb8bcadacd00",
+          "message": "Merge pull request #841 from f1shl3gs/dependabot/cargo/quote-1.0.26\n\nchore(deps): bump quote from 1.0.23 to 1.0.26",
+          "timestamp": "2023-03-17T00:10:15+08:00",
+          "tree_id": "934dc433a09ba3fe48077545ab19934a7a4c4543",
+          "url": "https://github.com/f1shl3gs/vertex/commit/961bc6cdbf871df94636fce4ae67cb8bcadacd00"
+        },
+        "date": 1678984746432,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1444,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 57340.8,
             "unit": "KiB"
           }
         ]
