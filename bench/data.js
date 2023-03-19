edbe86fd7129e3cbed65518369519a4b6cc5dd95
github-action-benchmark <github@users.noreply.github.com>
2023-03-19 16:29:16 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 09ccfc4d9125cc38446d16273feb2a72287fdc32

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679243345337,
+  "lastUpdate": 1679243356823,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -3421,6 +3421,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 715570,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09ccfc4d9125cc38446d16273feb2a72287fdc32",
+          "message": "Update toolchain to 1.68.0 (#848)\n\n* update rust to 1.68.0\r\n\r\n* replace tokio::pin! with std::pin::pin!\r\n\r\n* pin inferno to 0.11.15\r\n\r\n* remove unused dev-dependencies",
+          "timestamp": "2023-03-20T00:25:27+08:00",
+          "tree_id": "6c2c9330bbbe46597e56509ec6f49220cce168f0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/09ccfc4d9125cc38446d16273feb2a72287fdc32"
+        },
+        "date": 1679243347001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 719760,
             "unit": "ns/op"
           }
         ]
