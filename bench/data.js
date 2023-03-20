541b3ef8d49f2356f01a042bdbe384ff0621ed5d
github-action-benchmark <github@users.noreply.github.com>
2023-03-20 08:39:39 +0000

    add metrics (customSmallerIsBetter) benchmark result for 2d5416170f80465bfe4970d2a953f05ef6e7e2fa

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679301577987,
+  "lastUpdate": 1679301579736,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -8501,6 +8501,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 300.49,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d5416170f80465bfe4970d2a953f05ef6e7e2fa",
+          "message": "Merge pull request #853 from f1shl3gs/dependabot/cargo/async-recursion-1.0.4\n\nchore(deps): bump async-recursion from 1.0.2 to 1.0.4",
+          "timestamp": "2023-03-20T16:34:47+08:00",
+          "tree_id": "5846ccfacbe921d54a5e6408b2c050bcdcc45320",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2d5416170f80465bfe4970d2a953f05ef6e7e2fa"
+        },
+        "date": 1679301568054,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 114.2,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 237.59,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 380.51,
             "unit": "ns/op"
           }
         ]
