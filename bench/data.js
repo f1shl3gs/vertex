5485d567feb7fa0b230ce98e84cc97fff7a06b73
github-action-benchmark <github@users.noreply.github.com>
2023-03-22 03:53:05 +0000

    add Build (customSmallerIsBetter) benchmark result for 571b0d6a321c8ab5aee0cd93561cbc39932ffd52

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679457077287,
+  "lastUpdate": 1679457184993,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -29055,6 +29055,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 57756.3,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "571b0d6a321c8ab5aee0cd93561cbc39932ffd52",
+          "message": "Top (#867)\n\n* move alloc features to main.rs\r\n\r\n* jemalloc-extension require feature jemalloc to compile",
+          "timestamp": "2023-03-22T11:30:47+08:00",
+          "tree_id": "4d63719eb3184483fc7d7bba97fb40be0b936f8a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/571b0d6a321c8ab5aee0cd93561cbc39932ffd52"
+        },
+        "date": 1679457174639,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1104,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 57769.3,
             "unit": "KiB"
           }
         ]
