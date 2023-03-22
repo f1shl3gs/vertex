32f5abe0c3f8422153d969a6e90ed7a48914dfbd
github-action-benchmark <github@users.noreply.github.com>
2023-03-22 06:10:01 +0000

    add Build (customSmallerIsBetter) benchmark result for effa985a9c988d19db3eea1724c56b1898e02242

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679465323473,
+  "lastUpdate": 1679465401507,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -29734,6 +29734,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 57775.2,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "effa985a9c988d19db3eea1724c56b1898e02242",
+          "message": "Merge pull request #869 from f1shl3gs/dependabot/cargo/syn-2.0.5\n\nchore(deps): bump syn from 2.0.4 to 2.0.5",
+          "timestamp": "2023-03-22T13:47:44+08:00",
+          "tree_id": "291e4bde0e172eca765d2a5d884df1c8bdf001b3",
+          "url": "https://github.com/f1shl3gs/vertex/commit/effa985a9c988d19db3eea1724c56b1898e02242"
+        },
+        "date": 1679465392239,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1107,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 57792.9,
             "unit": "KiB"
           }
         ]
