f3bb0d88dbac74889e77ce9a5932a3387667b7a3
github-action-benchmark <github@users.noreply.github.com>
2023-03-22 08:50:07 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 4f457f4a497aec5889166a2e474393f4ca008949

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679474997307,
+  "lastUpdate": 1679475007347,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -3972,6 +3972,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 718520,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4f457f4a497aec5889166a2e474393f4ca008949",
+          "message": "Simplify tracing limit (#873)\n\n* simplify tracing-limit\r\n\r\n* refine no metric log",
+          "timestamp": "2023-03-22T16:46:21+08:00",
+          "tree_id": "c3b8d4cbbf8cdb53f6bca49aed7f479c06fea41a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4f457f4a497aec5889166a2e474393f4ca008949"
+        },
+        "date": 1679474996754,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 711510,
             "unit": "ns/op"
           }
         ]
