571b0d6a321c8ab5aee0cd93561cbc39932ffd52
f1shl3gs <fishlegs.engerman@gmail.com>
2023-03-22 11:30:47 +0800

    Top (#867)
    * move alloc features to main.rs
    
    * jemalloc-extension require feature jemalloc to compile

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use launch::RootCommand;
 
 extern crate vertex;
 
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(feature = "jemalloc")]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 fn main() {
     let cmd: RootCommand = argh::from_env();
 
