0164c3c1ed3624af4025a6eeab02664bb85c1b5e
github-action-benchmark <github@users.noreply.github.com>
2023-03-24 07:15:17 +0000

    add vertex (customSmallerIsBetter) benchmark result for 32932fb18c1ee897cb9981e963c8909351e182da

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679641287511,
+  "lastUpdate": 1679642117732,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -37926,6 +37926,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1390.2,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "32932fb18c1ee897cb9981e963c8909351e182da",
+          "message": "move tags insert bench to lib/event (#882)",
+          "timestamp": "2023-03-24T14:55:17+08:00",
+          "tree_id": "c3f6e1ce87bfac41cb382c66de6ec2739c0cfe3f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/32932fb18c1ee897cb9981e963c8909351e182da"
+        },
+        "date": 1679642107796,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 31879000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1074600,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 19.543,
             "unit": "ns/op"
           }
         ]
