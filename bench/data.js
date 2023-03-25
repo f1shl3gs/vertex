fbb68773a7d6fb6af0efc440f88da10ff83d00c6
github-action-benchmark <github@users.noreply.github.com>
2023-03-25 06:43:02 +0000

    add metrics (customSmallerIsBetter) benchmark result for 279f4a40e0d0fabfd243ad45ca7ee64c85e44105

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679673714377,
+  "lastUpdate": 1679726582093,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -9793,6 +9793,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 295.06,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "279f4a40e0d0fabfd243ad45ca7ee64c85e44105",
+          "message": "remove some unnecessary IntervalStream (#888)\n\n* replace IntervalStream with tokio::time::Interval\r\n\r\n* remove default_false",
+          "timestamp": "2023-03-25T14:39:02+08:00",
+          "tree_id": "b0ea803b0d50e2c9a6006363068f274d1a3175e4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/279f4a40e0d0fabfd243ad45ca7ee64c85e44105"
+        },
+        "date": 1679726571619,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 95.437,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 188.24,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 291.49,
             "unit": "ns/op"
           }
         ]
