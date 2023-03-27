329eaeb6ff27c2517711526cbd85108e468954c4
github-action-benchmark <github@users.noreply.github.com>
2023-03-27 09:06:45 +0000

    add condition (customSmallerIsBetter) benchmark result for f3d4ceb979b13668a3160d7d0f10de9f3ca9c5f0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679907999481,
+  "lastUpdate": 1679908005393,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -17416,6 +17416,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 61.555,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3d4ceb979b13668a3160d7d0f10de9f3ca9c5f0",
+          "message": "chore(deps): bump syn from 2.0.8 to 2.0.10 (#892)\n\nBumps [syn](https://github.com/dtolnay/syn) from 2.0.8 to 2.0.10.\r\n- [Release notes](https://github.com/dtolnay/syn/releases)\r\n- [Commits](https://github.com/dtolnay/syn/compare/2.0.8...2.0.10)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: syn\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-27T17:01:26+08:00",
+          "tree_id": "9e51565ce309de01a1ac5a602e3c451b81551c4e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f3d4ceb979b13668a3160d7d0f10de9f3ca9c5f0"
+        },
+        "date": 1679907996138,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 28.16,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 27.538,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 27.054,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 37.505,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 61.322,
             "unit": "ns/op"
           }
         ]
