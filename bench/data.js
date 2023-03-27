3def709310a22087e9f1af92e7f7c2ead544c9df
github-action-benchmark <github@users.noreply.github.com>
2023-03-27 13:36:29 +0000

    add condition (customSmallerIsBetter) benchmark result for a9054b7c71b34f11de3f05bf88d9f7a1d62fb85e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679924179040,
+  "lastUpdate": 1679924188981,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -17855,6 +17855,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 53.757,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a9054b7c71b34f11de3f05bf88d9f7a1d62fb85e",
+          "message": "chore(deps): bump indexmap from 1.9.2 to 1.9.3 (#896)\n\nBumps [indexmap](https://github.com/bluss/indexmap) from 1.9.2 to 1.9.3.\r\n- [Release notes](https://github.com/bluss/indexmap/releases)\r\n- [Changelog](https://github.com/bluss/indexmap/blob/master/RELEASES.md)\r\n- [Commits](https://github.com/bluss/indexmap/compare/1.9.2...1.9.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: indexmap\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-27T21:28:59+08:00",
+          "tree_id": "2200e231a74e9ed31920bbe6fc1c7fda42048104",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a9054b7c71b34f11de3f05bf88d9f7a1d62fb85e"
+        },
+        "date": 1679924176380,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 29.33,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 28.747,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 29.864,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 40.149,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 60.796,
             "unit": "ns/op"
           }
         ]
