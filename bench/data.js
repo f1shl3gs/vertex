8dd92a849a2f6652698b003ac064c49c1020a39e
github-action-benchmark <github@users.noreply.github.com>
2023-03-29 08:23:58 +0000

    add event (customSmallerIsBetter) benchmark result for 365253c422d0cda1c191e215ec5fc9ee9190fc7f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680078193006,
+  "lastUpdate": 1680078238244,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -44078,6 +44078,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1215.2,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "365253c422d0cda1c191e215ec5fc9ee9190fc7f",
+          "message": "remove serde_xml_rs cause xml-rs is not maintained anymore (#903)\n\n* remove serde_xml_rs\r\n\r\n* style",
+          "timestamp": "2023-03-29T16:19:08+08:00",
+          "tree_id": "e8e065311fe791e9a140f259116dd0c2db498f53",
+          "url": "https://github.com/f1shl3gs/vertex/commit/365253c422d0cda1c191e215ec5fc9ee9190fc7f"
+        },
+        "date": 1680078228790,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 61.727,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 185.48,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 430.94,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1251,
             "unit": "ns/op"
           }
         ]
