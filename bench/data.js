f0d997996241cfd430fc2ed33bd2286e59b5b449
github-action-benchmark <github@users.noreply.github.com>
2023-03-29 09:51:08 +0000

    add vertex (customSmallerIsBetter) benchmark result for fef21d7a605ee4fe1a0c946070184ba8142a1ff8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680082264205,
+  "lastUpdate": 1680083468278,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -43548,6 +43548,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 24.167,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fef21d7a605ee4fe1a0c946070184ba8142a1ff8",
+          "message": "move atty to dependencies (#904)",
+          "timestamp": "2023-03-29T17:25:31+08:00",
+          "tree_id": "0caa9f5d20dc381dd9b4981d4d611b9234b03ae5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fef21d7a605ee4fe1a0c946070184ba8142a1ff8"
+        },
+        "date": 1680083456388,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 44303000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1465000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 22.746,
             "unit": "ns/op"
           }
         ]
