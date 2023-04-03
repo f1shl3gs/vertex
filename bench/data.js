fc3c74d5eaf6683d69ceced0829ebc4312aa66b9
github-action-benchmark <github@users.noreply.github.com>
2023-04-03 06:03:37 +0000

    add condition (customSmallerIsBetter) benchmark result for 979e54b3a672e76bc6cc1c75479b4a4fecdda1cb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680501760125,
+  "lastUpdate": 1680501817923,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -19610,6 +19610,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.313,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "979e54b3a672e76bc6cc1c75479b4a4fecdda1cb",
+          "message": "chore(deps): bump proc-macro2 from 1.0.54 to 1.0.55 (#916)\n\nBumps [proc-macro2](https://github.com/dtolnay/proc-macro2) from 1.0.54 to 1.0.55.\r\n- [Release notes](https://github.com/dtolnay/proc-macro2/releases)\r\n- [Commits](https://github.com/dtolnay/proc-macro2/compare/1.0.54...1.0.55)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: proc-macro2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-03T13:57:58+08:00",
+          "tree_id": "c385ab6dd69bcabbaeae0960fe1c700a6c92430f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/979e54b3a672e76bc6cc1c75479b4a4fecdda1cb"
+        },
+        "date": 1680501807555,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 27.863,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 27.652,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 29.826,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 40.908,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 64.131,
             "unit": "ns/op"
           }
         ]
