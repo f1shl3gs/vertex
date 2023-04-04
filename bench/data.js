6384ce64f06e397decac84f9fe92615308db0db3
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 17:18:08 +0000

    add vertex (customSmallerIsBetter) benchmark result for ee5c9f79e835f1fc53c961bd1e8772b811607ce8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680628652990,
+  "lastUpdate": 1680628688100,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -46156,6 +46156,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 20.843,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee5c9f79e835f1fc53c961bd1e8772b811607ce8",
+          "message": "chore(deps): bump tonic-build from 0.9.0 to 0.9.1 (#922)\n\nBumps [tonic-build](https://github.com/hyperium/tonic) from 0.9.0 to 0.9.1.\r\n- [Release notes](https://github.com/hyperium/tonic/releases)\r\n- [Changelog](https://github.com/hyperium/tonic/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/hyperium/tonic/compare/v0.9.0...v0.9.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tonic-build\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-05T00:52:02+08:00",
+          "tree_id": "1076e274ee567aa501ea01cb3d6ab762ef0fc143",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ee5c9f79e835f1fc53c961bd1e8772b811607ce8"
+        },
+        "date": 1680628676091,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 48401000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1668100,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 24.499,
             "unit": "ns/op"
           }
         ]
