62da52abf9ee747e370565ef3a42f73186f7a729
github-action-benchmark <github@users.noreply.github.com>
2023-04-05 06:27:49 +0000

    add vertex (customSmallerIsBetter) benchmark result for 3514f5c1bd678bc3873388558d2a8f32dadf34a9

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680675247476,
+  "lastUpdate": 1680676069012,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -47099,6 +47099,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 25.752,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3514f5c1bd678bc3873388558d2a8f32dadf34a9",
+          "message": "chore(deps): bump libc from 0.2.140 to 0.2.141 (#924)\n\nBumps [libc](https://github.com/rust-lang/libc) from 0.2.140 to 0.2.141.\r\n- [Release notes](https://github.com/rust-lang/libc/releases)\r\n- [Commits](https://github.com/rust-lang/libc/compare/0.2.140...0.2.141)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: libc\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-05T14:07:35+08:00",
+          "tree_id": "23fc79613d1c920bfbb17ef740c6377459d568cc",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3514f5c1bd678bc3873388558d2a8f32dadf34a9"
+        },
+        "date": 1680676059476,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 38217000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1037600,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 21.442,
             "unit": "ns/op"
           }
         ]
