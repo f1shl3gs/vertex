de97d6664246b71a4853b6e20c221798e24f144a
github-action-benchmark <github@users.noreply.github.com>
2023-04-06 10:46:45 +0000

    add metrics (customSmallerIsBetter) benchmark result for 14bc0939818748f9b8313c7ebbe5dc8d488566ab

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680676433048,
+  "lastUpdate": 1680778005180,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -11969,6 +11969,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 318.72,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "14bc0939818748f9b8313c7ebbe5dc8d488566ab",
+          "message": "chore(deps): bump async-stream from 0.3.4 to 0.3.5 (#925)\n\nBumps [async-stream](https://github.com/tokio-rs/async-stream) from 0.3.4 to 0.3.5.\r\n- [Release notes](https://github.com/tokio-rs/async-stream/releases)\r\n- [Commits](https://github.com/tokio-rs/async-stream/compare/v0.3.4...v0.3.5)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: async-stream\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-06T18:43:04+08:00",
+          "tree_id": "90c9c692e731ea064f23628e44e5de811b0d5fb3",
+          "url": "https://github.com/f1shl3gs/vertex/commit/14bc0939818748f9b8313c7ebbe5dc8d488566ab"
+        },
+        "date": 1680777994993,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 97.088,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 188.81,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 291.96,
             "unit": "ns/op"
           }
         ]
