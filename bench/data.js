4ed740a8772da473387dada2b64f7fe87061b7b4
github-action-benchmark <github@users.noreply.github.com>
2023-04-07 09:26:01 +0000

    add Build (customSmallerIsBetter) benchmark result for d9a0dc0bc0da614ed58030f631cb510429ae44cb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680859376766,
+  "lastUpdate": 1680859561139,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -39036,6 +39036,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 57309.6,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9a0dc0bc0da614ed58030f631cb510429ae44cb",
+          "message": "chore(deps): bump serde_yaml from 0.9.20 to 0.9.21 (#927)\n\nBumps [serde_yaml](https://github.com/dtolnay/serde-yaml) from 0.9.20 to 0.9.21.\r\n- [Release notes](https://github.com/dtolnay/serde-yaml/releases)\r\n- [Commits](https://github.com/dtolnay/serde-yaml/compare/0.9.20...0.9.21)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde_yaml\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-07T17:03:26+08:00",
+          "tree_id": "68291379313630ad0f03538f432f9936f1d4c4d5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/d9a0dc0bc0da614ed58030f631cb510429ae44cb"
+        },
+        "date": 1680859550637,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1139,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 57310.3,
             "unit": "KiB"
           }
         ]
