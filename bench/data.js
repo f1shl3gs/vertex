a668bd0000df09e725ef4bf94135b2063f677b84
github-action-benchmark <github@users.noreply.github.com>
2023-04-11 06:40:18 +0000

    add event (customSmallerIsBetter) benchmark result for faf18491ed00929779a1d109de9e55fa4e1ba069

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681195217123,
+  "lastUpdate": 1681195218337,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -51078,6 +51078,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1444.9,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "faf18491ed00929779a1d109de9e55fa4e1ba069",
+          "message": "Remove rdkafka (#933)\n\n* remove dep:rdkafka\r\n\r\n* impl with rskafka\r\n\r\n* fix kafka tests",
+          "timestamp": "2023-04-11T14:35:33+08:00",
+          "tree_id": "cda40ef1a8178406f79eb112a20705f7bd40f253",
+          "url": "https://github.com/f1shl3gs/vertex/commit/faf18491ed00929779a1d109de9e55fa4e1ba069"
+        },
+        "date": 1681195208337,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 62.114,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 191.49,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 433.48,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1241.8,
             "unit": "ns/op"
           }
         ]
