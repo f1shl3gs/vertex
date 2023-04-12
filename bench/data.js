8394a865f7b863aac3493ea58d99057b082e658e
github-action-benchmark <github@users.noreply.github.com>
2023-04-12 12:01:32 +0000

    add metrics (customSmallerIsBetter) benchmark result for afa39c122a3c488f53f5f810ee7c0dff4ee880b4

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681300875677,
+  "lastUpdate": 1681300892849,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -12746,6 +12746,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 325.91,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "afa39c122a3c488f53f5f810ee7c0dff4ee880b4",
+          "message": "chore(deps): bump socket2 from 0.5.1 to 0.5.2 (#935)\n\nBumps [socket2](https://github.com/rust-lang/socket2) from 0.5.1 to 0.5.2.\r\n- [Release notes](https://github.com/rust-lang/socket2/releases)\r\n- [Changelog](https://github.com/rust-lang/socket2/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/socket2/compare/v0.5.1...v0.5.2)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: socket2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-12T19:56:33+08:00",
+          "tree_id": "31238b481d4b3a366b3a208b3bdd2bb446b6a707",
+          "url": "https://github.com/f1shl3gs/vertex/commit/afa39c122a3c488f53f5f810ee7c0dff4ee880b4"
+        },
+        "date": 1681300880329,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 110.27,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 252.79,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 404.37,
             "unit": "ns/op"
           }
         ]
