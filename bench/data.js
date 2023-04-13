0556cad8c17ccb723813ee66fcb798ff7db07506
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 05:42:55 +0000

    add vertex (customSmallerIsBetter) benchmark result for feb35994e5b1a5d15ef9a7e9e35b3642e91da4e8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681363822222,
+  "lastUpdate": 1681364575527,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -50666,6 +50666,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 18.412,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "feb35994e5b1a5d15ef9a7e9e35b3642e91da4e8",
+          "message": "chore(deps): bump serde_json from 1.0.95 to 1.0.96 (#939)\n\nBumps [serde_json](https://github.com/serde-rs/json) from 1.0.95 to 1.0.96.\r\n- [Release notes](https://github.com/serde-rs/json/releases)\r\n- [Commits](https://github.com/serde-rs/json/compare/v1.0.95...v1.0.96)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde_json\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-13T13:24:23+08:00",
+          "tree_id": "a2cc7e88040f70afa0baa9c54ed3a43312089f6b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/feb35994e5b1a5d15ef9a7e9e35b3642e91da4e8"
+        },
+        "date": 1681364565054,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 28048000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 913000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 19.375,
             "unit": "ns/op"
           }
         ]
