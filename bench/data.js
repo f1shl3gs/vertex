f581b2d286ce1c64a6c2ab4b61018e9a34f18156
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 05:28:33 +0000

    add prometheus (customSmallerIsBetter) benchmark result for b13948e777788a1008666a6158a367501684e23f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681363690114,
+  "lastUpdate": 1681363713251,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5480,6 +5480,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 723950,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b13948e777788a1008666a6158a367501684e23f",
+          "message": "chore(deps): bump quick-xml from 0.28.1 to 0.28.2 (#938)\n\nBumps [quick-xml](https://github.com/tafia/quick-xml) from 0.28.1 to 0.28.2.\r\n- [Release notes](https://github.com/tafia/quick-xml/releases)\r\n- [Changelog](https://github.com/tafia/quick-xml/blob/master/Changelog.md)\r\n- [Commits](https://github.com/tafia/quick-xml/compare/v0.28.1...v0.28.2)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: quick-xml\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-13T13:24:32+08:00",
+          "tree_id": "b38c2fc501da42b83ee0836251a5fe4192ce4d7b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b13948e777788a1008666a6158a367501684e23f"
+        },
+        "date": 1681363703168,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 737230,
             "unit": "ns/op"
           }
         ]
