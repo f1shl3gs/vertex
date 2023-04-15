307acaf943c65634a6a0d7b0ebbe13ff2b99c33d
f1shl3gs <fishlegs.engerman@gmail.com>
2023-04-15 22:04:31 +0800

    impl http & simplify

--- src/sources/jaeger/grpc.rs
+++ src/sources/jaeger/grpc.rs
@@ -1,7 +1,6 @@
 use async_trait::async_trait;
 use event::Event;
 use framework::{Pipeline, ShutdownSignal};
-use futures_util::future::Shared;
 use futures_util::FutureExt;
 use jaeger::proto::collector_service_server::CollectorServiceServer;
 use jaeger::proto::{CollectorService, PostSpansRequest, PostSpansResponse};
@@ -38,7 +37,7 @@ impl CollectorService for JaegerCollector {
 
 pub(super) async fn serve(
     config: GrpcServerConfig,
-    shutdown: Shared<ShutdownSignal>,
+    shutdown: ShutdownSignal,
     output: Pipeline,
 ) -> framework::Result<()> {
     let service = CollectorServiceServer::new(JaegerCollector {
