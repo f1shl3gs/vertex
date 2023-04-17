cb763d1bda6a6b5532bf2b42399a7e81c74f7d0a
github-action-benchmark <github@users.noreply.github.com>
2023-04-17 05:31:05 +0000

    add condition (customSmallerIsBetter) benchmark result for 34a47bfedac2a5e3a2dcbf75749f9f02c73f5ff6

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681709458190,
+  "lastUpdate": 1681709465545,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -23354,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 51.38,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "34a47bfedac2a5e3a2dcbf75749f9f02c73f5ff6",
+          "message": "chore(deps): bump kube from 0.82.0 to 0.82.1 (#949)\n\nBumps [kube](https://github.com/kube-rs/kube) from 0.82.0 to 0.82.1.\r\n- [Release notes](https://github.com/kube-rs/kube/releases)\r\n- [Changelog](https://github.com/kube-rs/kube/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/kube-rs/kube/compare/0.82.0...0.82.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: kube\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-17T13:26:14+08:00",
+          "tree_id": "ed5fa3d429efb87876f28bfdabdc11f5c78c572d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/34a47bfedac2a5e3a2dcbf75749f9f02c73f5ff6"
+        },
+        "date": 1681709453985,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.793,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.43,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.245,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.844,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 51.105,
             "unit": "ns/op"
           }
         ]
