e0d3847e735279ba1e9a935728dde3db43012a2c
github-action-benchmark <github@users.noreply.github.com>
2023-04-17 05:30:01 +0000

    add condition (customSmallerIsBetter) benchmark result for 224505cfbfa4b9cd1cd3a7e12f7b4ac32d929d48

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681709389497,
+  "lastUpdate": 1681709401818,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -23237,6 +23237,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 61.632,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "224505cfbfa4b9cd1cd3a7e12f7b4ac32d929d48",
+          "message": "chore(deps): bump prost-build from 0.11.8 to 0.11.9 (#948)\n\nBumps [prost-build](https://github.com/tokio-rs/prost) from 0.11.8 to 0.11.9.\r\n- [Release notes](https://github.com/tokio-rs/prost/releases)\r\n- [Commits](https://github.com/tokio-rs/prost/compare/v0.11.8...v0.11.9)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: prost-build\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-17T13:25:19+08:00",
+          "tree_id": "666a25da2cc3281f962045cdc316fe1084c26707",
+          "url": "https://github.com/f1shl3gs/vertex/commit/224505cfbfa4b9cd1cd3a7e12f7b4ac32d929d48"
+        },
+        "date": 1681709390066,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.843,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.032,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.426,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.753,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 51.38,
             "unit": "ns/op"
           }
         ]
