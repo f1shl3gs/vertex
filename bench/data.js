cc4e8a73088cc66eb4fe8491199c874bb4fd2d66
github-action-benchmark <github@users.noreply.github.com>
2023-04-24 09:54:25 +0000

    add condition (customSmallerIsBetter) benchmark result for 8fbb16fa21ad445ef0d0325e10e01631e5e4eb7f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682330039376,
+  "lastUpdate": 1682330065689,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "9d2d444cd09f7b1f51f920ac8d7a3ab417dcbbd8",
-          "message": "Merge pull request #718 from f1shl3gs/dependabot/cargo/netlink-sys-0.8.4\n\nchore(deps): bump netlink-sys from 0.8.3 to 0.8.4",
-          "timestamp": "2023-01-24T17:01:03+08:00",
-          "tree_id": "a37d94167bb751c656625914f0e809d43ac87735",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9d2d444cd09f7b1f51f920ac8d7a3ab417dcbbd8"
-        },
-        "date": 1674551148751,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 29.305,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 28.671,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 29.412,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 40.339,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 62.644,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "429204a7820c8b3c4e56cd43bbdbb42c164deb09",
           "message": "Merge pull request #720 from f1shl3gs/dependabot/cargo/ahash-0.8.3\n\nchore(deps): bump ahash from 0.8.2 to 0.8.3",
           "timestamp": "2023-01-26T04:11:21+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 64.755,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8fbb16fa21ad445ef0d0325e10e01631e5e4eb7f",
+          "message": "chore(deps): bump mock_instant from 0.2.1 to 0.3.0 (#960)\n\nBumps [mock_instant](https://github.com/museun/mock_instant) from 0.2.1 to 0.3.0.\r\n- [Release notes](https://github.com/museun/mock_instant/releases)\r\n- [Commits](https://github.com/museun/mock_instant/compare/v0.2.1...v0.3.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: mock_instant\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-24T17:48:12+08:00",
+          "tree_id": "4ee89e204f8e465feffc80b67e0b83f12d124b29",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8fbb16fa21ad445ef0d0325e10e01631e5e4eb7f"
+        },
+        "date": 1682330052297,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 30.35,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 30.585,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 31.066,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 40.534,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 66.631,
             "unit": "ns/op"
           }
         ]
