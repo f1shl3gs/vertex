e29df5635e1f0f0c7bef2d67b1ed0e2fa3bf475d
github-action-benchmark <github@users.noreply.github.com>
2023-04-24 09:53:38 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 3a3bbb26de0ac515e6d967272cef0363647a13c6

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682330016894,
+  "lastUpdate": 1682330018334,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "429204a7820c8b3c4e56cd43bbdbb42c164deb09",
-          "message": "Merge pull request #720 from f1shl3gs/dependabot/cargo/ahash-0.8.3\n\nchore(deps): bump ahash from 0.8.2 to 0.8.3",
-          "timestamp": "2023-01-26T04:11:21+08:00",
-          "tree_id": "717ed57f6a419b1d51ac516a836e22b30e650795",
-          "url": "https://github.com/f1shl3gs/vertex/commit/429204a7820c8b3c4e56cd43bbdbb42c164deb09"
-        },
-        "date": 1674677723852,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 712430,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -5799,6 +5770,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 958450,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a3bbb26de0ac515e6d967272cef0363647a13c6",
+          "message": "chore(deps): bump typetag from 0.2.7 to 0.2.8 (#958)\n\nBumps [typetag](https://github.com/dtolnay/typetag) from 0.2.7 to 0.2.8.\r\n- [Release notes](https://github.com/dtolnay/typetag/releases)\r\n- [Commits](https://github.com/dtolnay/typetag/compare/0.2.7...0.2.8)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: typetag\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-24T17:49:01+08:00",
+          "tree_id": "4f86de37971e8d0dc630c128eca04a5b0e40e39d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3a3bbb26de0ac515e6d967272cef0363647a13c6"
+        },
+        "date": 1682330003743,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 835120,
             "unit": "ns/op"
           }
         ]
