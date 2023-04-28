ffc2c806fa3441c1de199ee83f3d4b9a2b4d4e4c
github-action-benchmark <github@users.noreply.github.com>
2023-04-28 09:01:53 +0000

    add condition (customSmallerIsBetter) benchmark result for 1e9f21b6ae2d4dbf3fa9751262c9ae6abb340630

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682672474735,
+  "lastUpdate": 1682672513055,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "d6b53f22945b69f1fa4ebdf4c035b204c45e9594",
-          "message": "Merge pull request #725 from f1shl3gs/remove_indoc\n\nremove indoc",
-          "timestamp": "2023-01-31T11:34:48+08:00",
-          "tree_id": "2634cc65c57c5aecb3151e3e87407416eb8bc764",
-          "url": "https://github.com/f1shl3gs/vertex/commit/d6b53f22945b69f1fa4ebdf4c035b204c45e9594"
-        },
-        "date": 1675136413190,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 26.333,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 26.909,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 26.812,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 37.497,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 61.151,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "3c9da65103ba22c81c24e70d0b537168534c5b35",
           "message": "Merge pull request #727 from f1shl3gs/dependabot/cargo/futures-0.3.26\n\nchore(deps): bump futures from 0.3.25 to 0.3.26",
           "timestamp": "2023-01-31T15:07:42+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 96.23,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e9f21b6ae2d4dbf3fa9751262c9ae6abb340630",
+          "message": "chore(deps): bump tracing from 0.1.37 to 0.1.38 (#962)\n\nBumps [tracing](https://github.com/tokio-rs/tracing) from 0.1.37 to 0.1.38.\r\n- [Release notes](https://github.com/tokio-rs/tracing/releases)\r\n- [Commits](https://github.com/tokio-rs/tracing/compare/tracing-0.1.37...tracing-0.1.38)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tracing\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-04-28T16:56:26+08:00",
+          "tree_id": "8794bff4c324ba8a4365cf4a1b85f8a36c17a0bd",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1e9f21b6ae2d4dbf3fa9751262c9ae6abb340630"
+        },
+        "date": 1682672499561,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 29.575,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 30.316,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 29.848,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 40.687,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 63.848,
             "unit": "ns/op"
           }
         ]
