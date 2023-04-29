03e57d0354144c103363aa5e36460a40c95b126e
github-action-benchmark <github@users.noreply.github.com>
2023-04-29 04:09:37 +0000

    add condition (customSmallerIsBetter) benchmark result for a70b8cbd8c6580d610c446a4b69f073fe5050853

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682741360612,
+  "lastUpdate": 1682741376983,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "c6e2af3aede3e216ea2f76fc2321e3dd2fed0581",
-          "message": "Merge pull request #730 from f1shl3gs/update_futures_util_to_0.3.26\n\nupdate futures-util to 0.3.26",
-          "timestamp": "2023-01-31T16:53:26+08:00",
-          "tree_id": "77fef488e64710ea98aa382b76e5e4ba2ac7caa2",
-          "url": "https://github.com/f1shl3gs/vertex/commit/c6e2af3aede3e216ea2f76fc2321e3dd2fed0581"
-        },
-        "date": 1675155475313,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 22.346,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 22.023,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.008,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 31.052,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 49.764,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "fd236090050024f6868bf5f5f64f0b59fc0b14a1",
           "message": "Merge pull request #731 from f1shl3gs/dependabot/cargo/bytes-1.4.0\n\nchore(deps): bump bytes from 1.3.0 to 1.4.0",
           "timestamp": "2023-02-01T16:24:59+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 63.092,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a70b8cbd8c6580d610c446a4b69f073fe5050853",
+          "message": "Add frequency support for pprof (#969)\n\n* add comment for the actual number of worker threads\r\n\r\n* add `frequency` for profiler",
+          "timestamp": "2023-04-29T12:05:05+08:00",
+          "tree_id": "25a3166e88c4ff15f0db64e3c601d290bd4955fc",
+          "url": "https://github.com/f1shl3gs/vertex/commit/a70b8cbd8c6580d610c446a4b69f073fe5050853"
+        },
+        "date": 1682741364272,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.44,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.394,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.313,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.186,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 56.366,
             "unit": "ns/op"
           }
         ]
