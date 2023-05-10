b64c7471366ada5787d3652f5d45a5d04092a9e8
github-action-benchmark <github@users.noreply.github.com>
2023-05-10 17:36:20 +0000

    add condition (customSmallerIsBetter) benchmark result for ddef0b26e42b9f1a459688ecea704d3150e20c3f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683740161006,
+  "lastUpdate": 1683740180055,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "eca3fb2ac99c3989e28433c3b84f2379af651b44",
-          "message": "Merge pull request #743 from f1shl3gs/dependabot/cargo/tikv-jemalloc-sys-0.5.35.3.0-patched\n\nchore(deps): bump tikv-jemalloc-sys from 0.5.2+5.3.0-patched to 0.5.3+5.3.0-patched",
-          "timestamp": "2023-02-07T12:57:51+08:00",
-          "tree_id": "d64ae472c32ee3253ccc81923629d2a0de58fd59",
-          "url": "https://github.com/f1shl3gs/vertex/commit/eca3fb2ac99c3989e28433c3b84f2379af651b44"
-        },
-        "date": 1675746170299,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 30.428,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 29.295,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 30.825,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 41.952,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 63.214,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "264a44230309d3694d8f937204e63eab08106810",
           "message": "Merge pull request #742 from f1shl3gs/configurable\n\nConfigurable",
           "timestamp": "2023-02-07T13:13:33+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 62.35,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ddef0b26e42b9f1a459688ecea704d3150e20c3f",
+          "message": "chore(deps): bump quote from 1.0.26 to 1.0.27 (#979)\n\nBumps [quote](https://github.com/dtolnay/quote) from 1.0.26 to 1.0.27.\r\n- [Release notes](https://github.com/dtolnay/quote/releases)\r\n- [Commits](https://github.com/dtolnay/quote/compare/1.0.26...1.0.27)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: quote\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-11T01:31:02+08:00",
+          "tree_id": "e38cbd96e4b1bc0027e6769b957ec29922784715",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ddef0b26e42b9f1a459688ecea704d3150e20c3f"
+        },
+        "date": 1683740168414,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 27.193,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 27.397,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 25.667,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 37.052,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 57.497,
             "unit": "ns/op"
           }
         ]
