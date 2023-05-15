6123077d9a9830cc8f7260e2fba78b9f1d946eb0
github-action-benchmark <github@users.noreply.github.com>
2023-05-15 22:59:35 +0000

    add condition (customSmallerIsBetter) benchmark result for ff15d750953e5d860d2453b80d4ceb1a9a4f81b9

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684191553687,
+  "lastUpdate": 1684191575128,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "904734fb2fd0275ecf2061431817205b9c037e4d",
-          "message": "Merge pull request #752 from f1shl3gs/dependabot/cargo/tokio-util-0.7.5\n\nchore(deps): bump tokio-util from 0.7.4 to 0.7.5",
-          "timestamp": "2023-02-10T19:37:33+08:00",
-          "tree_id": "e20e12244ce35a8d0287954b5d288122c00199ea",
-          "url": "https://github.com/f1shl3gs/vertex/commit/904734fb2fd0275ecf2061431817205b9c037e4d"
-        },
-        "date": 1676029341578,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 23.406,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.295,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.936,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 31.329,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 53.952,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "636207a9a8448f5e799b207ddb488b0b74a3c575",
           "message": "Merge pull request #753 from f1shl3gs/migrate_sinks_to_configurable\n\nmigrate sink to configurable",
           "timestamp": "2023-02-10T19:57:03+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 53.982,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff15d750953e5d860d2453b80d4ceb1a9a4f81b9",
+          "message": "chore(deps): bump socket2 from 0.5.2 to 0.5.3 (#988)\n\nBumps [socket2](https://github.com/rust-lang/socket2) from 0.5.2 to 0.5.3.\r\n- [Release notes](https://github.com/rust-lang/socket2/releases)\r\n- [Changelog](https://github.com/rust-lang/socket2/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/socket2/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: socket2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-16T06:54:40+08:00",
+          "tree_id": "93b12fb45138be9ff1f54ca890ede8ebf81b78ee",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ff15d750953e5d860d2453b80d4ceb1a9a4f81b9"
+        },
+        "date": 1684191563227,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 25.229,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 26.073,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 25.882,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 33.976,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 55.393,
             "unit": "ns/op"
           }
         ]
