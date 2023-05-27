e583ed00e89b517a825d851a720bafad2555bce4
github-action-benchmark <github@users.noreply.github.com>
2023-05-27 13:12:54 +0000

    add condition (customSmallerIsBetter) benchmark result for 3d83cc2ee0f14bfb543e88e414c61e8038a60cc5

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685193121084,
+  "lastUpdate": 1685193173964,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "cc15dc9e4ce46a92390478de784ef0165fc046f1",
-          "message": "Merge pull request #767 from f1shl3gs/dependabot/cargo/rkyv-0.7.40\n\nchore(deps): bump rkyv from 0.7.39 to 0.7.40",
-          "timestamp": "2023-02-14T16:30:30+08:00",
-          "tree_id": "65b7b3ade9bcc7a5861951f62814d41dfa979782",
-          "url": "https://github.com/f1shl3gs/vertex/commit/cc15dc9e4ce46a92390478de784ef0165fc046f1"
-        },
-        "date": 1676363798645,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 24.808,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 25.16,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 25.598,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 33.735,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 55.671,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "1a3c29b5c7425bb5d46be85fb1659bfb150a6eaa",
           "message": "Merge pull request #768 from f1shl3gs/dependabot/cargo/once_cell-1.17.1\n\nchore(deps): bump once_cell from 1.17.0 to 1.17.1",
           "timestamp": "2023-02-16T04:29:26+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.585,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3d83cc2ee0f14bfb543e88e414c61e8038a60cc5",
+          "message": "chore(deps): bump proc-macro2 from 1.0.58 to 1.0.59 (#1000)\n\nBumps [proc-macro2](https://github.com/dtolnay/proc-macro2) from 1.0.58 to 1.0.59.\r\n- [Release notes](https://github.com/dtolnay/proc-macro2/releases)\r\n- [Commits](https://github.com/dtolnay/proc-macro2/compare/1.0.58...1.0.59)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: proc-macro2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-27T21:07:04+08:00",
+          "tree_id": "75795eaefa0a94d7ddaacc1c4879c40f78605cf8",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3d83cc2ee0f14bfb543e88e414c61e8038a60cc5"
+        },
+        "date": 1685193159023,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 28.945,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 28.747,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 29.305,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 42.222,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 60.058,
             "unit": "ns/op"
           }
         ]
