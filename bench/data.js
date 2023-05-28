518c1b5c6fe9f2de496fe2a98b73117a6bd4606b
github-action-benchmark <github@users.noreply.github.com>
2023-05-28 15:15:18 +0000

    add prometheus (customSmallerIsBetter) benchmark result for cf8a9c9f3e38e85d23bfd4509f2dacee2c74a8fc

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685194474336,
+  "lastUpdate": 1685286918526,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "1a3c29b5c7425bb5d46be85fb1659bfb150a6eaa",
-          "message": "Merge pull request #768 from f1shl3gs/dependabot/cargo/once_cell-1.17.1\n\nchore(deps): bump once_cell from 1.17.0 to 1.17.1",
-          "timestamp": "2023-02-16T04:29:26+08:00",
-          "tree_id": "c9af24c7e5d1dce5696dafac65428fe81b9ac537",
-          "url": "https://github.com/f1shl3gs/vertex/commit/1a3c29b5c7425bb5d46be85fb1659bfb150a6eaa"
-        },
-        "date": 1676493253952,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 717600,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -5799,6 +5770,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 921470,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cf8a9c9f3e38e85d23bfd4509f2dacee2c74a8fc",
+          "message": "chore(deps): bump quote from 1.0.27 to 1.0.28 (#1001)\n\nBumps [quote](https://github.com/dtolnay/quote) from 1.0.27 to 1.0.28.\r\n- [Release notes](https://github.com/dtolnay/quote/releases)\r\n- [Commits](https://github.com/dtolnay/quote/compare/1.0.27...1.0.28)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: quote\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-28T23:11:29+08:00",
+          "tree_id": "6f98d974698fc6f58bbc7fd1cc9c72127f89056f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/cf8a9c9f3e38e85d23bfd4509f2dacee2c74a8fc"
+        },
+        "date": 1685286904825,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 713040,
             "unit": "ns/op"
           }
         ]
