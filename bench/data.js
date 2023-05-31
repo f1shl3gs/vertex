0760c5e601d87a64a8f3354ef0a13daa5e97d71f
github-action-benchmark <github@users.noreply.github.com>
2023-05-31 16:51:00 +0000

    add condition (customSmallerIsBetter) benchmark result for 5545de745b8d53d5e991a24248df9126e9b1a392

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685551686485,
+  "lastUpdate": 1685551860721,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "3c0784ec09c2ea59592ee7b6192af77479f42c6e",
-          "message": "Merge pull request #772 from f1shl3gs/dependabot/cargo/memmap2-0.5.9\n\nchore(deps): bump memmap2 from 0.5.8 to 0.5.9",
-          "timestamp": "2023-02-21T19:11:52+08:00",
-          "tree_id": "51906382a17b5cfdd90f150d3db369a7d9b06b0c",
-          "url": "https://github.com/f1shl3gs/vertex/commit/3c0784ec09c2ea59592ee7b6192af77479f42c6e"
-        },
-        "date": 1676978411218,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 24.276,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.397,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.821,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 31.666,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 52.497,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "9835243453626ed66d7dfb0b2a4afc140b72ab79",
           "message": "Merge pull request #771 from f1shl3gs/dependabot/cargo/num_enum-0.5.10\n\nchore(deps): bump num_enum from 0.5.9 to 0.5.10",
           "timestamp": "2023-02-21T19:12:01+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 65.653,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5545de745b8d53d5e991a24248df9126e9b1a392",
+          "message": "chore(deps): bump chrono from 0.4.25 to 0.4.26 (#1008)\n\nBumps [chrono](https://github.com/chronotope/chrono) from 0.4.25 to 0.4.26.\r\n- [Release notes](https://github.com/chronotope/chrono/releases)\r\n- [Changelog](https://github.com/chronotope/chrono/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/chronotope/chrono/compare/v0.4.25...v0.4.26)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: chrono\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-06-01T00:43:18+08:00",
+          "tree_id": "6e2cdba09c75ca03eb8984fbdc191a166f055981",
+          "url": "https://github.com/f1shl3gs/vertex/commit/5545de745b8d53d5e991a24248df9126e9b1a392"
+        },
+        "date": 1685551848352,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 24.48,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 25.24,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 25.742,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 34.467,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 54.466,
             "unit": "ns/op"
           }
         ]
