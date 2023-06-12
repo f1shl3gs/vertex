d3d3f2c545f9a14194efd61766cfb70215471af2
github-action-benchmark <github@users.noreply.github.com>
2023-06-12 11:16:01 +0000

    add condition (customSmallerIsBetter) benchmark result for 184b91d34be296292f4c6bf967a7642b0e966dd8

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686568557583,
+  "lastUpdate": 1686568561067,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "734b4a393bb74d054663709e8dae60863e93583d",
-          "message": "Merge pull request #796 from f1shl3gs/dependabot/cargo/dyn-clone-1.0.11\n\nchore(deps): bump dyn-clone from 1.0.10 to 1.0.11",
-          "timestamp": "2023-03-01T01:44:34+08:00",
-          "tree_id": "15dad5c65ca87f20a5132a68ebcfc089888bd4a8",
-          "url": "https://github.com/f1shl3gs/vertex/commit/734b4a393bb74d054663709e8dae60863e93583d"
-        },
-        "date": 1677606625624,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 29.768,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 28.488,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 31.299,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 41.639,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 66.523,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "d33c44652dab3509d84f7b8903d45049d1376056",
           "message": "Merge pull request #799 from f1shl3gs/dependabot/cargo/prost-build-0.11.8\n\nchore(deps): bump prost-build from 0.11.6 to 0.11.8",
           "timestamp": "2023-03-01T20:53:19+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 56.352,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "184b91d34be296292f4c6bf967a7642b0e966dd8",
+          "message": "chore(deps): bump tokio-rustls from 0.24.0 to 0.24.1 (#1026)\n\nBumps [tokio-rustls](https://github.com/rustls/tokio-rustls) from 0.24.0 to 0.24.1.\r\n- [Commits](https://github.com/rustls/tokio-rustls/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tokio-rustls\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-06-12T19:11:00+08:00",
+          "tree_id": "f907dcb1dce99b78371e0996a17584ccc18e1371",
+          "url": "https://github.com/f1shl3gs/vertex/commit/184b91d34be296292f4c6bf967a7642b0e966dd8"
+        },
+        "date": 1686568545161,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.744,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.055,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 22.806,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 31.397,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 55.185,
             "unit": "ns/op"
           }
         ]
