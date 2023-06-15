e4663508b1e9c182f52f3e2a81b330a90657aa53
github-action-benchmark <github@users.noreply.github.com>
2023-06-15 02:20:52 +0000

    add condition (customSmallerIsBetter) benchmark result for 82e457ec5ff854ca12f39bf829b3d05de4b6f4a5

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686795642016,
+  "lastUpdate": 1686795652451,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "d33c44652dab3509d84f7b8903d45049d1376056",
-          "message": "Merge pull request #799 from f1shl3gs/dependabot/cargo/prost-build-0.11.8\n\nchore(deps): bump prost-build from 0.11.6 to 0.11.8",
-          "timestamp": "2023-03-01T20:53:19+08:00",
-          "tree_id": "34511cbb1c7072424df7ba97d920cbe1c334def7",
-          "url": "https://github.com/f1shl3gs/vertex/commit/d33c44652dab3509d84f7b8903d45049d1376056"
-        },
-        "date": 1677675477714,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 23.215,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.155,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.659,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 31.111,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 53.042,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "51254d869ef1849a875310a5eb0998813eb5159b",
           "message": "Merge pull request #798 from f1shl3gs/dependabot/cargo/crossbeam-utils-0.8.15\n\nchore(deps): bump crossbeam-utils from 0.8.14 to 0.8.15",
           "timestamp": "2023-03-01T20:53:40+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.185,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82e457ec5ff854ca12f39bf829b3d05de4b6f4a5",
+          "message": "chore(deps): bump crossbeam-utils from 0.8.15 to 0.8.16 (#1027)\n\nBumps [crossbeam-utils](https://github.com/crossbeam-rs/crossbeam) from 0.8.15 to 0.8.16.\r\n- [Release notes](https://github.com/crossbeam-rs/crossbeam/releases)\r\n- [Changelog](https://github.com/crossbeam-rs/crossbeam/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/crossbeam-rs/crossbeam/compare/crossbeam-utils-0.8.15...crossbeam-utils-0.8.16)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: crossbeam-utils\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-06-15T10:16:00+08:00",
+          "tree_id": "5c2f4f3450ae68c9c7864b698db82ca75dd15c63",
+          "url": "https://github.com/f1shl3gs/vertex/commit/82e457ec5ff854ca12f39bf829b3d05de4b6f4a5"
+        },
+        "date": 1686795639223,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.352,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 25.775,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 24.859,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 35.543,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 52.026,
             "unit": "ns/op"
           }
         ]
