8447ab1199d5d917447f7b24e0c9186da02f8c64
github-action-benchmark <github@users.noreply.github.com>
2023-07-20 09:38:39 +0000

    add Build (customSmallerIsBetter) benchmark result for f6121b6ba473bace07b199e43a385c1c370b0739

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689664923303,
+  "lastUpdate": 1689845918974,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -36224,40 +36224,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "2e8b86bb3fe7a8a71aaf808ee148ae251feacea3",
-          "message": "chore(deps): bump socket2 from 0.4.7 to 0.5.1 (#806)\n\nBumps [socket2](https://github.com/rust-lang/socket2) from 0.4.7 to 0.5.1.\r\n- [Release notes](https://github.com/rust-lang/socket2/releases)\r\n- [Changelog](https://github.com/rust-lang/socket2/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/socket2/commits/v0.5.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: socket2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-03-28T17:51:42+08:00",
-          "tree_id": "d36a82d987bc20fe341e312cccddf435bba2f937",
-          "url": "https://github.com/f1shl3gs/vertex/commit/2e8b86bb3fe7a8a71aaf808ee148ae251feacea3"
-        },
-        "date": 1679998813404,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 1426,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 57339.7,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "837774fde39613937f59a0df07745a3a6c1abf41",
           "message": "chore(deps): bump serde_json from 1.0.94 to 1.0.95 (#897)\n\nBumps [serde_json](https://github.com/serde-rs/json) from 1.0.94 to 1.0.95.\r\n- [Release notes](https://github.com/serde-rs/json/releases)\r\n- [Commits](https://github.com/serde-rs/json/compare/v1.0.94...v1.0.95)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde_json\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-03-28T17:51:56+08:00",
@@ -43007,6 +42973,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 44869.6,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6121b6ba473bace07b199e43a385c1c370b0739",
+          "message": "chore(deps): bump serde from 1.0.171 to 1.0.173 (#1109)\n\nBumps [serde](https://github.com/serde-rs/serde) from 1.0.171 to 1.0.173.\r\n- [Release notes](https://github.com/serde-rs/serde/releases)\r\n- [Commits](https://github.com/serde-rs/serde/compare/v1.0.171...v1.0.173)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-07-20T17:13:41+08:00",
+          "tree_id": "5da7772bd783b4d582f96a81d9615b9b608632a5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f6121b6ba473bace07b199e43a385c1c370b0739"
+        },
+        "date": 1689845902301,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1327,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 44863.2,
             "unit": "KiB"
           }
         ]
