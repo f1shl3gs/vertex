4df0102e58cffcfeb2e9a4ab7f0ee445f7236c31
github-action-benchmark <github@users.noreply.github.com>
2023-07-21 05:56:06 +0000

    add Build (customSmallerIsBetter) benchmark result for 1e083f7ce066beac7187a74c7d4f4a605c790119

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689918672754,
+  "lastUpdate": 1689918966620,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -36214,40 +36214,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "da5ca165ac5098e3730be3cb40edc549bde8ce68",
-          "message": "chore(deps): bump tempfile from 3.4.0 to 3.5.0 (#901)\n\nBumps [tempfile](https://github.com/Stebalien/tempfile) from 3.4.0 to 3.5.0.\r\n- [Release notes](https://github.com/Stebalien/tempfile/releases)\r\n- [Changelog](https://github.com/Stebalien/tempfile/blob/master/NEWS)\r\n- [Commits](https://github.com/Stebalien/tempfile/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tempfile\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-03-29T13:31:42+08:00",
-          "tree_id": "5cb426a564df6fbfa62fbdfe291e1f6ee53b16c0",
-          "url": "https://github.com/f1shl3gs/vertex/commit/da5ca165ac5098e3730be3cb40edc549bde8ce68"
-        },
-        "date": 1680069353401,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 1200,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 57362.3,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -43002,6 +42968,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1237,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 44845.6,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e083f7ce066beac7187a74c7d4f4a605c790119",
+          "message": "chore(deps): bump serde_yaml from 0.9.24 to 0.9.25 (#1111)\n\nBumps [serde_yaml](https://github.com/dtolnay/serde-yaml) from 0.9.24 to 0.9.25.\r\n- [Release notes](https://github.com/dtolnay/serde-yaml/releases)\r\n- [Commits](https://github.com/dtolnay/serde-yaml/compare/0.9.24...0.9.25)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde_yaml\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-07-21T13:28:00+08:00",
+          "tree_id": "9086abb7909d78d09e66b6e4c5efbd7048ea2f91",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1e083f7ce066beac7187a74c7d4f4a605c790119"
+        },
+        "date": 1689918949246,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1500,
             "unit": "s"
           },
           {
