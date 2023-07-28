5261aac650963330f9b5374b89a93682349d6d37
github-action-benchmark <github@users.noreply.github.com>
2023-07-28 10:22:49 +0000

    add condition (customSmallerIsBetter) benchmark result for db6e9a1fb447f3909a2f9c94f1f3fb0098485c4d

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690539761030,
+  "lastUpdate": 1690539769758,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13615,55 +13615,6 @@ window.BENCHMARK_DATA = {
             "username": "f1shl3gs"
           },
           "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "fc9ec2753f1e43c8ab63ce2eda57237d689018d5",
-          "message": "Merge pull request #831 from f1shl3gs/disable_threadpool_for_jaeger\n\ndisable unused `threadpool` feature",
-          "timestamp": "2023-03-11T17:37:37+08:00",
-          "tree_id": "ad8c203c0237d37f5a30a9465b7c8cc5118aef8d",
-          "url": "https://github.com/f1shl3gs/vertex/commit/fc9ec2753f1e43c8ab63ce2eda57237d689018d5"
-        },
-        "date": 1678527740446,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 22.714,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 22.953,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.392,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 31.253,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 53.647,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 52.297,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db6e9a1fb447f3909a2f9c94f1f3fb0098485c4d",
+          "message": "chore(deps): bump tikv-jemallocator from 0.5.0 to 0.5.4 (#1135)\n\nBumps [tikv-jemallocator](https://github.com/tikv/jemallocator) from 0.5.0 to 0.5.4.\r\n- [Release notes](https://github.com/tikv/jemallocator/releases)\r\n- [Changelog](https://github.com/tikv/jemallocator/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/tikv/jemallocator/compare/0.5.0...0.5.4)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tikv-jemallocator\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-07-28T18:18:19+08:00",
+          "tree_id": "c0b640ed68aa8163ff767d8b19b35732c83894ec",
+          "url": "https://github.com/f1shl3gs/vertex/commit/db6e9a1fb447f3909a2f9c94f1f3fb0098485c4d"
+        },
+        "date": 1690539753669,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 22.5,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 22.332,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 22.91,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 40.735,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 52.323,
             "unit": "ns/op"
           }
         ]
