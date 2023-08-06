3496ec0d7f98740b1b50ed7f475e2145fdb5cee6
github-action-benchmark <github@users.noreply.github.com>
2023-08-06 15:19:00 +0000

    add condition (customSmallerIsBetter) benchmark result for 85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691335106260,
+  "lastUpdate": 1691335140011,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "9990549d83aace6b329199f6037e3718f229ac9f",
-          "message": "Merge pull request #843 from f1shl3gs/dependabot/cargo/serde-1.0.156\n\nchore(deps): bump serde from 1.0.155 to 1.0.156",
-          "timestamp": "2023-03-16T00:03:02+08:00",
-          "tree_id": "daf9a80b523fe1fc6fd0ad86467b92a8fb00a879",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9990549d83aace6b329199f6037e3718f229ac9f"
-        },
-        "date": 1678896528837,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 31.263,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 31.088,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 31.729,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 43.076,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 65.574,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "21914811afe6e766e6c8c313f5e623ee2174b344",
           "message": "Merge pull request #844 from f1shl3gs/dependabot/cargo/assert_cmd-2.0.9\n\nchore(deps): bump assert_cmd from 2.0.8 to 2.0.9",
           "timestamp": "2023-03-16T13:36:08+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 56.018,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0",
+          "message": "fix validate_last_write (#1146)",
+          "timestamp": "2023-08-06T23:13:56+08:00",
+          "tree_id": "e828fe059d6d979afca37784b11c8700076095ec",
+          "url": "https://github.com/f1shl3gs/vertex/commit/85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0"
+        },
+        "date": 1691335123375,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 28.734,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 25.982,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 27.96,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 45.954,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 55.356,
             "unit": "ns/op"
           }
         ]
