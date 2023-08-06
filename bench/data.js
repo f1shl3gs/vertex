711695a22e834ddda8c4561fe6233880410e9ffd
github-action-benchmark <github@users.noreply.github.com>
2023-08-06 15:19:18 +0000

    add event (customSmallerIsBetter) benchmark result for 85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691335140011,
+  "lastUpdate": 1691335158242,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -58963,6 +58963,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1239,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0",
+          "message": "fix validate_last_write (#1146)",
+          "timestamp": "2023-08-06T23:13:56+08:00",
+          "tree_id": "e828fe059d6d979afca37784b11c8700076095ec",
+          "url": "https://github.com/f1shl3gs/vertex/commit/85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0"
+        },
+        "date": 1691335140048,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 69.157,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 194.1,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 459.45,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1328.1,
             "unit": "ns/op"
           }
         ]
