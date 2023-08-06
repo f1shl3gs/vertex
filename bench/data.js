b59c3b028cac9ab71673aef81827aa0ea21f6b78
github-action-benchmark <github@users.noreply.github.com>
2023-08-06 15:17:21 +0000

    add metrics (customSmallerIsBetter) benchmark result for 85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691335032316,
+  "lastUpdate": 1691335041029,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -5805,45 +5805,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "9990549d83aace6b329199f6037e3718f229ac9f",
-          "message": "Merge pull request #843 from f1shl3gs/dependabot/cargo/serde-1.0.156\n\nchore(deps): bump serde from 1.0.155 to 1.0.156",
-          "timestamp": "2023-03-16T00:03:02+08:00",
-          "tree_id": "daf9a80b523fe1fc6fd0ad86467b92a8fb00a879",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9990549d83aace6b329199f6037e3718f229ac9f"
-        },
-        "date": 1678896425593,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 90.76,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 185.81,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 301.37,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -13601,6 +13562,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 290.56,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0",
+          "message": "fix validate_last_write (#1146)",
+          "timestamp": "2023-08-06T23:13:56+08:00",
+          "tree_id": "e828fe059d6d979afca37784b11c8700076095ec",
+          "url": "https://github.com/f1shl3gs/vertex/commit/85f642dc00e7fd03b294f3b8a82c27c94b7ae7c0"
+        },
+        "date": 1691335026606,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 85.602,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 214.41,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 344.73,
             "unit": "ns/op"
           }
         ]
