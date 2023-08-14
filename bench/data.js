035f5fce556c38fdc060f64147c3d5a2e5cfe844
github-action-benchmark <github@users.noreply.github.com>
2023-08-14 12:42:12 +0000

    add condition (customSmallerIsBetter) benchmark result for dfa0aaddfcb00900c2ba8395968572923052467d

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692016858422,
+  "lastUpdate": 1692016931981,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "2c1802bbc648882ebef8267c38641a29f039406c",
-          "message": "Merge pull request #860 from f1shl3gs/dependabot/cargo/serde-1.0.158\n\nchore(deps): bump serde from 1.0.157 to 1.0.158",
-          "timestamp": "2023-03-21T15:52:03+08:00",
-          "tree_id": "8e001cdb633b1720bda0770b260eb6dc08b72d61",
-          "url": "https://github.com/f1shl3gs/vertex/commit/2c1802bbc648882ebef8267c38641a29f039406c"
-        },
-        "date": 1679385442198,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 27.432,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 27.955,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 28.779,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 37.942,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 57.716,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "ccc68c633f925edf80a929bb2bc43c64c513ea5c",
           "message": "Merge pull request #862 from f1shl3gs/update_syn_2.0.4\n\nupdate syn to 2.0.4",
           "timestamp": "2023-03-21T18:27:57+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 50.668,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dfa0aaddfcb00900c2ba8395968572923052467d",
+          "message": "use `dep:` to indicate dependence (#1163)",
+          "timestamp": "2023-08-14T20:36:47+08:00",
+          "tree_id": "30e1afd2652a7acb710e88dcfad40be4a566b4b4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/dfa0aaddfcb00900c2ba8395968572923052467d"
+        },
+        "date": 1692016913029,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 29.332,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 27.673,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 30.489,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 52.451,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 66.713,
             "unit": "ns/op"
           }
         ]
