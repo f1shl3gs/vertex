45d0400d27c7bbe57b41546f8aee5feb7ad12eba
github-action-benchmark <github@users.noreply.github.com>
2023-08-25 16:40:45 +0000

    add condition (customSmallerIsBetter) benchmark result for 8b6984823112df4bbbbffcbcce0c670b89f35f47

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692981599574,
+  "lastUpdate": 1692981645654,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13620,55 +13620,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "9adbcf1f4b7ba317f1749a04c57aa11ae288ec81",
-          "message": "chore(deps): bump tokio from 1.26.0 to 1.27.0 (#898)\n\nBumps [tokio](https://github.com/tokio-rs/tokio) from 1.26.0 to 1.27.0.\r\n- [Release notes](https://github.com/tokio-rs/tokio/releases)\r\n- [Commits](https://github.com/tokio-rs/tokio/compare/tokio-1.26.0...tokio-1.27.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tokio\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-03-28T17:08:40+08:00",
-          "tree_id": "775e8ca23039ff4dcc58690068c33fc5ca512969",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9adbcf1f4b7ba317f1749a04c57aa11ae288ec81"
-        },
-        "date": 1679994915618,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 28.621,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 27.171,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 27.593,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 38.963,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 59.439,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "2e8b86bb3fe7a8a71aaf808ee148ae251feacea3",
           "message": "chore(deps): bump socket2 from 0.4.7 to 0.5.1 (#806)\n\nBumps [socket2](https://github.com/rust-lang/socket2) from 0.4.7 to 0.5.1.\r\n- [Release notes](https://github.com/rust-lang/socket2/releases)\r\n- [Changelog](https://github.com/rust-lang/socket2/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/socket2/commits/v0.5.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: socket2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-03-28T17:51:42+08:00",
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 54.498,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b6984823112df4bbbbffcbcce0c670b89f35f47",
+          "message": "chore(deps): bump pin-project-lite from 0.2.12 to 0.2.13 (#1191)\n\nBumps [pin-project-lite](https://github.com/taiki-e/pin-project-lite) from 0.2.12 to 0.2.13.\r\n- [Release notes](https://github.com/taiki-e/pin-project-lite/releases)\r\n- [Changelog](https://github.com/taiki-e/pin-project-lite/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/taiki-e/pin-project-lite/compare/v0.2.12...v0.2.13)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: pin-project-lite\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-08-26T00:35:31+08:00",
+          "tree_id": "171d71030b9d31e7451e333ce704050ef9beb63f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8b6984823112df4bbbbffcbcce0c670b89f35f47"
+        },
+        "date": 1692981626637,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 33.66,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 33.463,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 33.896,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 53.752,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 74.952,
             "unit": "ns/op"
           }
         ]
