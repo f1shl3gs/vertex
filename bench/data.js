8293f34438bcfc0c4d336597346308a25eee82aa
github-action-benchmark <github@users.noreply.github.com>
2023-08-25 14:33:27 +0000

    add Build (customSmallerIsBetter) benchmark result for 2ef6ee062a71f785eb525c4b04ddbb433e5fc7d7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692973141748,
+  "lastUpdate": 1692974006968,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -36224,40 +36224,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "4b57c9f7c1fd5ccc6ba3922c242b4a67d1460846",
-          "message": "chore(deps): bump memmap2 from 0.5.10 to 0.6.0 (#981)\n\nBumps [memmap2](https://github.com/RazrFalcon/memmap2-rs) from 0.5.10 to 0.6.0.\r\n- [Changelog](https://github.com/RazrFalcon/memmap2-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/RazrFalcon/memmap2-rs/compare/v0.5.10...v0.6.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: memmap2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-05-11T01:32:42+08:00",
-          "tree_id": "766fcc5a78505bbeee38b437d3a7357e4b553fcb",
-          "url": "https://github.com/f1shl3gs/vertex/commit/4b57c9f7c1fd5ccc6ba3922c242b4a67d1460846"
-        },
-        "date": 1683741656085,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 1288,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 44222.7,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "0f2850ccaca28292487778d21b5c687cff130eb0",
           "message": "chore(deps): bump memmap2 from 0.6.0 to 0.6.1 (#985)\n\nBumps [memmap2](https://github.com/RazrFalcon/memmap2-rs) from 0.6.0 to 0.6.1.\r\n- [Changelog](https://github.com/RazrFalcon/memmap2-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/RazrFalcon/memmap2-rs/compare/v0.6.0...v0.6.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: memmap2\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-05-11T14:35:06+08:00",
@@ -43002,6 +42968,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 1376,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 44587.2,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ef6ee062a71f785eb525c4b04ddbb433e5fc7d7",
+          "message": "remove BackedArchive (#1189)",
+          "timestamp": "2023-08-25T22:13:56+08:00",
+          "tree_id": "b6fe311291523753696ea22230ceda780a2e4444",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2ef6ee062a71f785eb525c4b04ddbb433e5fc7d7"
+        },
+        "date": 1692973990279,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 1017,
             "unit": "s"
           },
           {
