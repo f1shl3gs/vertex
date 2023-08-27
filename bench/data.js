9fed4e497aa22f8958cfa08860d5c4fa7585706f
github-action-benchmark <github@users.noreply.github.com>
2023-08-27 03:45:24 +0000

    add condition (customSmallerIsBetter) benchmark result for 64345042bcf7cefbb932b8983bc48f4ae26160bf

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693107870569,
+  "lastUpdate": 1693107924053,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13610,55 +13610,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "da5ca165ac5098e3730be3cb40edc549bde8ce68",
-          "message": "chore(deps): bump tempfile from 3.4.0 to 3.5.0 (#901)\n\nBumps [tempfile](https://github.com/Stebalien/tempfile) from 3.4.0 to 3.5.0.\r\n- [Release notes](https://github.com/Stebalien/tempfile/releases)\r\n- [Changelog](https://github.com/Stebalien/tempfile/blob/master/NEWS)\r\n- [Commits](https://github.com/Stebalien/tempfile/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tempfile\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-03-29T13:31:42+08:00",
-          "tree_id": "5cb426a564df6fbfa62fbdfe291e1f6ee53b16c0",
-          "url": "https://github.com/f1shl3gs/vertex/commit/da5ca165ac5098e3730be3cb40edc549bde8ce68"
-        },
-        "date": 1680068192063,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 23.616,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.29,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.797,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 32.232,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 53.343,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -23403,6 +23354,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 56.891,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64345042bcf7cefbb932b8983bc48f4ae26160bf",
+          "message": "chore(deps): bump serde from 1.0.187 to 1.0.188 (#1194)\n\nBumps [serde](https://github.com/serde-rs/serde) from 1.0.187 to 1.0.188.\r\n- [Release notes](https://github.com/serde-rs/serde/releases)\r\n- [Commits](https://github.com/serde-rs/serde/compare/v1.0.187...v1.0.188)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-08-27T11:40:04+08:00",
+          "tree_id": "fc7ef7cbed7284fbb65a9c1c687824c83c458a0c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/64345042bcf7cefbb932b8983bc48f4ae26160bf"
+        },
+        "date": 1693107904383,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 29.954,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 27.913,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 30.532,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 47.783,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 62.334,
             "unit": "ns/op"
           }
         ]
