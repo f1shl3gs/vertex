74294bf9304f6d8172b44cbdc633bcf0027e163c
github-action-benchmark <github@users.noreply.github.com>
2023-09-01 05:55:25 +0000

    add condition (customSmallerIsBetter) benchmark result for f32fbed75873058e765e17a2ef276d947d6aa217

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693547668366,
+  "lastUpdate": 1693547725052,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "b07b1dec239b7947f39aef44b15bd4929cab31cf",
-          "message": "chore(deps): bump rustls from 0.21.1 to 0.21.2 (#1030)\n\nBumps [rustls](https://github.com/rustls/rustls) from 0.21.1 to 0.21.2.\r\n- [Changelog](https://github.com/rustls/rustls/blob/main/RELEASE_NOTES.md)\r\n- [Commits](https://github.com/rustls/rustls/compare/v/0.21.1...v/0.21.2)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: rustls\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-06-15T21:21:42+08:00",
-          "tree_id": "88d246ac560f17c4344d6c5aa25ad455509a9a19",
-          "url": "https://github.com/f1shl3gs/vertex/commit/b07b1dec239b7947f39aef44b15bd4929cab31cf"
-        },
-        "date": 1686835576933,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 24.098,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 27.478,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 25.458,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 35.913,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 54.866,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "ef8808ccebb854ded50d0d8b607cd91f0fe4200a",
           "message": "chore(deps): bump rustls-native-certs from 0.6.2 to 0.6.3 (#1031)\n\nBumps [rustls-native-certs](https://github.com/ctz/rustls-native-certs) from 0.6.2 to 0.6.3.\r\n- [Commits](https://github.com/ctz/rustls-native-certs/compare/v/0.6.2...v/0.6.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: rustls-native-certs\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-06-15T22:51:35+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 60.831,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f32fbed75873058e765e17a2ef276d947d6aa217",
+          "message": "chore(deps): bump headers from 0.3.8 to 0.3.9 (#1212)\n\nBumps [headers](https://github.com/hyperium/headers) from 0.3.8 to 0.3.9.\r\n- [Commits](https://github.com/hyperium/headers/compare/headers-v0.3.8...headers-v0.3.9)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: headers\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-09-01T13:51:01+08:00",
+          "tree_id": "a87b3a81893d69addd9aea8846cc0d7eeffd7e3b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f32fbed75873058e765e17a2ef276d947d6aa217"
+        },
+        "date": 1693547707998,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.147,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 22.549,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 22.834,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 37.036,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 54.375,
             "unit": "ns/op"
           }
         ]
