80a0f8c55c67b77a27c567cd7e1e17facee9ca18
github-action-benchmark <github@users.noreply.github.com>
2023-09-09 09:23:29 +0000

    add condition (customSmallerIsBetter) benchmark result for 84e77f3d2e0f9ed69a7709abccdcbc8c39fe568f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694251402195,
+  "lastUpdate": 1694251409007,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "a13dfbdb4d23336f320f518d3275dc8c5d4d373d",
-          "message": "chore(deps): bump quote from 1.0.31 to 1.0.32 (#1123)\n\nBumps [quote](https://github.com/dtolnay/quote) from 1.0.31 to 1.0.32.\r\n- [Release notes](https://github.com/dtolnay/quote/releases)\r\n- [Commits](https://github.com/dtolnay/quote/compare/1.0.31...1.0.32)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: quote\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-07-24T14:16:44+08:00",
-          "tree_id": "41fc718873bd442a94bd86dfbc8e5135f5fdf27a",
-          "url": "https://github.com/f1shl3gs/vertex/commit/a13dfbdb4d23336f320f518d3275dc8c5d4d373d"
-        },
-        "date": 1690179699364,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 33.743,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 31.451,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 31.761,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 59.426,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 69.309,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "b905d8502459ff5b93968cd7413ef3c0af946f5d",
           "message": "chore(deps): bump typetag from 0.2.11 to 0.2.12 (#1125)\n\nBumps [typetag](https://github.com/dtolnay/typetag) from 0.2.11 to 0.2.12.\r\n- [Release notes](https://github.com/dtolnay/typetag/releases)\r\n- [Commits](https://github.com/dtolnay/typetag/compare/0.2.11...0.2.12)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: typetag\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-07-25T12:40:56+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 57.017,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "84e77f3d2e0f9ed69a7709abccdcbc8c39fe568f",
+          "message": "replace pin-project with pin-project-lite (#1238)",
+          "timestamp": "2023-09-09T17:19:21+08:00",
+          "tree_id": "90a2f571ff482721d79a4f860bc735357e8d4bc4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/84e77f3d2e0f9ed69a7709abccdcbc8c39fe568f"
+        },
+        "date": 1694251391202,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 24.569,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 23.641,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 24.182,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 39.885,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 57.057,
             "unit": "ns/op"
           }
         ]
