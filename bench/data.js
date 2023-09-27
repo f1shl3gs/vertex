7f8a03129a3c37157482c0184359a483971d43fe
github-action-benchmark <github@users.noreply.github.com>
2023-09-27 10:25:26 +0000

    add condition (customSmallerIsBetter) benchmark result for 731bb5b2376d76d6feb755bfe9a67f4f4406a618

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695810317366,
+  "lastUpdate": 1695810326583,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "b2f5328ad2fa7a48720aaed7134902d26ce8e650",
-          "message": "chore(deps): bump notify from 6.0.1 to 6.1.0 (#1177)\n\nBumps [notify](https://github.com/notify-rs/notify) from 6.0.1 to 6.1.0.\r\n- [Release notes](https://github.com/notify-rs/notify/releases)\r\n- [Changelog](https://github.com/notify-rs/notify/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/notify-rs/notify/compare/notify-6.0.1...notify-6.1.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: notify\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-08-21T16:35:39+08:00",
-          "tree_id": "cb083b49f38f8eec6ef18a87a4875a2110fbc2d0",
-          "url": "https://github.com/f1shl3gs/vertex/commit/b2f5328ad2fa7a48720aaed7134902d26ce8e650"
-        },
-        "date": 1692607341660,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 23.643,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.035,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.013,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 39.019,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 53.443,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "3f3119b1d8b570dbde774b53699051e9d6bc5f6e",
           "message": "chore(deps): bump notify from 6.1.0 to 6.1.1 (#1181)\n\nBumps [notify](https://github.com/notify-rs/notify) from 6.1.0 to 6.1.1.\r\n- [Release notes](https://github.com/notify-rs/notify/releases)\r\n- [Changelog](https://github.com/notify-rs/notify/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/notify-rs/notify/compare/notify-6.1.0...notify-6.1.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: notify\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-08-22T12:10:49+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 65.023,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "731bb5b2376d76d6feb755bfe9a67f4f4406a618",
+          "message": "use `super` to import mods (#1295)",
+          "timestamp": "2023-09-27T18:21:21+08:00",
+          "tree_id": "8a6b88f7b7ce0c8a09cf4144bc954ea769ee475f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/731bb5b2376d76d6feb755bfe9a67f4f4406a618"
+        },
+        "date": 1695810307791,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.277,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 22.305,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 22.783,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 140.44,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 50.458,
             "unit": "ns/op"
           }
         ]
