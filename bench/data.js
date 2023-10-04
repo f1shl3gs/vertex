355147f7f28c3ca1fb5afb803c4802970e314e2e
github-action-benchmark <github@users.noreply.github.com>
2023-10-04 03:31:18 +0000

    add condition (customSmallerIsBetter) benchmark result for 59c6fa5e068a1fdaa01cb6df5273335ee23086d0

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696390230490,
+  "lastUpdate": 1696390278185,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6810,55 +6810,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "a9d33ff4672a67621d022cac9f1a97b0d819bc9a",
-          "message": "chore(deps): bump dashmap from 5.5.1 to 5.5.2 (#1202)\n\nBumps [dashmap](https://github.com/xacrimon/dashmap) from 5.5.1 to 5.5.2.\r\n- [Release notes](https://github.com/xacrimon/dashmap/releases)\r\n- [Commits](https://github.com/xacrimon/dashmap/compare/v5.5.1...v5.5.2)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: dashmap\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-08-29T22:42:50+08:00",
-          "tree_id": "56420cfc14d2f28a93f37557e2cc4eccd8de4a53",
-          "url": "https://github.com/f1shl3gs/vertex/commit/a9d33ff4672a67621d022cac9f1a97b0d819bc9a"
-        },
-        "date": 1693320420390,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 22.99,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 22.925,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.516,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 36.979,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 51.066,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 52.59,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "59c6fa5e068a1fdaa01cb6df5273335ee23086d0",
+          "message": "chore(deps): bump lru from 0.11.1 to 0.12.0 (#1318)\n\nBumps [lru](https://github.com/jeromefroe/lru-rs) from 0.11.1 to 0.12.0.\r\n- [Changelog](https://github.com/jeromefroe/lru-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/jeromefroe/lru-rs/compare/0.11.1...0.12.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: lru\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-10-04T11:26:23+08:00",
+          "tree_id": "15a0eaf85a19643276e9019f427938fadc2fab0f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/59c6fa5e068a1fdaa01cb6df5273335ee23086d0"
+        },
+        "date": 1696390256518,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 28.64,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 28.426,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 29.628,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 184.44,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 61.309,
             "unit": "ns/op"
           }
         ]
