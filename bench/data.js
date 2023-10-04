dbd4780c55d3e1c6423f6cc734d56a6087f2cb89
github-action-benchmark <github@users.noreply.github.com>
2023-10-04 09:39:49 +0000

    add event (customSmallerIsBetter) benchmark result for fdc07a5c4616f8535ccc9b234d8c8a8749c6714b

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696412374332,
+  "lastUpdate": 1696412388996,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -25418,50 +25418,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "9149d2f1d7bf7c82d8be71552fd9323a4f4028a9",
-          "message": "CI: decrease points in bench charts (#1204)\n\n* set `max-items-in-chart` to 100",
-          "timestamp": "2023-08-29T23:50:12+08:00",
-          "tree_id": "e7f2275126be7354fecaa89f33072920c4bd2994",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9149d2f1d7bf7c82d8be71552fd9323a4f4028a9"
-        },
-        "date": 1693324489963,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "tags/insert/1",
-            "value": 70.676,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/5",
-            "value": 211.67,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/10",
-            "value": 530.57,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/20",
-            "value": 1527.6,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "49699333+dependabot[bot]@users.noreply.github.com",
             "name": "dependabot[bot]",
             "username": "dependabot[bot]"
@@ -29811,6 +29767,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 1167.3,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fdc07a5c4616f8535ccc9b234d8c8a8749c6714b",
+          "message": "sinks/prometheus: fix infinite bucket value (#1320)",
+          "timestamp": "2023-10-04T17:35:52+08:00",
+          "tree_id": "94eb725b3980cf8964fd0a8cede38cecdbc03975",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fdc07a5c4616f8535ccc9b234d8c8a8749c6714b"
+        },
+        "date": 1696412370744,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 53.225,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 163.75,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 403.36,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 1147,
             "unit": "ns/op"
           }
         ]
