ac859058c3a249b462ed4ab565e4c129ddca5699
github-action-benchmark <github@users.noreply.github.com>
2023-10-04 09:39:34 +0000

    add prometheus (customSmallerIsBetter) benchmark result for fdc07a5c4616f8535ccc9b234d8c8a8749c6714b

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696412360094,
+  "lastUpdate": 1696412374332,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "9149d2f1d7bf7c82d8be71552fd9323a4f4028a9",
-          "message": "CI: decrease points in bench charts (#1204)\n\n* set `max-items-in-chart` to 100",
-          "timestamp": "2023-08-29T23:50:12+08:00",
-          "tree_id": "e7f2275126be7354fecaa89f33072920c4bd2994",
-          "url": "https://github.com/f1shl3gs/vertex/commit/9149d2f1d7bf7c82d8be71552fd9323a4f4028a9"
-        },
-        "date": 1693324434533,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 836590,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 722020,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fdc07a5c4616f8535ccc9b234d8c8a8749c6714b",
+          "message": "sinks/prometheus: fix infinite bucket value (#1320)",
+          "timestamp": "2023-10-04T17:35:52+08:00",
+          "tree_id": "94eb725b3980cf8964fd0a8cede38cecdbc03975",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fdc07a5c4616f8535ccc9b234d8c8a8749c6714b"
+        },
+        "date": 1696412351609,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 864070,
             "unit": "ns/op"
           }
         ]
