d88e67fd563ff74c81c2905ffec31a7293089afc
github-action-benchmark <github@users.noreply.github.com>
2023-10-13 14:08:32 +0000

    add condition (customSmallerIsBetter) benchmark result for f8ae71d341a4bef2bd9bd0ac6b243f8cfcd6f1e7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697206072009,
+  "lastUpdate": 1697206112662,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "8f3817b86964e41adee5abc39de1815c7323318b",
-          "message": "chore(deps): bump lru from 0.11.0 to 0.11.1 (#1234)\n\nBumps [lru](https://github.com/jeromefroe/lru-rs) from 0.11.0 to 0.11.1.\r\n- [Changelog](https://github.com/jeromefroe/lru-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/jeromefroe/lru-rs/compare/0.11.0...0.11.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: lru\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-09-06T22:51:21+08:00",
-          "tree_id": "3766267f56f6a2d7207bff67719a51e5e9d6d2aa",
-          "url": "https://github.com/f1shl3gs/vertex/commit/8f3817b86964e41adee5abc39de1815c7323318b"
-        },
-        "date": 1694012166192,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 30.986,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 30.344,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 33.131,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 51.722,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 66.335,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "58f9c0a677a0316be141a964471633b9bf5ab33b",
           "message": "chore(deps): bump chrono from 0.4.29 to 0.4.30 (#1236)\n\nBumps [chrono](https://github.com/chronotope/chrono) from 0.4.29 to 0.4.30.\r\n- [Release notes](https://github.com/chronotope/chrono/releases)\r\n- [Changelog](https://github.com/chronotope/chrono/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/chronotope/chrono/compare/v0.4.29...v0.4.30)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: chrono\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-09-08T02:38:24+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 67.19,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f8ae71d341a4bef2bd9bd0ac6b243f8cfcd6f1e7",
+          "message": "remove duplicate get_hostname (#1342)",
+          "timestamp": "2023-10-13T22:03:03+08:00",
+          "tree_id": "f8c6661c186b820785b61a1abfd80f45e6297a84",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f8ae71d341a4bef2bd9bd0ac6b243f8cfcd6f1e7"
+        },
+        "date": 1697206090687,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 30.986,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 30.327,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 31.641,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 177.38,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 61.921,
             "unit": "ns/op"
           }
         ]
