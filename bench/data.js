69b9011d0fa8b105deca41e47cc57a7d0f7db374
github-action-benchmark <github@users.noreply.github.com>
2023-10-13 08:31:37 +0000

    add condition (customSmallerIsBetter) benchmark result for 5fa15ee4860a192109eab1dfbf2ff6b511bceaaa

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697185858734,
+  "lastUpdate": 1697185897821,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "d442f0b404917ea0cadfd09bc2152945d7924205",
-          "message": "chore(deps): bump chrono from 0.4.28 to 0.4.29 (#1233)\n\n* chore(deps): bump chrono from 0.4.28 to 0.4.29\r\n\r\nBumps [chrono](https://github.com/chronotope/chrono) from 0.4.28 to 0.4.29.\r\n- [Release notes](https://github.com/chronotope/chrono/releases)\r\n- [Changelog](https://github.com/chronotope/chrono/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/chronotope/chrono/compare/v0.4.28...v0.4.29)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: chrono\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\n* fix deprecate warnings\r\n\r\n---------\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\r\nCo-authored-by: f1shl3gs <fishlegs.engerman@gmail.com>",
-          "timestamp": "2023-09-06T20:50:55+08:00",
-          "tree_id": "ce444298af95e00cc18dabc0b54a8854ffabfb04",
-          "url": "https://github.com/f1shl3gs/vertex/commit/d442f0b404917ea0cadfd09bc2152945d7924205"
-        },
-        "date": 1694004913201,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 25.788,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 25.406,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 27.602,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 43.261,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 55.204,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "8f3817b86964e41adee5abc39de1815c7323318b",
           "message": "chore(deps): bump lru from 0.11.0 to 0.11.1 (#1234)\n\nBumps [lru](https://github.com/jeromefroe/lru-rs) from 0.11.0 to 0.11.1.\r\n- [Changelog](https://github.com/jeromefroe/lru-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/jeromefroe/lru-rs/compare/0.11.0...0.11.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: lru\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-09-06T22:51:21+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.335,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5fa15ee4860a192109eab1dfbf2ff6b511bceaaa",
+          "message": "bump serde to 1.0.189 (#1341)",
+          "timestamp": "2023-10-13T16:26:31+08:00",
+          "tree_id": "e351b30c5b980b7a67c883439c88cdf589615bd4",
+          "url": "https://github.com/f1shl3gs/vertex/commit/5fa15ee4860a192109eab1dfbf2ff6b511bceaaa"
+        },
+        "date": 1697185874899,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 30.186,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 30.704,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 30.948,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 174.04,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 67.19,
             "unit": "ns/op"
           }
         ]
