d901c01292e2b58353cb5edbc28bacc9bd0827cf
github-action-benchmark <github@users.noreply.github.com>
2023-10-14 16:36:20 +0000

    add condition (customSmallerIsBetter) benchmark result for f3dd8ea9f88e08dbe875b3d6410dc66ea2f0f30a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697301377594,
+  "lastUpdate": 1697301380906,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6810,55 +6810,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "363c73750a1209447f5914d803ac17b62d53b4cb",
-          "message": "chore(deps): bump kube from 0.85.0 to 0.86.0 (#1237)\n\n* chore(deps): bump kube from 0.85.0 to 0.86.0\r\n\r\nBumps [kube](https://github.com/kube-rs/kube) from 0.85.0 to 0.86.0.\r\n- [Release notes](https://github.com/kube-rs/kube/releases)\r\n- [Changelog](https://github.com/kube-rs/kube/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/kube-rs/kube/compare/0.85.0...0.86.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: kube\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\n* bump k8s-openapi to 0.20.0\r\n\r\n---------\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\r\nCo-authored-by: f1shl3gs <fishlegs.engerman@gmail.com>",
-          "timestamp": "2023-09-09T01:28:13+08:00",
-          "tree_id": "f515d55274a29924110e0e0f1aefee6606a0c8a7",
-          "url": "https://github.com/f1shl3gs/vertex/commit/363c73750a1209447f5914d803ac17b62d53b4cb"
-        },
-        "date": 1694194328939,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 24.625,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.698,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 24.14,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 39.237,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 57.017,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 51.405,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3dd8ea9f88e08dbe875b3d6410dc66ea2f0f30a",
+          "message": "bump regex to 1.10.1 (#1345)",
+          "timestamp": "2023-10-15T00:32:12+08:00",
+          "tree_id": "d757feaaa04c604158767fa14555663a09bcf870",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f3dd8ea9f88e08dbe875b3d6410dc66ea2f0f30a"
+        },
+        "date": 1697301361711,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 23.333,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 22.635,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 23.165,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 134.88,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 52.293,
             "unit": "ns/op"
           }
         ]
