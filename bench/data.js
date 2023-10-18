53347b40c9f3d2dbb77fc544b5cd96e0a476ed72
github-action-benchmark <github@users.noreply.github.com>
2023-10-18 16:30:23 +0000

    add metrics (customSmallerIsBetter) benchmark result for 4687ea0663d597c522f02dfbed7ffd94266fa894

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697646590797,
+  "lastUpdate": 1697646623372,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "e435b9d5ac7287ff5874e72db88140ceb29be5ab",
-          "message": "fix built_info's package description (#1245)",
-          "timestamp": "2023-09-12T23:51:28+08:00",
-          "tree_id": "8902e4a57cbc76e92407f6c93f42cee46290aefa",
-          "url": "https://github.com/f1shl3gs/vertex/commit/e435b9d5ac7287ff5874e72db88140ceb29be5ab"
-        },
-        "date": 1694534082494,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 97.745,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 228.64,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 343.04,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 285.03,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4687ea0663d597c522f02dfbed7ffd94266fa894",
+          "message": "bump uuid to 1.5.0 (#1356)",
+          "timestamp": "2023-10-19T00:26:49+08:00",
+          "tree_id": "4f24d639d5cd7fecac8e76d4c5adcf7a06a84fe0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4687ea0663d597c522f02dfbed7ffd94266fa894"
+        },
+        "date": 1697646600823,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 105.45,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 235.24,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 389.23,
             "unit": "ns/op"
           }
         ]
