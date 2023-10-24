da0fde77bf06cf2e11b898e1e91c823b06bd0a04
github-action-benchmark <github@users.noreply.github.com>
2023-10-24 04:06:26 +0000

    add condition (customSmallerIsBetter) benchmark result for 6377b9a732f3336a1ba097ab05dce7997cdf6c17

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698120376691,
+  "lastUpdate": 1698120386727,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6810,55 +6810,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "099922639b26b63ae3a127337dd0485c4bb87880",
-          "message": "chore(deps): bump syn from 2.0.35 to 2.0.36 (#1263)\n\nBumps [syn](https://github.com/dtolnay/syn) from 2.0.35 to 2.0.36.\r\n- [Release notes](https://github.com/dtolnay/syn/releases)\r\n- [Commits](https://github.com/dtolnay/syn/compare/2.0.35...2.0.36)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: syn\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-09-17T10:57:29+08:00",
-          "tree_id": "3744d032e7a984eacdbb511ec088a456936a0449",
-          "url": "https://github.com/f1shl3gs/vertex/commit/099922639b26b63ae3a127337dd0485c4bb87880"
-        },
-        "date": 1694919759794,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 34.285,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 33.828,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 35.419,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 53.149,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 69.701,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "fishlegs.engerman@gmail.com",
             "name": "f1shl3gs",
             "username": "f1shl3gs"
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 67.156,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6377b9a732f3336a1ba097ab05dce7997cdf6c17",
+          "message": "chore(deps): bump quick-xml from 0.30.0 to 0.31.0 (#1373)\n\nBumps [quick-xml](https://github.com/tafia/quick-xml) from 0.30.0 to 0.31.0.\r\n- [Release notes](https://github.com/tafia/quick-xml/releases)\r\n- [Changelog](https://github.com/tafia/quick-xml/blob/master/Changelog.md)\r\n- [Commits](https://github.com/tafia/quick-xml/compare/v0.30.0...v0.31.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: quick-xml\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-10-24T12:01:52+08:00",
+          "tree_id": "ff0a865653444ae9df4a7fdd365528e622c50ab5",
+          "url": "https://github.com/f1shl3gs/vertex/commit/6377b9a732f3336a1ba097ab05dce7997cdf6c17"
+        },
+        "date": 1698120364539,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 27.776,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 28.812,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 28.58,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 179.95,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 60.099,
             "unit": "ns/op"
           }
         ]
