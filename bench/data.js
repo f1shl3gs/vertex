74d9d8b4f608d6fe5c5b083c5c845d20424e7db3
github-action-benchmark <github@users.noreply.github.com>
2023-10-28 14:28:01 +0000

    add condition (customSmallerIsBetter) benchmark result for 9c75ba8a8215b329ee5f80a5b1eaa83af94e94b1

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698503262281,
+  "lastUpdate": 1698503281066,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "40ee01994fc9554d755ce25b73839df6732c32aa",
-          "message": "remove hyper-proxy (#1283)",
-          "timestamp": "2023-09-24T13:35:05+08:00",
-          "tree_id": "c6373b07f8bb6651a102ac7eba2344eae4d82844",
-          "url": "https://github.com/f1shl3gs/vertex/commit/40ee01994fc9554d755ce25b73839df6732c32aa"
-        },
-        "date": 1695533930551,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 24.147,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 23.392,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 23.716,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 136.64,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 57.096,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "a20e16112bdf55f791c2aacbd4c32495274f847c",
           "message": "Rollback tracing to 0.1.37 (#1284)\n\n* remove no-proxy\r\n\r\n* rollback tracing to 0.1.37",
           "timestamp": "2023-09-24T14:05:51+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 55.61,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c75ba8a8215b329ee5f80a5b1eaa83af94e94b1",
+          "message": "just serde fields without `serde_yaml` (#1392)",
+          "timestamp": "2023-10-28T22:24:07+08:00",
+          "tree_id": "dae7b5370854d36076444b9ccd714134c51b697c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9c75ba8a8215b329ee5f80a5b1eaa83af94e94b1"
+        },
+        "date": 1698503261400,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 21.163,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 21.635,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 22.836,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 151.83,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 47.6,
             "unit": "ns/op"
           }
         ]
