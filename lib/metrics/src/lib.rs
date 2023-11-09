24bb3a57a216d361c576358b19635f15b6071a31
f1shl3gs <fishlegs.engerman@gmail.com>
2023-11-09 20:42:00 +0800

    lib/metrics: remove sub registry (#1408)

--- lib/metrics/src/lib.rs
+++ lib/metrics/src/lib.rs
@@ -15,6 +15,5 @@ pub use gauge::Gauge;
 pub use histogram::{exponential_buckets, linear_buckets, Histogram, HistogramObservation};
 pub use metric::{Metric, Observation};
 pub use registry::{
-    global_registry, register_counter, register_gauge, register_histogram, sub_registry, Registry,
-    Reporter, SubRegistry,
+    global_registry, register_counter, register_gauge, register_histogram, Registry, Reporter,
 };
