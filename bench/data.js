6f0d3cc90cfae678e54d8d5b2bc2680e420241a3
github-action-benchmark <github@users.noreply.github.com>
2023-11-11 10:26:17 +0000

    add vertex (customSmallerIsBetter) benchmark result for 2fdace40f1955dcfa5be7dd8635a404c2af6023c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699698104572,
+  "lastUpdate": 1699698377408,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -21526,45 +21526,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "5636a1391782918cc4d888a6b9622bdd3a02a7f3",
-          "message": "refactor error (#1301)\n\n* refine Error\r\n\r\n* fix test",
-          "timestamp": "2023-09-28T14:21:48+08:00",
-          "tree_id": "a140e0203df674c4b75ed063c4b0959a6f6dbcb2",
-          "url": "https://github.com/f1shl3gs/vertex/commit/5636a1391782918cc4d888a6b9622bdd3a02a7f3"
-        },
-        "date": 1695883383475,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 33927000,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 1121600,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 21.609,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "8a46b4471798fe401c44a6f2997f68068ea50fc2",
           "message": "fix metrics collect (#1302)",
           "timestamp": "2023-09-28T18:38:28+08:00",
@@ -25409,6 +25370,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 44.236,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2fdace40f1955dcfa5be7dd8635a404c2af6023c",
+          "message": "chore(deps): bump proptest from 1.3.1 to 1.4.0 (#1414)\n\nBumps [proptest](https://github.com/proptest-rs/proptest) from 1.3.1 to 1.4.0.\r\n- [Release notes](https://github.com/proptest-rs/proptest/releases)\r\n- [Changelog](https://github.com/proptest-rs/proptest/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/proptest-rs/proptest/compare/v1.3.1...v1.4.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: proptest\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-11-11T18:03:04+08:00",
+          "tree_id": "82cbba14054f69afa34ab97993069539e41f7e5a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/2fdace40f1955dcfa5be7dd8635a404c2af6023c"
+        },
+        "date": 1699698353283,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 46809000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1465400,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 77.072,
             "unit": "ns/op"
           }
         ]
