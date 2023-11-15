fa0b042fdee61eb3e93d23f1d5f3776b0d59e196
f1shl3gs <fishlegs.engerman@gmail.com>
2023-11-16 01:56:37 +0800

    try

--- ci/cross/bootstrap-ubuntu.sh
+++ ci/cross/bootstrap-ubuntu.sh
@@ -16,27 +16,3 @@ unzip protoc-${PROTOC_VERSION}-linux-x86_64.zip
 cp bin/protoc /usr/local/bin
 cp -r include/google /usr/local/include/
 rm protoc-${PROTOC_VERSION}-linux-x86_64.zip
-
-# Setup mold
-#
-# We explicitly put `mold-wrapper.so` right beside `mold` itself because it's hard-coded to look in the same directory
-# first when trying to load the shared object, so we can dodge having to care about the "right" lib folder to put it in.
-TEMP=$(mktemp -d)
-MOLD_VERSION=2.3.3
-MOLD_TARGET=mold-${MOLD_VERSION}-$(uname -m)-linux
-curl -fsSL "https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/${MOLD_TARGET}.tar.gz" \
-    --output "$TEMP/${MOLD_TARGET}.tar.gz"
-tar \
-    -xvf "${TEMP}/${MOLD_TARGET}.tar.gz" \
-    -C "${TEMP}"
-cp "${TEMP}/${MOLD_TARGET}/bin/mold" /usr/bin/mold
-cp "${TEMP}/${MOLD_TARGET}/lib/mold/mold-wrapper.so" /usr/bin/mold-wrapper.so
-rm -rf "$TEMP"
-
-# Now configure Cargo to use our rustc wrapper script.
-mkdir "${HOME}/.cargo"
-cat <<EOF >>"${HOME}/.cargo/config.toml"
-[target.x86_64-unknown-linux-gnu]
-linker="clang"
-rustflags = ["-C", "link-arg=-fuse-ld=/usr/bin/mold"]
-EOF
--- ci/prepare.sh
+++ ci/prepare.sh
@@ -25,3 +25,33 @@ if [[ "${CI-}" == "true" && "${PROFILE-}" != "debug" ]]; then
 fi
 
 bash ci/install_protoc.sh
+
+# Install mold, because the system linker wastes a bunch of time.
+#
+# Notably, we don't install/configure it when we're going to do anything with `cross`, as `cross` takes the Cargo
+# configuration from the host system and ships it over...  which isn't good when we're overriding the `rustc-wrapper`
+# and all of that.
+MOLD_VERSION=2.3.3
+CARGO_DIR="${HOME}/.cargo"
+if [[ "$(${CARGO_DIR}/bin/mold -v | awk '{print $2}')" != "$MOLD_VERSION" ]]; then
+  # We explicitly put `mold-wrapper.so` right beside `mold` itself because it's hard-coded to look in the same directory
+  # first when trying to load the shared object, so we can dodge having to care about the "right" lib folder to put it in.
+  TEMP=$(mktemp -d)
+  MOLD_TARGET=mold-${MOLD_VERSION}-$(uname -m)-linux
+  curl -fsSL "https://github.com/rui314/mold/releases/download/v${MOLD_VERSION}/${MOLD_TARGET}.tar.gz" \
+      --output "$TEMP/${MOLD_TARGET}.tar.gz"
+  tar \
+      -xvf "${TEMP}/${MOLD_TARGET}.tar.gz" \
+      -C "${TEMP}"
+  cp "${TEMP}/${MOLD_TARGET}/bin/mold" "${CARGO_DIR}/bin/mold"
+  cp "${TEMP}/${MOLD_TARGET}/lib/mold/mold-wrapper.so" "${CARGO_DIR}/bin/mold-wrapper.so"
+  rm -rf "$TEMP"
+fi
+
+# Now configure Cargo to use our rustc wrapper script.
+# echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> "${HOME}/.bash_profile"
+cat <<EOF >>"${CARGO_DIR}/config.toml"
+[target.x86_64-unknown-linux-gnu]
+linker="clang"
+rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+EOF
