72e2ac84da1b4922f07e80c76b7e22089b86c02a
github-action-benchmark <github@users.noreply.github.com>
2023-11-22 12:41:56 +0000

    add Build (customSmallerIsBetter) benchmark result for 3ece3d81311bc7ee72a9aad41eb4257c8a6acb52

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700656453615,
+  "lastUpdate": 1700656916576,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -18124,40 +18124,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "af09841d762c8f4fa07812973efe5d98947fef38",
-          "message": "Sinks/influxdb: org should not be a template value (#1323)\n\n* org should not be a `Template`",
-          "timestamp": "2023-10-04T21:57:46+08:00",
-          "tree_id": "02eb48393ed2669598c7f8046d709d4ccd0fc109",
-          "url": "https://github.com/f1shl3gs/vertex/commit/af09841d762c8f4fa07812973efe5d98947fef38"
-        },
-        "date": 1696428947488,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 937,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 42703.5,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "585ffb19ae940e5acdbfd898034565c2d417a7bb",
           "message": "fix statfs (#1324)",
           "timestamp": "2023-10-05T15:02:13+08:00",
@@ -21507,6 +21473,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 52362.3,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3ece3d81311bc7ee72a9aad41eb4257c8a6acb52",
+          "message": "lib/event: impl `Tags` with cow (#1431)\n\n* shuffle bench keys\r\n* impl Tags with cow",
+          "timestamp": "2023-11-22T20:30:32+08:00",
+          "tree_id": "0e42ac0c861febb7eebd95e923740a184c27265c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3ece3d81311bc7ee72a9aad41eb4257c8a6acb52"
+        },
+        "date": 1700656902587,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 585,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 41108.3,
             "unit": "KiB"
           }
         ]
