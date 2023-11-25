010c6620ffe953d4422d767de7cbdb6b24021ea6
f1shl3gs <fishlegs.engerman@gmail.com>
2023-11-25 21:10:00 +0800

    fix

--- benches/node_source_hwmon.rs
+++ benches/node_source_hwmon.rs
@@ -1,12 +1,14 @@
+use std::path::PathBuf;
+
 use criterion::{criterion_group, criterion_main, measurement::WallTime, Criterion};
 use vertex::sources::node::hwmon::gather;
 
 pub fn hwmon_gather(c: &mut Criterion) -> &mut Criterion<WallTime> {
-    let path = "tests/fixtures/sys";
+    let path: PathBuf = "tests/fixtures/sys".into();
 
     c.bench_function("hwmon_gather", |b| {
         b.to_async(tokio::runtime::Runtime::new().unwrap())
-            .iter(|| gather(path))
+            .iter(|| gather(path.clone()))
     })
 }
 
