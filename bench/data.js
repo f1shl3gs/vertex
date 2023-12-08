5864a6836a2c4613abcaf41f60c7fc3d2e06e135
github-action-benchmark <github@users.noreply.github.com>
2023-12-08 02:50:38 +0000

    add condition (customSmallerIsBetter) benchmark result for ce9fcbf7ed29b8923ba001dda6019b13107a3f2e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702003821872,
+  "lastUpdate": 1702003838280,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "a9a8b144ce60159ca1b2e1d9d3199f5f070143d7",
-          "message": "implement healthcheck (#1330)",
-          "timestamp": "2023-10-06T19:11:08+08:00",
-          "tree_id": "599505184170d8956116fca823787f4a73ab0d96",
-          "url": "https://github.com/f1shl3gs/vertex/commit/a9a8b144ce60159ca1b2e1d9d3199f5f070143d7"
-        },
-        "date": 1696590963089,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 30.889,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 30.543,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 32.727,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 185.35,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 66.054,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "92760bd611859b03b1e7c40500fa605aef415b28",
           "message": "bump libc to 0.2.149 (#1331)",
           "timestamp": "2023-10-07T14:14:13+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 39.184,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ce9fcbf7ed29b8923ba001dda6019b13107a3f2e",
+          "message": "chore(deps): bump once_cell from 1.18.0 to 1.19.0 (#1452)\n\n* chore(deps): bump once_cell from 1.18.0 to 1.19.0\r\n\r\nBumps [once_cell](https://github.com/matklad/once_cell) from 1.18.0 to 1.19.0.\r\n- [Changelog](https://github.com/matklad/once_cell/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/matklad/once_cell/compare/v1.18.0...v1.19.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: once_cell\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\n* add `default-features = false` to `once_cell`\r\n\r\n---------\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\r\nCo-authored-by: f1shl3gs <fishlegs.engerman@gmail.com>",
+          "timestamp": "2023-12-08T10:47:26+08:00",
+          "tree_id": "12d23b9dc7e75283822095215278360d66ad0dbe",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ce9fcbf7ed29b8923ba001dda6019b13107a3f2e"
+        },
+        "date": 1702003824155,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 17.932,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 17.622,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 17.874,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 110.41,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 40.15,
             "unit": "ns/op"
           }
         ]
