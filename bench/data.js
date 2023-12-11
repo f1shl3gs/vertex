bfe6d75164f2938543a624ed7bff9d2dad137a99
github-action-benchmark <github@users.noreply.github.com>
2023-12-11 13:40:00 +0000

    add condition (customSmallerIsBetter) benchmark result for 91b73b48e234cf1d5b75ec228badcfbd16c84385

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702301984229,
+  "lastUpdate": 1702302000768,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -6820,55 +6820,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "732acf597c88b584b31a4107ad1e5768c1619d84",
-          "message": "bump proc-macro2 to 1.0.69 (#1336)",
-          "timestamp": "2023-10-09T12:26:00+08:00",
-          "tree_id": "a22ea344959c222c9e800747640af0ae0a12428b",
-          "url": "https://github.com/f1shl3gs/vertex/commit/732acf597c88b584b31a4107ad1e5768c1619d84"
-        },
-        "date": 1696825800119,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "condition/ordering",
-            "value": 26.399,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/contains",
-            "value": 26.935,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/nested",
-            "value": 25.505,
-            "unit": "ns/op"
-          },
-          {
-            "name": "condition/match",
-            "value": 147.39,
-            "unit": "ns/op"
-          },
-          {
-            "name": "",
-            "value": 56.66,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "a752b17b6563aa8f4f2299437d136e93e8b914ad",
           "message": "bump tokio to 1.33.0 (#1337)",
           "timestamp": "2023-10-09T20:36:14+08:00",
@@ -11703,6 +11654,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "",
             "value": 40.12,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "91b73b48e234cf1d5b75ec228badcfbd16c84385",
+          "message": "optimize for BTreeMap's `contains_key` and `insert` (#1458)",
+          "timestamp": "2023-12-11T21:36:22+08:00",
+          "tree_id": "acd511892da75e43fa1441e2b697d47423782563",
+          "url": "https://github.com/f1shl3gs/vertex/commit/91b73b48e234cf1d5b75ec228badcfbd16c84385"
+        },
+        "date": 1702301986496,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "condition/ordering",
+            "value": 18.02,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/contains",
+            "value": 17.626,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/nested",
+            "value": 17.654,
+            "unit": "ns/op"
+          },
+          {
+            "name": "condition/match",
+            "value": 110.29,
+            "unit": "ns/op"
+          },
+          {
+            "name": "",
+            "value": 39.569,
             "unit": "ns/op"
           }
         ]
