6ea47add4c22eaf388aa327e4d86ed6b29b20bf2
github-action-benchmark <github@users.noreply.github.com>
2024-01-01 11:55:54 +0000

    add Build (customSmallerIsBetter) benchmark result for 240e95a9d8ce1ee68979ccec2db139a3dd99d906

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704109687310,
+  "lastUpdate": 1704110154008,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -18124,40 +18124,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "c4886a27244a1cca61cd738e8c1f2b8c8872cafd",
-          "message": "chore(deps): bump http from 0.2.9 to 0.2.10 (#1413)\n\nBumps [http](https://github.com/hyperium/http) from 0.2.9 to 0.2.10.\r\n- [Release notes](https://github.com/hyperium/http/releases)\r\n- [Changelog](https://github.com/hyperium/http/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/hyperium/http/compare/v0.2.9...v0.2.10)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: http\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-11-11T17:55:16+08:00",
-          "tree_id": "a6e056ff6def6e4df5540896b588643a385be8a9",
-          "url": "https://github.com/f1shl3gs/vertex/commit/c4886a27244a1cca61cd738e8c1f2b8c8872cafd"
-        },
-        "date": 1699697242410,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 638,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 52756.4,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "2fdace40f1955dcfa5be7dd8635a404c2af6023c",
           "message": "chore(deps): bump proptest from 1.3.1 to 1.4.0 (#1414)\n\nBumps [proptest](https://github.com/proptest-rs/proptest) from 1.3.1 to 1.4.0.\r\n- [Release notes](https://github.com/proptest-rs/proptest/releases)\r\n- [Changelog](https://github.com/proptest-rs/proptest/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/proptest-rs/proptest/compare/v1.3.1...v1.4.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: proptest\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2023-11-11T18:03:04+08:00",
@@ -21502,6 +21468,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 573,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 41475.6,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "240e95a9d8ce1ee68979ccec2db139a3dd99d906",
+          "message": "fix sanity (#1525)",
+          "timestamp": "2024-01-01T19:44:19+08:00",
+          "tree_id": "701d5df91c60bb065338d65733086547147d270c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/240e95a9d8ce1ee68979ccec2db139a3dd99d906"
+        },
+        "date": 1704110139797,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 592,
             "unit": "s"
           },
           {
