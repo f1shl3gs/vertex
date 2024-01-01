e4953f500d744666c2bb1c800b9a048436c01ceb
github-action-benchmark <github@users.noreply.github.com>
2024-01-01 17:48:26 +0000

    add metrics (customSmallerIsBetter) benchmark result for 23f6a9bba5f2561a98e1f33e51d0081094c9c93a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704131291262,
+  "lastUpdate": 1704131306119,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "e88bea920c4c6989acbad122372a964a1a36c856",
-          "message": "chore(deps): bump tempfile from 3.8.0 to 3.8.1 (#1389)\n\nBumps [tempfile](https://github.com/Stebalien/tempfile) from 3.8.0 to 3.8.1.\r\n- [Changelog](https://github.com/Stebalien/tempfile/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/Stebalien/tempfile/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: tempfile\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2023-10-27T13:10:10+08:00",
-          "tree_id": "1a649abc14d0905486b8fdef0a3606e2ba3f02a7",
-          "url": "https://github.com/f1shl3gs/vertex/commit/e88bea920c4c6989acbad122372a964a1a36c856"
-        },
-        "date": 1698383586241,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 89.913,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 186.84,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 279.38,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 181.15,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "23f6a9bba5f2561a98e1f33e51d0081094c9c93a",
+          "message": "chore(deps): bump syn from 2.0.43 to 2.0.44 (#1528)\n\nBumps [syn](https://github.com/dtolnay/syn) from 2.0.43 to 2.0.44.\r\n- [Release notes](https://github.com/dtolnay/syn/releases)\r\n- [Commits](https://github.com/dtolnay/syn/compare/2.0.43...2.0.44)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: syn\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-01-02T01:45:54+08:00",
+          "tree_id": "1c0e0c114fff7f1a4d7cdfb33244dd1978d9ce5a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/23f6a9bba5f2561a98e1f33e51d0081094c9c93a"
+        },
+        "date": 1704131291073,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.525,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 90.827,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 183.96,
             "unit": "ns/op"
           }
         ]
