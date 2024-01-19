70a9464a183e08eb8216ccdd3d3e37bb3b91030e
f1shl3gs <fishlegs.engerman@gmail.com>
2024-01-19 12:54:54 +0800

    simplify (#1563)

--- lib/sysinfo/src/lib.rs
+++ lib/sysinfo/src/lib.rs
@@ -1,24 +1,5 @@
 #[cfg(unix)]
 mod unix;
 
-pub fn os() -> Option<String> {
-    #[cfg(unix)]
-    return unix::os_version();
-
-    #[cfg(not(unix))]
-    None
-}
-
-pub fn machine_id() -> std::io::Result<String> {
-    #[cfg(unix)]
-    return unix::machine_id();
-
-    #[cfg(not(unix))]
-    Err(std::io::Error::new(
-        std::io::ErrorKind::Unsupported,
-        "unsupported OS",
-    ))
-}
-
 #[cfg(unix)]
-pub use unix::kernel;
+pub use unix::*;
