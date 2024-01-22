26beddfa850e7873e5f741514053fcf7995bbc78
github-action-benchmark <github@users.noreply.github.com>
2024-01-22 04:47:41 +0000

    add event (customSmallerIsBetter) benchmark result for f703ccdbfd80fe39ea314b5087431e0778cd6434

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705898840969,
+  "lastUpdate": 1705898861039,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -25428,50 +25428,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "f22a1d3f34dfda31af21e2efad150aadc4eabff1",
-          "message": "lib/event: reimplement Value::String with String (#1438)",
-          "timestamp": "2023-11-26T21:12:15+08:00",
-          "tree_id": "f385436572ec8930dea3c7bac6ff3362f922dc55",
-          "url": "https://github.com/f1shl3gs/vertex/commit/f22a1d3f34dfda31af21e2efad150aadc4eabff1"
-        },
-        "date": 1701004494771,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "tags/insert/1",
-            "value": 33.056,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/5",
-            "value": 153.5,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/10",
-            "value": 319.1,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/20",
-            "value": 653.17,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fishlegs.engerman@gmail.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "967382afecc999c09e34d5ada72ae91c159a3566",
           "message": "add dmi and softirqs (#1439)",
           "timestamp": "2023-11-27T04:13:48+08:00",
@@ -29811,6 +29767,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 664.47,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f703ccdbfd80fe39ea314b5087431e0778cd6434",
+          "message": "bump kube to 0.88.0, k8s-openapi to 0.21.0 (#1572)",
+          "timestamp": "2024-01-22T12:44:56+08:00",
+          "tree_id": "e22ed3c6fdca1fa9c903eb1ec34227f21f95b51a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f703ccdbfd80fe39ea314b5087431e0778cd6434"
+        },
+        "date": 1705898847041,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 31.875,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 146.48,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 336.66,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 646.99,
             "unit": "ns/op"
           }
         ]
