6ace3e9ccbea0c7cfe0a540c3d1dd57ee7d2254d
github-action-benchmark <github@users.noreply.github.com>
2024-03-18 12:27:44 +0000

    add metrics (customSmallerIsBetter) benchmark result for 0dbeb499846ed940bb4c043fcc9b3ef64e2c71f5

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710764833677,
+  "lastUpdate": 1710764864268,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8beaaf9f05201e4c7e98f3af37c0f3e8e8384b44",
-          "message": "chore(deps): bump serde_json from 1.0.110 to 1.0.111 (#1542)\n\nBumps [serde_json](https://github.com/serde-rs/json) from 1.0.110 to 1.0.111.\r\n- [Release notes](https://github.com/serde-rs/json/releases)\r\n- [Commits](https://github.com/serde-rs/json/compare/v1.0.110...v1.0.111)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde_json\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-01-04T22:04:13+08:00",
-          "tree_id": "814c28e4238b74059816b95b3ae1e86b2e61bfe8",
-          "url": "https://github.com/f1shl3gs/vertex/commit/8beaaf9f05201e4c7e98f3af37c0f3e8e8384b44"
-        },
-        "date": 1704377228380,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.808,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 91.866,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 183.5,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 175.44,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0dbeb499846ed940bb4c043fcc9b3ef64e2c71f5",
+          "message": "add lossy to decoding config (#1659)",
+          "timestamp": "2024-03-18T20:25:01+08:00",
+          "tree_id": "e42f1e7e57c2ba09904fd910d3a8edcf861aca64",
+          "url": "https://github.com/f1shl3gs/vertex/commit/0dbeb499846ed940bb4c043fcc9b3ef64e2c71f5"
+        },
+        "date": 1710764849374,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.385,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 89.492,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 174.61,
             "unit": "ns/op"
           }
         ]
