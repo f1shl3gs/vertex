19871b2bf255fe7cbdba896ad5c5c7a1ddfbd862
github-action-benchmark <github@users.noreply.github.com>
2024-04-03 09:13:03 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 36965fa454eff0fa336043174f35ef54275a51d6

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712135582172,
+  "lastUpdate": 1712135583382,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "0b4a704199c7b9dce197d734b34674770ff940d1",
-          "message": "chore(deps): bump indexmap from 2.1.0 to 2.2.0 (#1584)\n\n* chore(deps): bump indexmap from 2.1.0 to 2.2.0\r\n\r\nBumps [indexmap](https://github.com/indexmap-rs/indexmap) from 2.1.0 to 2.2.0.\r\n- [Changelog](https://github.com/indexmap-rs/indexmap/blob/master/RELEASES.md)\r\n- [Commits](https://github.com/indexmap-rs/indexmap/compare/2.1.0...2.2.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: indexmap\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\n* fix deprecate method warning\r\n\r\n* replace swap_remove with shift_remove to keep orders of the IndexMap/IndexSet\r\n\r\n---------\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\r\nCo-authored-by: f1shl3gs <fishlegs.engerman@gmail.com>",
-          "timestamp": "2024-01-29T03:58:37+08:00",
-          "tree_id": "a9f4485ea7196d4f938c2f9a5e22028f67ac06e1",
-          "url": "https://github.com/f1shl3gs/vertex/commit/0b4a704199c7b9dce197d734b34674770ff940d1"
-        },
-        "date": 1706472038338,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 537530,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 530700,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fishlegs.engerman@gmail.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "36965fa454eff0fa336043174f35ef54275a51d6",
+          "message": "cleanup (#1691)",
+          "timestamp": "2024-04-03T17:10:33+08:00",
+          "tree_id": "8e38d8f6344aaf211cca5f97dd929b11b633ac9c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/36965fa454eff0fa336043174f35ef54275a51d6"
+        },
+        "date": 1712135568101,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 532850,
             "unit": "ns/op"
           }
         ]
