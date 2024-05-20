c687cfd4d18ef492b12b6b6e621fc61d3ac939ab
f1shl3gs <fishlegs.engerman@gmail.com>
2024-05-20 13:18:31 +0800

    remove netlink

--- src/sources/node/tcpstat.rs
+++ src/sources/node/tcpstat.rs
@@ -1,15 +1,11 @@
+use std::io;
+use std::os::fd::AsRawFd;
+use std::pin::Pin;
+use std::task::{ready, Context, Poll};
+
 use event::{tags, Metric};
-use netlink_packet_core::{
-    constants::{NLM_F_DUMP, NLM_F_REQUEST},
-    NetlinkHeader, NetlinkMessage, NetlinkPayload,
-};
-use netlink_packet_sock_diag::{
-    inet::{ExtensionFlags, InetRequest, SocketId, StateFlags},
-    SockDiagMessage, AF_INET, AF_INET6, IPPROTO_TCP, TCP_CLOSE, TCP_CLOSE_WAIT, TCP_CLOSING,
-    TCP_ESTABLISHED, TCP_FIN_WAIT1, TCP_FIN_WAIT2, TCP_LAST_ACK, TCP_LISTEN, TCP_SYN_RECV,
-    TCP_SYN_SENT, TCP_TIME_WAIT,
-};
-use netlink_sys::{protocols::NETLINK_SOCK_DIAG, AsyncSocket, AsyncSocketExt, TokioSocket};
+use tokio::io::unix::AsyncFd;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadBuf};
 
 use super::Error;
 
@@ -42,7 +38,8 @@ macro_rules! state_metric {
 }
 
 pub async fn gather() -> Result<Vec<Metric>, Error> {
-    let (v4, v6) = tokio::join!(fetch_tcp_stats(AF_INET), fetch_tcp_stats(AF_INET6));
+    let v4 = fetch_tcp_stats(AF_INET).await?;
+    let v6 = fetch_tcp_stats(AF_INET6).await?;
 
     Ok(vec![
         state_metric!("established", v4.established + v6.established),
@@ -59,56 +56,234 @@ pub async fn gather() -> Result<Vec<Metric>, Error> {
     ])
 }
 
-async fn fetch_tcp_stats(family: u8) -> Statistics {
-    let mut stats = Statistics::default();
-    let mut socket = TokioSocket::new(NETLINK_SOCK_DIAG).unwrap();
+/// Netlink Protocol type
+const NETLINK_SOCK_DIAG: u16 = 4;
+const SOCK_DIAG_BY_FAMILY: u16 = 20;
 
-    let mut header = NetlinkHeader::default();
-    header.flags = NLM_F_REQUEST | NLM_F_DUMP;
+/// The message is ignored.
+pub const NLMSG_NOOP: u16 = 1;
+/// The message signals an error and the payload contains a nlmsgerr structure.
+/// This can be looked at as a NACK and typically it is from FEC to CPC.
+pub const NLMSG_ERROR: u16 = 2;
+/// The message terminates a multipart message.
+/// Data lost
+pub const NLMSG_DONE: u16 = 3;
+pub const NLMSG_OVERRUN: u16 = 4;
 
-    let socket_id = match family {
-        AF_INET => SocketId::new_v4(),
-        AF_INET6 => SocketId::new_v6(),
-        _ => panic!("unknown family"),
-    };
+const AF_INET: u8 = 2;
+const AF_INET6: u8 = 10;
 
-    let mut packet = NetlinkMessage::new(
-        header,
-        SockDiagMessage::InetRequest(InetRequest {
-            family,
-            protocol: IPPROTO_TCP,
-            extensions: ExtensionFlags::empty(),
-            states: StateFlags::all(),
-            socket_id,
+/// (both server and client) represents an open connection, data
+/// received can be delivered to the user. The normal state for the
+/// data transfer phase of the connection.
+pub const TCP_ESTABLISHED: u8 = 1;
+/// (client) represents waiting for a matching connection request
+/// after having sent a connection request.
+pub const TCP_SYN_SENT: u8 = 2;
+/// (server) represents waiting for a confirming connection request
+/// acknowledgment after having both received and sent a connection
+/// request.
+pub const TCP_SYN_RECV: u8 = 3;
+/// (both server and client) represents waiting for a connection
+/// termination request from the remote TCP, or an acknowledgment of
+/// the connection termination request previously sent.
+pub const TCP_FIN_WAIT1: u8 = 4;
+/// (both server and client) represents waiting for a connection
+/// termination request from the remote TCP.
+pub const TCP_FIN_WAIT2: u8 = 5;
+/// (either server or client) represents waiting for enough time to
+/// pass to be sure the remote TCP received the acknowledgment of its
+/// connection termination request.
+pub const TCP_TIME_WAIT: u8 = 6;
+/// (both server and client) represents no connection state at all.
+pub const TCP_CLOSE: u8 = 7;
+/// (both server and client) represents waiting for a connection
+/// termination request from the local user.
+pub const TCP_CLOSE_WAIT: u8 = 8;
+/// (both server and client) represents waiting for an acknowledgment
+/// of the connection termination request previously sent to the
+/// remote TCP (which includes an acknowledgment of its connection
+/// termination request).
+pub const TCP_LAST_ACK: u8 = 9;
+/// (server) represents waiting for a connection request from any
+/// remote TCP and port.
+pub const TCP_LISTEN: u8 = 10;
+/// (both server and client) represents waiting for a connection termination
+/// request acknowledgment from the remote TCP.
+pub const TCP_CLOSING: u8 = 11;
+
+/// Length of a Netlink packet header.
+const NETLINK_HEADER_LEN: usize = 16;
+
+struct NetlinkSocket {
+    inner: AsyncFd<libc::c_int>,
+}
+
+impl NetlinkSocket {
+    fn new() -> io::Result<Self> {
+        let fd = unsafe {
+            let ret = libc::socket(
+                libc::PF_NETLINK,
+                libc::SOCK_DGRAM | libc::SOCK_CLOEXEC | libc::SOCK_NONBLOCK,
+                NETLINK_SOCK_DIAG as libc::c_int,
+            );
+
+            if ret < 0 {
+                return Err(io::Error::last_os_error());
+            }
+
+            ret
+        };
+
+        Ok(NetlinkSocket {
+            inner: AsyncFd::new(fd)?,
         })
-        .into(),
-    );
+    }
+}
 
-    packet.finalize();
+impl AsyncRead for NetlinkSocket {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        loop {
+            let mut guard = ready!(self.inner.poll_read_ready(cx))?;
 
-    let mut buf = vec![0; packet.header.length as usize];
-    assert_eq!(buf.len(), packet.buffer_len());
-    packet.serialize(&mut buf[..]);
+            let unfilled = buf.initialize_unfilled();
+            #[allow(clippy::blocks_in_conditions)]
+            match guard.try_io(|inner| {
+                let ret = unsafe {
+                    libc::recv(
+                        inner.as_raw_fd(),
+                        unfilled.as_mut_ptr() as *mut libc::c_void,
+                        unfilled.len(),
+                        0,
+                    )
+                };
+                if ret == -1 {
+                    return Err(io::Error::last_os_error());
+                }
 
-    if let Err(e) = socket.send(&buf[..]).await {
-        panic!("send error {}", e);
+                Ok(ret as usize)
+            }) {
+                Ok(Ok(len)) => {
+                    buf.advance(len);
+                    return Poll::Ready(Ok(()));
+                }
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+}
+
+impl AsyncWrite for NetlinkSocket {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        loop {
+            let mut guard = ready!(self.inner.poll_write_ready(cx))?;
+
+            #[allow(clippy::blocks_in_conditions)]
+            match guard.try_io(|inner| {
+                let ret = unsafe {
+                    libc::send(
+                        inner.as_raw_fd(),
+                        buf.as_ptr() as *const libc::c_void,
+                        buf.len(),
+                        libc::MSG_NOSIGNAL,
+                    )
+                };
+                if ret < 0 {
+                    return Err(io::Error::last_os_error());
+                }
+
+                Ok(ret as usize)
+            }) {
+                Ok(res) => return Poll::Ready(res),
+                Err(_would_block) => continue,
+            }
+        }
     }
 
-    let mut buf = vec![0; 8 * 1024];
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        // Is netlink flush is a no-op !?
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        let ret = unsafe { libc::shutdown(self.inner.as_raw_fd(), libc::SHUT_RDWR) };
+        if ret == -1 {
+            return Poll::Ready(Err(io::Error::last_os_error()));
+        }
+
+        Poll::Ready(Ok(()))
+    }
+}
+
+async fn fetch_tcp_stats(family: u8) -> io::Result<Statistics> {
+    #[rustfmt::skip]
+        let msg = [
+        // u32, length of the netlink packet, including the header and the payload
+        72u8, 0, 0, 0,
+        // u16, message type, SOCK_DIAG_BY_FAMILY
+        20, 0,
+        // u16, flags NLM_F_REQUEST | NLM_F_DUMP
+        1, 3,
+        // u32, sequence number
+        1, 0, 0, 0,
+        // u32, port number
+        0, 0, 0, 0,
+
+        // payload
+        family, 6, 0, 0, 254, 15, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ];
+
+    let mut sock = NetlinkSocket::new()?;
+    sock.write_all(&msg).await?;
+
+    let mut stats = Statistics::default();
+
+    let mut buf = [0u8; 8 * 1024];
+    let mut read_offset = 0;
     'RECV: loop {
-        buf.clear();
-        socket.recv(&mut buf).await.unwrap();
+        let cnt = sock.read(&mut buf[read_offset..]).await? + read_offset;
+        read_offset = 0;
 
-        let size = buf.len();
+        if cnt < 4 {
+            read_offset = cnt;
+            continue;
+        }
+
         let mut offset = 0;
-        loop {
-            let bytes = &buf[offset..];
-            let pkt = <NetlinkMessage<SockDiagMessage>>::deserialize(bytes).unwrap();
+        while offset < cnt {
+            let len = u32::from_ne_bytes((&buf[offset..offset + 4]).try_into().unwrap()) as usize;
+            if cnt - offset < len {
+                // not enough
+                read_offset = cnt - offset;
+                buf.copy_within(offset..cnt, 0);
+                break;
+            }
 
-            match pkt.payload {
-                NetlinkPayload::Noop => {}
-                NetlinkPayload::InnerMessage(SockDiagMessage::InetResponse(resp)) => {
-                    match resp.header.state {
+            // full packet
+            let msg_typ = u16::from_ne_bytes((&buf[offset + 4..offset + 6]).try_into().unwrap());
+            match msg_typ {
+                SOCK_DIAG_BY_FAMILY => {
+                    let state = match buf[offset + NETLINK_HEADER_LEN] {
+                        AF_INET | AF_INET6 => buf[offset + NETLINK_HEADER_LEN + 1],
+                        _family => continue,
+                    };
+
+                    match state {
                         TCP_ESTABLISHED => stats.established += 1,
                         TCP_SYN_SENT => stats.syn_sent += 1,
                         TCP_SYN_RECV => stats.syn_recv += 1,
@@ -120,30 +295,24 @@ async fn fetch_tcp_stats(family: u8) -> Statistics {
                         TCP_LAST_ACK => stats.last_ack += 1,
                         TCP_LISTEN => stats.listen += 1,
                         TCP_CLOSING => stats.closing += 1,
-                        _ => {}
+                        _ => {
+                            error!(message = "unknown tcp state", ?state);
+                        }
                     }
                 }
-                NetlinkPayload::Done(_msg) => {
-                    break 'RECV;
-                }
-                NetlinkPayload::Error(err) => {
-                    error!(message = "Invalid netlink payload", ?err);
-                    break 'RECV;
-                }
-                NetlinkPayload::Overrun(_) => {
-                    panic!("overrun")
-                }
-                _ => {
-                    panic!("Unknown payload type")
-                }
+                NLMSG_NOOP => continue,
+                NLMSG_DONE => break 'RECV,
+                NLMSG_ERROR => return Err(io::Error::other("overrun packet")),
+                _typ => return Err(io::Error::other("invalid packet type")),
             }
 
-            offset += pkt.header.length as usize;
-            if offset == size || pkt.header.length == 0 {
-                break;
-            }
+            offset += len;
+        }
+
+        if cnt < buf.len() {
+            return Ok(stats);
         }
     }
 
-    stats
+    Ok(stats)
 }
