bafcf7f66ff7fb8db8219a8f992f64509e491e74
github-action-benchmark <github@users.noreply.github.com>
2024-10-10 03:02:50 +0000

    add prometheus (customSmallerIsBetter) benchmark result for c933082cd80bbb2f73c96b7f32119995485fc66f

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728364345948,
+  "lastUpdate": 1728529370651,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "05263f2ab921df2a540bce76fdf1a6b1e913f6d9",
-          "message": "remove unnecessary feature flag (#1833)",
-          "timestamp": "2024-07-19T20:59:57+08:00",
-          "tree_id": "e3cf2426f8cc35ce2f5cb136385e3ff618795c3f",
-          "url": "https://github.com/f1shl3gs/vertex/commit/05263f2ab921df2a540bce76fdf1a6b1e913f6d9"
-        },
-        "date": 1721394136013,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 543550,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 534840,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c933082cd80bbb2f73c96b7f32119995485fc66f",
+          "message": "chore(deps): bump kube from 0.95.0 to 0.96.0 (#1935)\n\nBumps [kube](https://github.com/kube-rs/kube) from 0.95.0 to 0.96.0.\r\n- [Release notes](https://github.com/kube-rs/kube/releases)\r\n- [Changelog](https://github.com/kube-rs/kube/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/kube-rs/kube/compare/0.95.0...0.96.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: kube\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-10-10T11:00:01+08:00",
+          "tree_id": "448ac9b5ad266ec1a1adc2f7e9af96410a47d6a9",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c933082cd80bbb2f73c96b7f32119995485fc66f"
+        },
+        "date": 1728529354245,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 541310,
             "unit": "ns/op"
           }
         ]
