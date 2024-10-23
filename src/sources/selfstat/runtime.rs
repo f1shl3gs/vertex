cae932ace26eaed76f54111818dd90f5a7e6b316
f1shl3gs <fishlegs.engerman@gmail.com>
2024-10-23 09:34:06 +0800

    add global_queue_depth

--- src/sources/selfstat/runtime.rs
+++ src/sources/selfstat/runtime.rs
@@ -15,5 +15,10 @@ pub fn metrics() -> Vec<Metric> {
             "The current number of alive tasks in the runtime",
             metrics.num_alive_tasks(),
         ),
+        Metric::gauge(
+            "tokio_global_queue_depth",
+            "The number of currently scheduled in the runtime's global queue",
+            metrics.global_queue_depth(),
+        ),
     ]
 }
