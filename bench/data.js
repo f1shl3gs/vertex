7bfa1d9e90918fd9405499b3ce4d8ec145d0e9ff
github-action-benchmark <github@users.noreply.github.com>
2024-11-17 12:15:51 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 3c1e824cf30190adc6d110b696120dd8288cf2af

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731834783844,
+  "lastUpdate": 1731845751592,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "5f100ac2407d94211b5d227827ef4c4f49c02f18",
-          "message": "cargo update (#1884)",
-          "timestamp": "2024-08-25T14:28:18+08:00",
-          "tree_id": "a4a545050037448dbb95bbaa8e5e2b2a2ed5c78f",
-          "url": "https://github.com/f1shl3gs/vertex/commit/5f100ac2407d94211b5d227827ef4c4f49c02f18"
-        },
-        "date": 1724567427872,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 524720,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 531680,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3c1e824cf30190adc6d110b696120dd8288cf2af",
+          "message": "remove unnecessary implements (#1986)",
+          "timestamp": "2024-11-17T20:13:36+08:00",
+          "tree_id": "72697c1f2afe871107762afe419024e06aaae6c0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3c1e824cf30190adc6d110b696120dd8288cf2af"
+        },
+        "date": 1731845734358,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 530160,
             "unit": "ns/op"
           }
         ]
