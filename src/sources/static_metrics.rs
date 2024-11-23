4e8c24751a42ee33e0fa0960d9b191f66003dbae
f1shl3gs <26081140+f1shl3gs@users.noreply.github.com>
2024-11-23 15:21:18 +0800

    add sources/static_metrics (#1995)
    * add sources/static_metrics

---
+++ src/sources/static_metrics.rs
@@ -0,0 +1,248 @@
+use std::time::Duration;
+
+use async_trait::async_trait;
+use chrono::Utc;
+use configurable::{configurable_component, Configurable};
+use event::tags::Tags;
+use event::{Metric, MetricValue};
+use framework::config::{default_interval, Output, SourceConfig, SourceContext};
+use framework::{Pipeline, ShutdownSignal, Source};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize, Configurable)]
+struct StaticMetricConfig {
+    name: String,
+    #[serde(default)]
+    description: Option<String>,
+    #[serde(skip_serializing_if = "Tags::is_empty")]
+    tags: Tags,
+    value: MetricValue,
+}
+
+/// Produce static metrics defined in configuration.
+#[configurable_component(source, name = "static_metrics")]
+#[serde(deny_unknown_fields)]
+struct Config {
+    #[serde(default = "default_interval", with = "humanize::duration::serde")]
+    interval: Duration,
+
+    #[serde(default)]
+    metrics: Vec<StaticMetricConfig>,
+}
+
+#[async_trait]
+#[typetag::serde(name = "static_metrics")]
+impl SourceConfig for Config {
+    async fn build(&self, cx: SourceContext) -> crate::Result<Source> {
+        Ok(Box::pin(run(
+            self.interval,
+            self.metrics.clone(),
+            cx.output,
+            cx.shutdown,
+        )))
+    }
+
+    fn outputs(&self) -> Vec<Output> {
+        vec![Output::metrics()]
+    }
+}
+
+async fn run(
+    interval: Duration,
+    metric_configs: Vec<StaticMetricConfig>,
+    mut output: Pipeline,
+    mut shutdown: ShutdownSignal,
+) -> Result<(), ()> {
+    let mut ticker = tokio::time::interval(interval);
+
+    loop {
+        tokio::select! {
+            biased;
+
+            // The first tick completes immediately
+            _ = ticker.tick() => {},
+            _ = &mut shutdown => {
+                break
+            }
+        }
+
+        // generate metrics
+        let ts = Utc::now();
+        let metrics = metric_configs
+            .iter()
+            .map(|conf| {
+                Metric::new(
+                    conf.name.clone(),
+                    conf.description.clone(),
+                    conf.tags.clone(),
+                    ts,
+                    conf.value.clone(),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        if let Err(err) = output.send(metrics).await {
+            warn!(message = "failed to send metrics", ?err);
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::testing::components::{run_and_assert_source_compliance, SOURCE_TAGS};
+    use event::{tags, Bucket, Event, Quantile};
+
+    #[test]
+    fn generate_config() {
+        crate::testing::test_generate_config::<Config>();
+    }
+
+    async fn events_from_config(config: Config) -> Vec<Event> {
+        run_and_assert_source_compliance(config, Duration::from_millis(100), &SOURCE_TAGS).await
+    }
+
+    #[tokio::test]
+    async fn default_empty() {
+        let events = events_from_config(Config {
+            interval: default_interval(),
+            metrics: vec![],
+        })
+        .await;
+
+        assert!(events.is_empty());
+    }
+
+    #[tokio::test]
+    async fn multiple_metrics_with_tags() {
+        let events = events_from_config(Config {
+            interval: default_interval(),
+            metrics: vec![
+                StaticMetricConfig {
+                    name: "gauge".to_string(),
+                    description: None,
+                    tags: tags!("foo" => "bar"),
+                    value: MetricValue::Gauge(1.1),
+                },
+                StaticMetricConfig {
+                    name: "sum".to_string(),
+                    description: Some("sum description".to_string()),
+                    tags: tags!("foo" => "bar"),
+                    value: MetricValue::Sum(2.2),
+                },
+                StaticMetricConfig {
+                    name: "histogram".to_string(),
+                    description: Some("histogram".to_string()),
+                    tags: tags!("bar" => "foo"),
+                    value: MetricValue::Histogram {
+                        count: 2,
+                        sum: 2.2,
+                        buckets: vec![
+                            Bucket {
+                                upper: 1.0,
+                                count: 1,
+                            },
+                            Bucket {
+                                upper: 2.0,
+                                count: 1,
+                            },
+                        ],
+                    },
+                },
+                StaticMetricConfig {
+                    name: "summary".to_string(),
+                    description: Some("summary".to_string()),
+                    tags: tags!("bar" => "foo"),
+                    value: MetricValue::Summary {
+                        count: 3,
+                        sum: 3.3,
+                        quantiles: vec![
+                            Quantile {
+                                quantile: 1.1,
+                                value: 1.1,
+                            },
+                            Quantile {
+                                quantile: 2.2,
+                                value: 2.2,
+                            },
+                            Quantile {
+                                quantile: 3.3,
+                                value: 3.3,
+                            },
+                        ],
+                    },
+                },
+            ],
+        })
+        .await;
+
+        let want = [
+            (
+                "gauge",
+                None,
+                tags!("foo" => "bar"),
+                MetricValue::Gauge(1.1),
+            ),
+            (
+                "sum",
+                Some("sum description".to_string()),
+                tags!("foo" => "bar"),
+                MetricValue::Sum(2.2),
+            ),
+            (
+                "histogram",
+                Some("histogram".to_string()),
+                tags!("bar" => "foo"),
+                MetricValue::Histogram {
+                    count: 2,
+                    sum: 2.2,
+                    buckets: vec![
+                        Bucket {
+                            upper: 1.0,
+                            count: 1,
+                        },
+                        Bucket {
+                            upper: 2.0,
+                            count: 1,
+                        },
+                    ],
+                },
+            ),
+            (
+                "summary",
+                Some("summary".to_string()),
+                tags!("bar" => "foo"),
+                MetricValue::Summary {
+                    count: 3,
+                    sum: 3.3,
+                    quantiles: vec![
+                        Quantile {
+                            quantile: 1.1,
+                            value: 1.1,
+                        },
+                        Quantile {
+                            quantile: 2.2,
+                            value: 2.2,
+                        },
+                        Quantile {
+                            quantile: 3.3,
+                            value: 3.3,
+                        },
+                    ],
+                },
+            ),
+        ];
+
+        assert_eq!(events.len(), want.len());
+        for (got, (name, description, tags, value)) in
+            events.iter().map(|event| event.as_metric()).zip(want)
+        {
+            assert_eq!(got.name(), name);
+            assert_eq!(got.description, description);
+            assert_eq!(got.tags(), &tags);
+            assert_eq!(got.value(), &value);
+        }
+    }
+}
