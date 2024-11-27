30282d6ce6a49febe24052bdce3aa2cd4d1c3c80
github-action-benchmark <github@users.noreply.github.com>
2024-11-27 05:01:02 +0000

    add metrics (customSmallerIsBetter) benchmark result for 854e6c3818b738f991087b03aac3cce422582188

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732683656636,
+  "lastUpdate": 1732683662184,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "2a658166a59cde6b52fd4fd220dc2cadbd81a66f",
-          "message": "bump to 1.81.0 (#1902)",
-          "timestamp": "2024-09-06T13:00:32+08:00",
-          "tree_id": "dab4af2d5985724cefa029b05ad983a1683c1028",
-          "url": "https://github.com/f1shl3gs/vertex/commit/2a658166a59cde6b52fd4fd220dc2cadbd81a66f"
-        },
-        "date": 1725598969483,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.148,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 96.349,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 185.05,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 190.36,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "854e6c3818b738f991087b03aac3cce422582188",
+          "message": "chore(deps): bump libc from 0.2.165 to 0.2.166 (#2003)\n\nBumps [libc](https://github.com/rust-lang/libc) from 0.2.165 to 0.2.166.\r\n- [Release notes](https://github.com/rust-lang/libc/releases)\r\n- [Changelog](https://github.com/rust-lang/libc/blob/0.2.166/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/libc/compare/0.2.165...0.2.166)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: libc\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-11-27T12:58:37+08:00",
+          "tree_id": "bd5a65e21472422790e170365e23dc42825a391e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/854e6c3818b738f991087b03aac3cce422582188"
+        },
+        "date": 1732683644774,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.333,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 95.947,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 201.03,
             "unit": "ns/op"
           }
         ]
