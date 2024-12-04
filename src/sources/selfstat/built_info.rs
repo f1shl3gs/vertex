4e6272cecfb3becc04c181154ce82341582f1b48
f1shl3gs <fishlegs.engerman@gmail.com>
2024-12-04 13:32:46 +0800

    style

--- src/sources/selfstat/built_info.rs
+++ src/sources/selfstat/built_info.rs
@@ -3,18 +3,14 @@ use event::{tags, Metric};
 use crate::built_info;
 
 pub fn built_info() -> Metric {
-    let version = built_info::PKG_VERSION;
-    let target = built_info::TARGET;
-    let debug = built_info::DEBUG;
-
     Metric::gauge_with_tags(
         "build_info",
         "A metric with a constant '1' value labeled by version, revision, branch, and rust version from vertex was built",
         1,
         tags!(
-            "version" => version,
-            "target" => target,
-            "debug" => debug,
+            "version" => built_info::PKG_VERSION,
+            "target" => built_info::TARGET,
+            "debug" => built_info::DEBUG,
             "rustc_version" => built_info::RUSTC_VERSION,
             "rustc_channel" => built_info::RUSTC_CHANNEL,
             "git" => format!("{}/{}", built_info::GIT_BRANCH, built_info::GIT_HASH),
