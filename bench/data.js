c3d2bc7ba9fdfc060b157ca0583cdb41c667318f
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 09:37:43 +0000

    add Build (customSmallerIsBetter) benchmark result for 7cd607c6171b7b0a53fba41d4f74e31bc859a45d

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733823462909,
+  "lastUpdate": 1733823463852,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "c933082cd80bbb2f73c96b7f32119995485fc66f",
-          "message": "chore(deps): bump kube from 0.95.0 to 0.96.0 (#1935)\n\nBumps [kube](https://github.com/kube-rs/kube) from 0.95.0 to 0.96.0.\r\n- [Release notes](https://github.com/kube-rs/kube/releases)\r\n- [Changelog](https://github.com/kube-rs/kube/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/kube-rs/kube/compare/0.95.0...0.96.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: kube\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-10-10T11:00:01+08:00",
-          "tree_id": "448ac9b5ad266ec1a1adc2f7e9af96410a47d6a9",
-          "url": "https://github.com/f1shl3gs/vertex/commit/c933082cd80bbb2f73c96b7f32119995485fc66f"
-        },
-        "date": 1728529826858,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 526,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 35409.5,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "660315c00ece6572976a507dd9dd6dcf785c369c",
           "message": "chore(deps): bump hyper from 1.4.1 to 1.5.0 (#1936)\n\nBumps [hyper](https://github.com/hyperium/hyper) from 1.4.1 to 1.5.0.\r\n- [Release notes](https://github.com/hyperium/hyper/releases)\r\n- [Changelog](https://github.com/hyperium/hyper/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/hyperium/hyper/compare/v1.4.1...v1.5.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: hyper\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2024-10-16T11:05:45+08:00",
@@ -16600,6 +16566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 537,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 35756.1,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7cd607c6171b7b0a53fba41d4f74e31bc859a45d",
+          "message": "chore(deps): bump libc from 0.2.167 to 0.2.168 (#2034)\n\nBumps [libc](https://github.com/rust-lang/libc) from 0.2.167 to 0.2.168.\r\n- [Release notes](https://github.com/rust-lang/libc/releases)\r\n- [Changelog](https://github.com/rust-lang/libc/blob/0.2.168/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/libc/compare/0.2.167...0.2.168)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: libc\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-12-10T17:27:10+08:00",
+          "tree_id": "ca072496bf4476900e61bd4361467cf5e1649d35",
+          "url": "https://github.com/f1shl3gs/vertex/commit/7cd607c6171b7b0a53fba41d4f74e31bc859a45d"
+        },
+        "date": 1733823445490,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 523,
             "unit": "s"
           },
           {
