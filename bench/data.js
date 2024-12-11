da58e35c945a76525c5b034325908a43712bcb02
github-action-benchmark <github@users.noreply.github.com>
2024-12-11 04:26:57 +0000

    add metrics (customSmallerIsBetter) benchmark result for fccfb36cd2592059b680a50d116674384ae01fa9

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733891191636,
+  "lastUpdate": 1733891217732,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "d7e5cfb461edfef94a8725940f9552784542f6dd",
-          "message": "chore(deps): bump rustls from 0.23.14 to 0.23.15 (#1939)\n\nBumps [rustls](https://github.com/rustls/rustls) from 0.23.14 to 0.23.15.\r\n- [Release notes](https://github.com/rustls/rustls/releases)\r\n- [Changelog](https://github.com/rustls/rustls/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/rustls/rustls/compare/v/0.23.14...v/0.23.15)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: rustls\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-10-17T11:42:22+08:00",
-          "tree_id": "2e026ca37b0e6680c66ed8cd116aa891f3f24f2c",
-          "url": "https://github.com/f1shl3gs/vertex/commit/d7e5cfb461edfef94a8725940f9552784542f6dd"
-        },
-        "date": 1729136671964,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.194,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 101.14,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 186.45,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 189.28,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fccfb36cd2592059b680a50d116674384ae01fa9",
+          "message": "chore(deps): bump serde from 1.0.215 to 1.0.216 (#2040)\n\nBumps [serde](https://github.com/serde-rs/serde) from 1.0.215 to 1.0.216.\r\n- [Release notes](https://github.com/serde-rs/serde/releases)\r\n- [Commits](https://github.com/serde-rs/serde/compare/v1.0.215...v1.0.216)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-12-11T12:24:17+08:00",
+          "tree_id": "09a9d2734f6d189fb5be74285d16c5a5da430e99",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fccfb36cd2592059b680a50d116674384ae01fa9"
+        },
+        "date": 1733891200436,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.284,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 97.782,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 182.76,
             "unit": "ns/op"
           }
         ]
