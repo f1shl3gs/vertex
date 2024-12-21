860f1299d5a9783feda57b70b28cc6f2dbe93f91
github-action-benchmark <github@users.noreply.github.com>
2024-12-21 15:39:36 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 3eda802ebe7f5c53a857454b23a08051df8356c9

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734793499008,
+  "lastUpdate": 1734795576166,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "58b8747e48b50df3e86774f7e1b3d96540380a04",
-          "message": "chore(deps): bump regex from 1.11.0 to 1.11.1 (#1960)\n\nBumps [regex](https://github.com/rust-lang/regex) from 1.11.0 to 1.11.1.\r\n- [Release notes](https://github.com/rust-lang/regex/releases)\r\n- [Changelog](https://github.com/rust-lang/regex/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/regex/compare/1.11.0...1.11.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: regex\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-10-25T10:39:58+08:00",
-          "tree_id": "d959239619ebf7b2e60fece577bd5f7241cc3bf9",
-          "url": "https://github.com/f1shl3gs/vertex/commit/58b8747e48b50df3e86774f7e1b3d96540380a04"
-        },
-        "date": 1729824112007,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 527590,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 542130,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3eda802ebe7f5c53a857454b23a08051df8356c9",
+          "message": "support multiple targets to check (#2071)",
+          "timestamp": "2024-12-21T23:37:11+08:00",
+          "tree_id": "9614d1378362b245ea449a1cdcef9b6298c9f2ae",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3eda802ebe7f5c53a857454b23a08051df8356c9"
+        },
+        "date": 1734795558033,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 541280,
             "unit": "ns/op"
           }
         ]
