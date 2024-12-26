9f729c9165b00275dbc9456c263e76a7470f0435
github-action-benchmark <github@users.noreply.github.com>
2024-12-26 04:09:05 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 4aba10abc3fced4da23234d9fcba70edabb482d4

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735180681847,
+  "lastUpdate": 1735186145321,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8b743e12d0ea9cccf1693ef059f8b8a1de34fa29",
-          "message": "chore(deps): bump twox-hash from 2.0.0 to 2.0.1 (#1973)\n\nBumps [twox-hash](https://github.com/shepmaster/twox-hash) from 2.0.0 to 2.0.1.\r\n- [Changelog](https://github.com/shepmaster/twox-hash/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/shepmaster/twox-hash/compare/v2.0.0...v2.0.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: twox-hash\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-11-05T10:15:23+08:00",
-          "tree_id": "7a44f5607e043f3f66848ed3c0a1a29181c2ad7b",
-          "url": "https://github.com/f1shl3gs/vertex/commit/8b743e12d0ea9cccf1693ef059f8b8a1de34fa29"
-        },
-        "date": 1730773058854,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 543680,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 526810,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4aba10abc3fced4da23234d9fcba70edabb482d4",
+          "message": "fix metrics update (#2084)",
+          "timestamp": "2024-12-26T12:06:47+08:00",
+          "tree_id": "34387fcda5740db3bcaffce9f372ca86e24029c3",
+          "url": "https://github.com/f1shl3gs/vertex/commit/4aba10abc3fced4da23234d9fcba70edabb482d4"
+        },
+        "date": 1735186127671,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 529240,
             "unit": "ns/op"
           }
         ]
