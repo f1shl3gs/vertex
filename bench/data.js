a135ece32b928bf081b7be38bdb19e9e273b3663
github-action-benchmark <github@users.noreply.github.com>
2024-12-28 00:29:01 +0000

    add vertex (customSmallerIsBetter) benchmark result for 8b1a0a7682261465fbbf2f1818946309af56bbbb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735345676563,
+  "lastUpdate": 1735345741335,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -16624,45 +16624,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "3ac811f734efc56139c68a181154b6df119fbf1d",
-          "message": "chore(deps): bump pprof from 0.13.0 to 0.14.0 (#1977)\n\nBumps [pprof](https://github.com/tikv/pprof-rs) from 0.13.0 to 0.14.0.\r\n- [Changelog](https://github.com/tikv/pprof-rs/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/tikv/pprof-rs/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: pprof\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-11-08T20:18:48+08:00",
-          "tree_id": "cc4f7e2465cf838f52e3b2e75a0c4221b88bfb70",
-          "url": "https://github.com/f1shl3gs/vertex/commit/3ac811f734efc56139c68a181154b6df119fbf1d"
-        },
-        "date": 1731068935186,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 27473000,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 672940,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 44.268,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "554671b86247c99fe0cd551d0272eba5e63a02b9",
           "message": "chore(deps): bump thiserror from 2.0.0 to 2.0.1 (#1978)\n\nBumps [thiserror](https://github.com/dtolnay/thiserror) from 2.0.0 to 2.0.1.\r\n- [Release notes](https://github.com/dtolnay/thiserror/releases)\r\n- [Commits](https://github.com/dtolnay/thiserror/compare/2.0.0...2.0.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: thiserror\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
           "timestamp": "2024-11-09T11:45:31+08:00",
@@ -20507,6 +20468,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 44.784,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b1a0a7682261465fbbf2f1818946309af56bbbb",
+          "message": "sources/node: add missing metrics which added by node_exporter (#2089)",
+          "timestamp": "2024-12-28T08:18:33+08:00",
+          "tree_id": "321e891bb70c1b835fd4e6563e9d53ec9cfac5d7",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8b1a0a7682261465fbbf2f1818946309af56bbbb"
+        },
+        "date": 1735345722964,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 28305000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 1912.8,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 45.331,
             "unit": "ns/op"
           }
         ]
