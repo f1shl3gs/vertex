0b49a6f1f6849909c90a2dd392b210c14aa3ff2d
github-action-benchmark <github@users.noreply.github.com>
2024-12-29 06:22:11 +0000

    add prometheus (customSmallerIsBetter) benchmark result for e30971b2adb4f1ff4b91c989b70b6f081cebc6e8

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735433784789,
+  "lastUpdate": 1735453331919,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "47133f1e4a90d3d6c08c205b4bcdd699f6b2cf93",
-          "message": "chore(deps): bump serde from 1.0.214 to 1.0.215 (#1980)\n\nBumps [serde](https://github.com/serde-rs/serde) from 1.0.214 to 1.0.215.\r\n- [Release notes](https://github.com/serde-rs/serde/releases)\r\n- [Commits](https://github.com/serde-rs/serde/compare/v1.0.214...v1.0.215)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: serde\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-11-12T12:58:52+08:00",
-          "tree_id": "865f2d90bf1e1afe1accd7c3bcdea54d1606baa6",
-          "url": "https://github.com/f1shl3gs/vertex/commit/47133f1e4a90d3d6c08c205b4bcdd699f6b2cf93"
-        },
-        "date": 1731387650371,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 528020,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 531130,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e30971b2adb4f1ff4b91c989b70b6f081cebc6e8",
+          "message": "bump glob to 0.3.2 (#2092)",
+          "timestamp": "2024-12-29T14:19:49+08:00",
+          "tree_id": "61d93b57081e5fc021219e6085470ade1f32e205",
+          "url": "https://github.com/f1shl3gs/vertex/commit/e30971b2adb4f1ff4b91c989b70b6f081cebc6e8"
+        },
+        "date": 1735453313866,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 529570,
             "unit": "ns/op"
           }
         ]
