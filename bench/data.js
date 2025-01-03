0d7aaf0ccc4f860391c70689bacb6e0d14439140
github-action-benchmark <github@users.noreply.github.com>
2025-01-03 09:41:47 +0000

    add metrics (customSmallerIsBetter) benchmark result for 8a9a7ecfa09596aaa46d47dcd0223554070b2965

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735897301548,
+  "lastUpdate": 1735897307854,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "94281258de30bc7f4c3dfb4f160aa78330247433",
-          "message": "chore(deps): bump kube from 0.96.0 to 0.97.0 (#1989)\n\nBumps [kube](https://github.com/kube-rs/kube) from 0.96.0 to 0.97.0.\r\n- [Release notes](https://github.com/kube-rs/kube/releases)\r\n- [Changelog](https://github.com/kube-rs/kube/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/kube-rs/kube/compare/0.96.0...0.97.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: kube\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-11-20T23:28:30+08:00",
-          "tree_id": "4c41668a969622132d8bdf59c3e0bcb78d20c148",
-          "url": "https://github.com/f1shl3gs/vertex/commit/94281258de30bc7f4c3dfb4f160aa78330247433"
-        },
-        "date": 1732116643193,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.314,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 96.641,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 190.02,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 186.65,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a9a7ecfa09596aaa46d47dcd0223554070b2965",
+          "message": "refine logs (#2101)",
+          "timestamp": "2025-01-03T17:39:18+08:00",
+          "tree_id": "fb0694e51bea4a2e65fcb75aef7beabf835dea7c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8a9a7ecfa09596aaa46d47dcd0223554070b2965"
+        },
+        "date": 1735897289749,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.49,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 97.469,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 188.58,
             "unit": "ns/op"
           }
         ]
