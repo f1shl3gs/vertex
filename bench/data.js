be7f96611137c2dc86de188fbf50f2188c889732
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 08:55:50 +0000

    add metrics (customSmallerIsBetter) benchmark result for c4eac1c77f80e40b56e683c5e500ac147075e79c

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737622541453,
+  "lastUpdate": 1737622550299,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "7cd607c6171b7b0a53fba41d4f74e31bc859a45d",
-          "message": "chore(deps): bump libc from 0.2.167 to 0.2.168 (#2034)\n\nBumps [libc](https://github.com/rust-lang/libc) from 0.2.167 to 0.2.168.\r\n- [Release notes](https://github.com/rust-lang/libc/releases)\r\n- [Changelog](https://github.com/rust-lang/libc/blob/0.2.168/CHANGELOG.md)\r\n- [Commits](https://github.com/rust-lang/libc/compare/0.2.167...0.2.168)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: libc\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2024-12-10T17:27:10+08:00",
-          "tree_id": "ca072496bf4476900e61bd4361467cf5e1649d35",
-          "url": "https://github.com/f1shl3gs/vertex/commit/7cd607c6171b7b0a53fba41d4f74e31bc859a45d"
-        },
-        "date": 1733822965295,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.34,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 95.878,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 184.43,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 184.15,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c4eac1c77f80e40b56e683c5e500ac147075e79c",
+          "message": "lib/codecs: decode inputs to Events (#2151)",
+          "timestamp": "2025-01-23T16:53:12+08:00",
+          "tree_id": "31f68ccd8217b8fc05303673d4927cb99d4c52d2",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c4eac1c77f80e40b56e683c5e500ac147075e79c"
+        },
+        "date": 1737622532034,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.354,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 97.852,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 187.55,
             "unit": "ns/op"
           }
         ]
