b9fd3604866dfbb7a1c263092808fdd06ae5cedd
github-action-benchmark <github@users.noreply.github.com>
2025-02-02 17:35:53 +0000

    add metrics (customSmallerIsBetter) benchmark result for b19b09498e93f0eec89b66a3d40fbf1e0f33f569

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738517748401,
+  "lastUpdate": 1738517753221,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "45e9a96ceede6b5d4eb88aeadedbf23e2e56c0e0",
-          "message": "Do not reload config after every pull (#2067)\n\n* track config hash and yield config only when it changed",
-          "timestamp": "2024-12-21T03:50:21+08:00",
-          "tree_id": "5d3c5b4ac77132ff17ebfb0f98e6ea0381972ac2",
-          "url": "https://github.com/f1shl3gs/vertex/commit/45e9a96ceede6b5d4eb88aeadedbf23e2e56c0e0"
-        },
-        "date": 1734724349412,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.17,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 97.179,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 187.43,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 183.63,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b19b09498e93f0eec89b66a3d40fbf1e0f33f569",
+          "message": "lib/value: remove coalesce (#2180)",
+          "timestamp": "2025-02-03T01:33:20+08:00",
+          "tree_id": "baa5c6fe73d1329963c2f51f34e59213a440ff08",
+          "url": "https://github.com/f1shl3gs/vertex/commit/b19b09498e93f0eec89b66a3d40fbf1e0f33f569"
+        },
+        "date": 1738517734734,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.459,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 107.71,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 182.94,
             "unit": "ns/op"
           }
         ]
