20c492835e486c46aff01a22835907e3a0118dd6
github-action-benchmark <github@users.noreply.github.com>
2025-02-02 06:13:55 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 8bf7a1a1b6d40d29f64bd6d54f3afe317a6d02d8

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738472660806,
+  "lastUpdate": 1738476835031,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "cd518dbbb02dab8f0cb913539953859d6267b24f",
-          "message": "use UNSPECIFIED ip address (#2065)",
-          "timestamp": "2024-12-20T23:25:00+08:00",
-          "tree_id": "60049a20103667c51fa03da54124f59f9788372e",
-          "url": "https://github.com/f1shl3gs/vertex/commit/cd518dbbb02dab8f0cb913539953859d6267b24f"
-        },
-        "date": 1734708446063,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 541540,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 528290,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8bf7a1a1b6d40d29f64bd6d54f3afe317a6d02d8",
+          "message": "chore(deps): bump syn from 2.0.96 to 2.0.98 (#2176)\n\nBumps [syn](https://github.com/dtolnay/syn) from 2.0.96 to 2.0.98.\n- [Release notes](https://github.com/dtolnay/syn/releases)\n- [Commits](https://github.com/dtolnay/syn/compare/2.0.96...2.0.98)\n\n---\nupdated-dependencies:\n- dependency-name: syn\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-02-02T14:11:25+08:00",
+          "tree_id": "1714f1a049ee73d181eb4f01ab30a73b748d7788",
+          "url": "https://github.com/f1shl3gs/vertex/commit/8bf7a1a1b6d40d29f64bd6d54f3afe317a6d02d8"
+        },
+        "date": 1738476816590,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 522580,
             "unit": "ns/op"
           }
         ]
