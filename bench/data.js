fc948a942b3a148744353bf8abf1d0ee78ea4f8d
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 12:43:54 +0000

    add prometheus (customSmallerIsBetter) benchmark result for ccf23f7e1c041d5b1409697b8d275a18feacceee

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738754742175,
+  "lastUpdate": 1738759434631,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "74f37d56f1cab84ea088d78c82039e08d1b06023",
-          "message": "fix metric tags and description when build our own metric (#2077)",
-          "timestamp": "2024-12-24T02:25:24+08:00",
-          "tree_id": "1818d1b76749f61350d35107e50e7e77e2ca0971",
-          "url": "https://github.com/f1shl3gs/vertex/commit/74f37d56f1cab84ea088d78c82039e08d1b06023"
-        },
-        "date": 1734978444367,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 528540,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 533640,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ccf23f7e1c041d5b1409697b8d275a18feacceee",
+          "message": "disable jemalloc by default (#2190)",
+          "timestamp": "2025-02-05T20:41:24+08:00",
+          "tree_id": "0e5690b33a633f74c5d45bde86c50655f127ee94",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ccf23f7e1c041d5b1409697b8d275a18feacceee"
+        },
+        "date": 1738759415262,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 522980,
             "unit": "ns/op"
           }
         ]
