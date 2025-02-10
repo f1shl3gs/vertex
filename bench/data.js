057b69826cd72239650446fbbeb161f4ce339671
github-action-benchmark <github@users.noreply.github.com>
2025-02-10 14:25:43 +0000

    add vertex (customSmallerIsBetter) benchmark result for 54103e7c53bf7a3c509dfb6412ea2083958f8c24

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739197475130,
+  "lastUpdate": 1739197543909,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -16624,45 +16624,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "575d4d356c25d3a785a5c73f2e179f0f7a205c4b",
-          "message": "sinks/prometheus: optimize metrics store and write output (#2083)",
-          "timestamp": "2024-12-26T07:43:10+08:00",
-          "tree_id": "f50309d2984dc6a9355865b3c5784a3faab06fac",
-          "url": "https://github.com/f1shl3gs/vertex/commit/575d4d356c25d3a785a5c73f2e179f0f7a205c4b"
-        },
-        "date": 1735170814815,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 28629000,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 1983.3,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 49.768,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "f18d07ae75911aa59f64328886f92697e4ae46ac",
           "message": "refine source routine (#2080)",
           "timestamp": "2024-12-26T10:27:35+08:00",
@@ -20507,6 +20468,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 46.324,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "54103e7c53bf7a3c509dfb6412ea2083958f8c24",
+          "message": "source/fluent: implement with TcpSource (#2195)",
+          "timestamp": "2025-02-10T22:13:38+08:00",
+          "tree_id": "96225dd628bdb53be6d28b3f9cebafa49ff4d51e",
+          "url": "https://github.com/f1shl3gs/vertex/commit/54103e7c53bf7a3c509dfb6412ea2083958f8c24"
+        },
+        "date": 1739197524516,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 29378000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 719630,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 45.538,
             "unit": "ns/op"
           }
         ]
