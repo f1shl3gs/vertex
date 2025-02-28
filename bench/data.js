195d69dcc74d6a3d9f41724a54d6ab82a25be893
github-action-benchmark <github@users.noreply.github.com>
2025-02-28 12:46:13 +0000

    add metrics (customSmallerIsBetter) benchmark result for bbea72c72c39aaf20caafa48808682f61162a2b7

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740746752277,
+  "lastUpdate": 1740746773141,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "034c2df37c68e164a3bbea42a4876c4dc6137109",
-          "message": "bump toolchain to 1.84.0 (#2119)",
-          "timestamp": "2025-01-10T20:58:48+08:00",
-          "tree_id": "01fc94cfd5d31551b566d7035b2ffc0327a9a60c",
-          "url": "https://github.com/f1shl3gs/vertex/commit/034c2df37c68e164a3bbea42a4876c4dc6137109"
-        },
-        "date": 1736514064230,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.337,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 107.4,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 180.8,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 177.26,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bbea72c72c39aaf20caafa48808682f61162a2b7",
+          "message": "chore(deps): bump typetag from 0.2.19 to 0.2.20 (#2234)\n\nBumps [typetag](https://github.com/dtolnay/typetag) from 0.2.19 to 0.2.20.\n- [Release notes](https://github.com/dtolnay/typetag/releases)\n- [Commits](https://github.com/dtolnay/typetag/compare/0.2.19...0.2.20)\n\n---\nupdated-dependencies:\n- dependency-name: typetag\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-02-28T20:43:43+08:00",
+          "tree_id": "4de521651e1f5587d1f9ca97e8b2bbb5328eadae",
+          "url": "https://github.com/f1shl3gs/vertex/commit/bbea72c72c39aaf20caafa48808682f61162a2b7"
+        },
+        "date": 1740746754005,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.386,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 102.48,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 177.88,
             "unit": "ns/op"
           }
         ]
