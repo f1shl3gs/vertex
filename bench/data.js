1452f1add949088c9c2494b7920422422af12e8a
github-action-benchmark <github@users.noreply.github.com>
2025-03-15 15:00:24 +0000

    add metrics (customSmallerIsBetter) benchmark result for e210e63e2380a4f2b3d1fc5276a52b60095c0ad4

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742038967213,
+  "lastUpdate": 1742050824898,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "ce522e9ee10e5281c5fa456eb28fccf67ada1eee",
-          "message": "misc/update (#2156)",
-          "timestamp": "2025-01-28T04:34:11+08:00",
-          "tree_id": "2cee5456a21d5016c66bec261999f581844be85e",
-          "url": "https://github.com/f1shl3gs/vertex/commit/ce522e9ee10e5281c5fa456eb28fccf67ada1eee"
-        },
-        "date": 1738010177819,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.425,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 97.969,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 185.62,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 183.7,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e210e63e2380a4f2b3d1fc5276a52b60095c0ad4",
+          "message": "Merge pull request #2269 from f1shl3gs/observe/add_type_to_endpoint\n\nlib/framework: add type to observer's Endpoint",
+          "timestamp": "2025-03-15T22:58:05+08:00",
+          "tree_id": "bd066b3432ed0c4ffbe622f40efda7f09bec3c8d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/e210e63e2380a4f2b3d1fc5276a52b60095c0ad4"
+        },
+        "date": 1742050804749,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.508,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 103.56,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 185.21,
             "unit": "ns/op"
           }
         ]
