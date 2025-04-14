5f8bded30c3856142f5d41dfd659fbdeddf3bd18
github-action-benchmark <github@users.noreply.github.com>
2025-04-14 13:32:15 +0000

    add vertex (customSmallerIsBetter) benchmark result for 24fe151777669df4a12aaf72704b90bb0dd1d09e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744637100753,
+  "lastUpdate": 1744637535022,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -16624,45 +16624,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "81c04fe450057e8afddec5ef63e8bbc4ddbc0bdc",
-          "message": "lib/codecs: add metadata support for VTL deserializer (#2193)",
-          "timestamp": "2025-02-07T05:49:48+08:00",
-          "tree_id": "7151d8f0ee0c2ca9238b6fa316faafab11ea8852",
-          "url": "https://github.com/f1shl3gs/vertex/commit/81c04fe450057e8afddec5ef63e8bbc4ddbc0bdc"
-        },
-        "date": 1738879230249,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 29256000,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 591110,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 45.251,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "dd9d551398630df009bced7180b9e8053551616a",
           "message": "remove lib/xdr (#2194)",
           "timestamp": "2025-02-07T07:40:43+08:00",
@@ -20507,6 +20468,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 52.412,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "24fe151777669df4a12aaf72704b90bb0dd1d09e",
+          "message": "Merge pull request #2302 from f1shl3gs/dependabot/cargo/lru-0.14.0\n\nchore(deps): bump lru from 0.13.0 to 0.14.0",
+          "timestamp": "2025-04-14T21:21:14+08:00",
+          "tree_id": "1c5ceb255a0196032abf60dfbf89dbf936cffd61",
+          "url": "https://github.com/f1shl3gs/vertex/commit/24fe151777669df4a12aaf72704b90bb0dd1d09e"
+        },
+        "date": 1744637513231,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 22537000,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 643530,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 52.195,
             "unit": "ns/op"
           }
         ]
