3d21a33067950b336d36d0e01d620296cd8fc7a9
f1shl3gs <fishlegs.engerman@gmail.com>
2025-04-24 01:28:30 +0800

    remove proto since we don't need it

--- lib/event/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=proto/event.proto");
-
-    prost_build::Config::new()
-        .btree_map(["."])
-        .bytes(["bytes"])
-        .compile_protos(&["proto/event.proto"], &["proto/"])
-        .unwrap();
-}
