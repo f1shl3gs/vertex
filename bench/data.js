684aefba060207e64a95e4e9b520f2a2e1783b94
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 08:24:06 +0000

    add metrics (customSmallerIsBetter) benchmark result for 03a7ce05ab34ab81d55ddd8f49cf70e002ef18f3

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745742222022,
+  "lastUpdate": 1745742246251,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "49e7b6a5dda6040e4db07e797c273da38a315ff7",
-          "message": "chore(deps): bump uuid from 1.13.1 to 1.13.2 (#2209)\n\nBumps [uuid](https://github.com/uuid-rs/uuid) from 1.13.1 to 1.13.2.\n- [Release notes](https://github.com/uuid-rs/uuid/releases)\n- [Commits](https://github.com/uuid-rs/uuid/compare/1.13.1...v1.13.2)\n\n---\nupdated-dependencies:\n- dependency-name: uuid\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2025-02-18T12:18:37+08:00",
-          "tree_id": "1ae1a6aeaa564965c29024981f62e547045ce58e",
-          "url": "https://github.com/f1shl3gs/vertex/commit/49e7b6a5dda6040e4db07e797c273da38a315ff7"
-        },
-        "date": 1739852443555,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 17.321,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 107.5,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 183.29,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 173.93,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03a7ce05ab34ab81d55ddd8f49cf70e002ef18f3",
+          "message": "Merge pull request #2317 from f1shl3gs/lib/buffer/fix_queue_test\n\nlib/buffer: fix queue's mpsc test",
+          "timestamp": "2025-04-27T16:20:49+08:00",
+          "tree_id": "e5ad9f346cf2137c3f9b59424d46014279f2657d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/03a7ce05ab34ab81d55ddd8f49cf70e002ef18f3"
+        },
+        "date": 1745742225209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 17.312,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 99.524,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 174.59,
             "unit": "ns/op"
           }
         ]
