5255a2579e34619e71d1f3aa8bd5e18c05668e3c
github-action-benchmark <github@users.noreply.github.com>
2025-05-01 14:23:44 +0000

    add prometheus (customSmallerIsBetter) benchmark result for bdd6f938b9b8acedc53db664a2be10bb89851543

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746081082494,
+  "lastUpdate": 1746109424340,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "5ad0a92d4f3ef6378cee81ffd27a35d256a6fcc6",
-          "message": "chore(deps): bump pem from 3.0.4 to 3.0.5 (#2219)\n\nBumps [pem](https://github.com/jcreekmore/pem-rs) from 3.0.4 to 3.0.5.\n- [Changelog](https://github.com/jcreekmore/pem-rs/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/jcreekmore/pem-rs/compare/v3.0.4...v3.0.5)\n\n---\nupdated-dependencies:\n- dependency-name: pem\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2025-02-22T10:25:21+08:00",
-          "tree_id": "3f6302860a916bf1e46a8c3beac441d0ab148e78",
-          "url": "https://github.com/f1shl3gs/vertex/commit/5ad0a92d4f3ef6378cee81ffd27a35d256a6fcc6"
-        },
-        "date": 1740191251904,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 524460,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 545640,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bdd6f938b9b8acedc53db664a2be10bb89851543",
+          "message": "Merge pull request #2325 from f1shl3gs/lib/multline/impl_preset\n\nlib/multiline: implement some preset",
+          "timestamp": "2025-05-01T22:21:14+08:00",
+          "tree_id": "21ac94eddecad355d5bc60eac27264e3b078a73f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/bdd6f938b9b8acedc53db664a2be10bb89851543"
+        },
+        "date": 1746109402883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 545170,
             "unit": "ns/op"
           }
         ]
