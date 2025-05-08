e8ba57c17687226848d7b55ee635c200774c2ee6
github-action-benchmark <github@users.noreply.github.com>
2025-05-08 06:50:28 +0000

    add prometheus (customSmallerIsBetter) benchmark result for c1508bfd95fd28fec7277bad7ab7eb187cb79307

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746687027075,
+  "lastUpdate": 1746687028548,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "0e08331c35749647cd0ab374e8372f25b4c595ce",
-          "message": "remove fields (#2230)",
-          "timestamp": "2025-02-27T02:27:21+08:00",
-          "tree_id": "49f1808da68e8cf72528fd62b9afa3b4e2eca35c",
-          "url": "https://github.com/f1shl3gs/vertex/commit/0e08331c35749647cd0ab374e8372f25b4c595ce"
-        },
-        "date": 1740594563168,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 513810,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 553060,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c1508bfd95fd28fec7277bad7ab7eb187cb79307",
+          "message": "Merge pull request #2337 from f1shl3gs/deps/remove_ahash\n\ndeps: remove ahash",
+          "timestamp": "2025-05-08T14:48:05+08:00",
+          "tree_id": "4c9e0149ec26f3aa305896fcc4aa72c253823510",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c1508bfd95fd28fec7277bad7ab7eb187cb79307"
+        },
+        "date": 1746687004721,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 558770,
             "unit": "ns/op"
           }
         ]
