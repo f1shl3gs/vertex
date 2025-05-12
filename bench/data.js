9041b74aa5b886f84f775e57e458b44036e51af2
github-action-benchmark <github@users.noreply.github.com>
2025-05-12 06:02:28 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 10b6d16e73b60e3171f9fd9b6660c4e1d0bcdf7f

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746687576673,
+  "lastUpdate": 1747029748050,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "49699333+dependabot[bot]@users.noreply.github.com",
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "f658b4b20d67f66f2a9e727a5511542ac8356a8f",
-          "message": "chore(deps): bump uuid from 1.15.0 to 1.15.1 (#2231)\n\nBumps [uuid](https://github.com/uuid-rs/uuid) from 1.15.0 to 1.15.1.\n- [Release notes](https://github.com/uuid-rs/uuid/releases)\n- [Commits](https://github.com/uuid-rs/uuid/compare/v1.15.0...v1.15.1)\n\n---\nupdated-dependencies:\n- dependency-name: uuid\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2025-02-27T18:39:00+08:00",
-          "tree_id": "6cca06cc594ff9dfa97214464932cc1afbef1951",
-          "url": "https://github.com/f1shl3gs/vertex/commit/f658b4b20d67f66f2a9e727a5511542ac8356a8f"
-        },
-        "date": 1740652858458,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 515320,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 558770,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10b6d16e73b60e3171f9fd9b6660c4e1d0bcdf7f",
+          "message": "Merge pull request #2338 from f1shl3gs/lib/configurable/simplify_and_reduce_codebase\n\nlib/configurable: reduce codebase and the binary size",
+          "timestamp": "2025-05-12T14:00:17+08:00",
+          "tree_id": "260c584d8c061d9bf755426787925231590baa47",
+          "url": "https://github.com/f1shl3gs/vertex/commit/10b6d16e73b60e3171f9fd9b6660c4e1d0bcdf7f"
+        },
+        "date": 1747029726502,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 553890,
             "unit": "ns/op"
           }
         ]
