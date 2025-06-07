10019d28eabf5ba1900893299bf204f67a03c18f
github-action-benchmark <github@users.noreply.github.com>
2025-06-07 20:45:25 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 3048e4f0a0177aec59f7555397f45c60a50e5359

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749328271874,
+  "lastUpdate": 1749329125283,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "cbcf953e9fc53ac87e7acf14498a1db2812b4da8",
-          "message": "Merge pull request #2280 from f1shl3gs/dependabot/cargo/tonic-0dfe49084d\n\nchore(deps): bump the tonic group with 3 updates",
-          "timestamp": "2025-03-26T14:24:55+08:00",
-          "tree_id": "47106805b81dfc0bc32f6b2febd59192891a148a",
-          "url": "https://github.com/f1shl3gs/vertex/commit/cbcf953e9fc53ac87e7acf14498a1db2812b4da8"
-        },
-        "date": 1742970411602,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 554860,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 554040,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3048e4f0a0177aec59f7555397f45c60a50e5359",
+          "message": "Merge pull request #2390 from f1shl3gs/sources/node/rename\n\nsources/node: rename `node_metrics` to `node`",
+          "timestamp": "2025-06-08T04:42:49+08:00",
+          "tree_id": "8869e43485cbd66edd5d5cbcf26366b4109b06f8",
+          "url": "https://github.com/f1shl3gs/vertex/commit/3048e4f0a0177aec59f7555397f45c60a50e5359"
+        },
+        "date": 1749329103560,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 558440,
             "unit": "ns/op"
           }
         ]
