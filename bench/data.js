a1e1d8e7e83bfa820519c7014782601184060ffb
github-action-benchmark <github@users.noreply.github.com>
2025-06-08 17:20:34 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 33ac0c442ebd21aafd4534cbebed7c5885111fbe

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749393825281,
+  "lastUpdate": 1749403234571,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "e88e27d5226c1b5645a1291d16adf631fa1003eb",
-          "message": "Merge pull request #2284 from f1shl3gs/observer/simplify_observer_register\n\nlib/framework: simplify observer register & warning if the observer have no receivers",
-          "timestamp": "2025-03-27T22:07:32+08:00",
-          "tree_id": "81b5bc08cde5330b03333ca479bc29a66adf27ff",
-          "url": "https://github.com/f1shl3gs/vertex/commit/e88e27d5226c1b5645a1291d16adf631fa1003eb"
-        },
-        "date": 1743084571842,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 558400,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 558220,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "33ac0c442ebd21aafd4534cbebed7c5885111fbe",
+          "message": "Merge pull request #2393 from f1shl3gs/misc/small_optimize\n\nmisc: small optimize",
+          "timestamp": "2025-06-09T01:17:51+08:00",
+          "tree_id": "16ae7f522fd9298402d1f2d95ba20c5bf6dd04a8",
+          "url": "https://github.com/f1shl3gs/vertex/commit/33ac0c442ebd21aafd4534cbebed7c5885111fbe"
+        },
+        "date": 1749403212832,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 565060,
             "unit": "ns/op"
           }
         ]
