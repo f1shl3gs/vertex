2e48ad9edf7ec87d0d135ef3ae5f91a892d2952c
github-action-benchmark <github@users.noreply.github.com>
2025-07-03 06:37:51 +0000

    add Build (customSmallerIsBetter) benchmark result for ef86afff44554dc3626c13139bbe070a080f65cc

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751524268919,
+  "lastUpdate": 1751524671660,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "91dec175fad00ee024638cc893cd4f1436636138",
-          "message": "Merge pull request #2345 from f1shl3gs/dependabot/cargo/sqlx-0.8.6\n\nchore(deps): bump sqlx from 0.8.5 to 0.8.6",
-          "timestamp": "2025-05-20T11:22:41+08:00",
-          "tree_id": "9aa4fcfd09eb892aeb2cab3c2904654c98ea1ad0",
-          "url": "https://github.com/f1shl3gs/vertex/commit/91dec175fad00ee024638cc893cd4f1436636138"
-        },
-        "date": 1747711967145,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 520,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 38560.4,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "4a77bc945be333fb4721e24b325cf9d2090c9ed6",
           "message": "Merge pull request #2346 from f1shl3gs/lib/resolver/init\n\nlib/resolver: init and remove hickory-resolver",
           "timestamp": "2025-05-21T14:11:01+08:00",
@@ -16600,6 +16566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 506,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 37744.7,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ef86afff44554dc3626c13139bbe070a080f65cc",
+          "message": "Merge pull request #2450 from f1shl3gs/dependabot/cargo/lru-0.16.0\n\nchore(deps): bump lru from 0.15.0 to 0.16.0",
+          "timestamp": "2025-07-03T14:27:34+08:00",
+          "tree_id": "d1e883b74d2b496f43836d83a042ecba2c3c6135",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ef86afff44554dc3626c13139bbe070a080f65cc"
+        },
+        "date": 1751524651398,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 504,
             "unit": "s"
           },
           {
