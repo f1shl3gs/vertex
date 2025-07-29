1d9a9e82d78f8d758e68114a2aaf791f412e30f0
f1shl3gs <fishlegs.engerman@gmail.com>
2025-07-29 13:16:43 +0800

    fix compile errors for conflicting of prost and tonic

--- lib/jaeger/build.rs
+++ lib/jaeger/build.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("cargo:rerun-if-changed=proto/collector.proto");
     println!("cargo:rerun-if-changed=proto/model.proto");
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .compile_protos(&["proto/collector.proto", "proto/model.proto"], &["proto"])
--- lib/prometheus/build.rs
+++ lib/prometheus/build.rs
@@ -7,7 +7,7 @@ fn main() {
     // It would be nice to just add these derives to all the types, but
     // prost automatically adds them already to enums, which causes the
     // extra derives to conflict with itself.
-    prost_build.type_attribute("Label", "#[derive(Eq, Hash, Ord, PartialOrd)]");
+    prost_build.type_attribute("Label", "#[derive(Ord, PartialOrd)]");
 
     prost_build
         .compile_protos(&["proto/prometheus-remote.proto"], &["proto/"])
