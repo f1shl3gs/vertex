342fffc3d513e0e76b52d879965fbaf018d4e1ae
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 19:41:51 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 9e190bd583804e9d2c7dff38b4172b146a7570b5

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758629838751,
+  "lastUpdate": 1758829311942,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "c6ceac81018a129bcea818984d170aed5746a15a",
-          "message": "Merge pull request #2448 from f1shl3gs/cargo/update\n\ncargo: update",
-          "timestamp": "2025-07-02T11:00:15+08:00",
-          "tree_id": "de6c3a025cdfbf8f2cbed929634968194d3107fe",
-          "url": "https://github.com/f1shl3gs/vertex/commit/c6ceac81018a129bcea818984d170aed5746a15a"
-        },
-        "date": 1751425334023,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 491300,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 496170,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9e190bd583804e9d2c7dff38b4172b146a7570b5",
+          "message": "Merge pull request #2559 from f1shl3gs/dependabot/cargo/regex-1.11.3\n\nchore(deps): bump regex from 1.11.2 to 1.11.3",
+          "timestamp": "2025-09-26T03:39:21+08:00",
+          "tree_id": "17f7bec9c9818136bb787b42a3f2e00024fc6e08",
+          "url": "https://github.com/f1shl3gs/vertex/commit/9e190bd583804e9d2c7dff38b4172b146a7570b5"
+        },
+        "date": 1758829288697,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 506980,
             "unit": "ns/op"
           }
         ]
