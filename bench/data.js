2c806503b871ae8993ead66a2f99f93433f79a29
github-action-benchmark <github@users.noreply.github.com>
2025-10-03 18:15:12 +0000

    add Build (customSmallerIsBetter) benchmark result for 07391e70648b0f1f1276d7ce93c59fd4e34786f9

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759355919932,
+  "lastUpdate": 1759515312043,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "94996f2ac7c8c0c3a00d85cf4faa8fc7f66e55c4",
-          "message": "Merge pull request #2464 from f1shl3gs/deps/chrono-tz/remove_serde_feature\n\ndeps: remove serde feature of chrono-tz",
-          "timestamp": "2025-07-12T19:34:37+08:00",
-          "tree_id": "15e01a0c6a12937742577e807bc36e14be870845",
-          "url": "https://github.com/f1shl3gs/vertex/commit/94996f2ac7c8c0c3a00d85cf4faa8fc7f66e55c4"
-        },
-        "date": 1752320676067,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 512,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 37783.1,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "76280666f998877a0811bae3dd3fc7179d3ca426",
           "message": "Merge pull request #2465 from f1shl3gs/dependabot/cargo/memmap2-0.9.7\n\nchore(deps): bump memmap2 from 0.9.5 to 0.9.7",
           "timestamp": "2025-07-13T20:12:24+08:00",
@@ -16600,6 +16566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 542,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 39499.8,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07391e70648b0f1f1276d7ce93c59fd4e34786f9",
+          "message": "Merge pull request #2572 from f1shl3gs/ci/spell_checking\n\nci: add check spelling",
+          "timestamp": "2025-10-04T02:04:18+08:00",
+          "tree_id": "7642e1841affcff627be395c192c83959da939f3",
+          "url": "https://github.com/f1shl3gs/vertex/commit/07391e70648b0f1f1276d7ce93c59fd4e34786f9"
+        },
+        "date": 1759515290953,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 555,
             "unit": "s"
           },
           {
