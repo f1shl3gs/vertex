937a4278a9b846dfd5220450b376d4027fb8e63f
github-action-benchmark <github@users.noreply.github.com>
2025-10-15 17:04:49 +0000

    add Build (customSmallerIsBetter) benchmark result for 01e2958da5f4956d79520e2c51dbc845ba8d77db

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760547798619,
+  "lastUpdate": 1760547889561,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "c34e885d0537690d1b8bcca6bce463845342ce8d",
-          "message": "Merge pull request #2492 from f1shl3gs/re-organize-ignored-files\n\nre-organize ignored files",
-          "timestamp": "2025-08-10T00:44:50+08:00",
-          "tree_id": "6ff73948b915dd1b522ea7a30d1ff8ea6713296d",
-          "url": "https://github.com/f1shl3gs/vertex/commit/c34e885d0537690d1b8bcca6bce463845342ce8d"
-        },
-        "date": 1754758480152,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 506,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 37455.8,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "caa1368b595a696b76f9fda9003135914d8d2a97",
           "message": "Merge pull request #2493 from f1shl3gs/sources/prometheus_textfile/add\n\nsources/prometheus_textfile: init",
           "timestamp": "2025-08-10T04:43:33+08:00",
@@ -16605,6 +16571,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 39772.6,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01e2958da5f4956d79520e2c51dbc845ba8d77db",
+          "message": "Merge pull request #2602 from f1shl3gs/extensions/exec_observer/remove_args\n\nextensions/exec_observer: remove args from config",
+          "timestamp": "2025-10-16T00:53:46+08:00",
+          "tree_id": "7f79d110c71bf0224178ba9926f68f0410a9b86a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/01e2958da5f4956d79520e2c51dbc845ba8d77db"
+        },
+        "date": 1760547867253,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 560,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 39770.4,
             "unit": "KiB"
           }
         ]
