c1d85126b7cb06ab70a362e8b2629765c9a2379e
f1shl3gs <fishlegs.engerman@gmail.com>
2025-10-21 01:30:07 +0800

    fix required value of non-attributes field

--- lib/configurable-derive/src/configurable.rs
+++ lib/configurable-derive/src/configurable.rs
@@ -131,10 +131,10 @@ fn generate_named_struct_field(field: &syn::Field, field_attrs: FieldAttrs) -> T
         field.ident.clone().expect("filed has a name").to_string()
     };
 
-    let maybe_required = if field_attrs.default.is_some() || field_attrs.default_fn.is_some() {
-        false
+    let maybe_required = if field_attrs.required {
+        true
     } else {
-        field_attrs.required
+        field_attrs.default.is_none() && field_attrs.default_fn.is_none()
     };
 
     let maybe_description = match field_attrs.description {
