9c88d4da1098f0831064dc1f5f41405d966bd196
f1shl3gs <fishlegs.engerman@gmail.com>
2025-10-21 00:25:05 +0800

    support multiple endpoints

--- src/sources/redis/mod.rs
+++ src/sources/redis/mod.rs
@@ -13,8 +13,9 @@ use chrono::Utc;
 use configurable::configurable_component;
 use connection::{Connection, Error as ClientError};
 use event::Metric;
-use framework::Source;
 use framework::config::{OutputType, SecretString, SourceConfig, SourceContext, default_interval};
+use framework::{Pipeline, ShutdownSignal, Source};
+use tokio::task::JoinSet;
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -46,9 +47,9 @@ impl From<std::num::ParseFloatError> for Error {
 #[configurable_component(source, name = "redis")]
 #[serde(deny_unknown_fields)]
 struct Config {
-    /// Redis address
+    /// Redis addresses to request for.
     #[configurable(required, format = "ip-address")]
-    endpoint: SocketAddr,
+    endpoints: Vec<SocketAddr>,
 
     /// Duration between each scrape.
     #[serde(default = "default_interval", with = "humanize::duration::serde")]
@@ -71,81 +72,29 @@ impl SourceConfig for Config {
             return Err("password is required, if username provided".into());
         }
 
-        let address = self.endpoint;
+        let endpoints = self.endpoints.clone();
         let username = self.username.clone();
         let password = self.password.clone().map(|p| p.into());
         let client_name = self.client_name.clone();
         let interval = self.interval;
 
-        let mut output = cx.output;
-        let mut shutdown = cx.shutdown;
+        let output = cx.output;
+        let shutdown = cx.shutdown;
 
         Ok(Box::pin(async move {
-            let mut ticker = tokio::time::interval(interval);
-            let mut errors = 0;
-            let mut scraped = 0;
-            let mut last_err = false;
-
-            loop {
-                tokio::select! {
-                    _ = ticker.tick() => {
-                        scraped += 1;
-                    },
-                    _ = &mut shutdown => break,
-                }
-
-                let start = Instant::now();
-                let result = collect(
-                    address,
-                    username.as_ref(),
-                    password.as_ref(),
-                    client_name.as_ref(),
+            let mut tasks = JoinSet::from_iter(endpoints.into_iter().map(|endpoint| {
+                run(
+                    endpoint,
+                    interval,
+                    username.clone(),
+                    password.clone(),
+                    client_name.clone(),
+                    output.clone(),
+                    shutdown.clone(),
                 )
-                .await;
-                let elapsed = start.elapsed();
+            }));
 
-                let mut metrics = match result {
-                    Ok(metrics) => metrics,
-                    Err(_err) => {
-                        last_err = true;
-                        errors += 1;
-
-                        Vec::with_capacity(4)
-                    }
-                };
-
-                metrics.extend([
-                    Metric::sum(
-                        "redis_scrape_errors_total",
-                        "Errors in requests to the exporter",
-                        errors,
-                    ),
-                    Metric::sum("redis_scrapes_total", "Total number of scrapes", scraped),
-                    Metric::gauge(
-                        "redis_last_scrape_duration_seconds",
-                        "Duration in seconds since last scraping request",
-                        elapsed,
-                    ),
-                    Metric::gauge(
-                        "redis_last_scrape_error",
-                        "The last scrape error status.",
-                        last_err,
-                    ),
-                ]);
-
-                let timestamp = Utc::now();
-                metrics.iter_mut().for_each(|metric| {
-                    if !metric.name().starts_with("redis") {
-                        metric.set_name(format!("redis_{}", metric.name()));
-                    }
-
-                    metric.timestamp = Some(timestamp);
-                });
-
-                if let Err(_err) = output.send(metrics).await {
-                    break;
-                }
-            }
+            while tasks.join_next().await.is_some() {}
 
             Ok(())
         }))
@@ -158,6 +107,86 @@ impl SourceConfig for Config {
     fn can_acknowledge(&self) -> bool {
         false
     }
+}
+
+async fn run(
+    endpoint: SocketAddr,
+    interval: Duration,
+    username: Option<String>,
+    password: Option<String>,
+    client_name: Option<String>,
+    mut output: Pipeline,
+    mut shutdown: ShutdownSignal,
+) -> Result<(), ()> {
+    let start = crate::common::calculate_start(&endpoint, interval);
+    let mut ticker = tokio::time::interval_at(start.into(), interval);
+
+    let mut errors = 0;
+    let mut scraped = 0;
+    let mut last_err = false;
+    let target_value = endpoint.to_string();
+    loop {
+        tokio::select! {
+            _ = ticker.tick() => {},
+            _ = &mut shutdown => break,
+        }
+
+        scraped += 1;
+        let start = Instant::now();
+        let result = collect(
+            endpoint,
+            username.as_ref(),
+            password.as_ref(),
+            client_name.as_ref(),
+        )
+        .await;
+        let elapsed = start.elapsed();
+
+        let mut metrics = match result {
+            Ok(metrics) => metrics,
+            Err(_err) => {
+                last_err = true;
+                errors += 1;
+
+                Vec::with_capacity(4)
+            }
+        };
+
+        metrics.extend([
+            Metric::sum(
+                "redis_scrape_errors_total",
+                "Errors in requests to the exporter",
+                errors,
+            ),
+            Metric::sum("redis_scrapes_total", "Total number of scrapes", scraped),
+            Metric::gauge(
+                "redis_last_scrape_duration_seconds",
+                "Duration in seconds since last scraping request",
+                elapsed,
+            ),
+            Metric::gauge(
+                "redis_last_scrape_error",
+                "The last scrape error status.",
+                last_err,
+            ),
+        ]);
+
+        let timestamp = Utc::now();
+        metrics.iter_mut().for_each(|metric| {
+            if !metric.name().starts_with("redis") {
+                metric.set_name(format!("redis_{}", metric.name()));
+            }
+
+            metric.timestamp = Some(timestamp);
+            metric.tags_mut().insert("target", &target_value);
+        });
+
+        if let Err(_err) = output.send(metrics).await {
+            break;
+        }
+    }
+
+    Ok(())
 }
 
 async fn collect(
