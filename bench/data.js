fbc93ba84f52a7fec974edf8d37b8effc1511c2d
github-action-benchmark <github@users.noreply.github.com>
2025-10-24 20:32:42 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 57b8912d84d8aa850723e3837a50a6e6b382f97c

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761336112317,
+  "lastUpdate": 1761337961980,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "4cd6c1425ec5550949b722c1248ee03b1aa656cb",
-          "message": "Merge pull request #2535 from f1shl3gs/dependabot/cargo/tonic-b048c00cd4\n\nchore(deps): bump the tonic group with 4 updates",
-          "timestamp": "2025-09-04T02:01:02+08:00",
-          "tree_id": "0c3854e75939370ac567fbe8d1218d57ac081a90",
-          "url": "https://github.com/f1shl3gs/vertex/commit/4cd6c1425ec5550949b722c1248ee03b1aa656cb"
-        },
-        "date": 1756922591605,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 498800,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 517740,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "57b8912d84d8aa850723e3837a50a6e6b382f97c",
+          "message": "Merge pull request #2644 from f1shl3gs/lib/framework/remove_secret_string\n\nlib/framework: remove SecretString",
+          "timestamp": "2025-10-25T04:30:00+08:00",
+          "tree_id": "7ec6c12b40d4431c10d0a7f53c18c8a3246c5871",
+          "url": "https://github.com/f1shl3gs/vertex/commit/57b8912d84d8aa850723e3837a50a6e6b382f97c"
+        },
+        "date": 1761337939469,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 520920,
             "unit": "ns/op"
           }
         ]
