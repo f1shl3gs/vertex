a385586a304e650e35c483768ae4cfd9e3d40516
github-action-benchmark <github@users.noreply.github.com>
2025-10-25 15:41:07 +0000

    add event (customSmallerIsBetter) benchmark result for 03a5642aa2e3c46ffebc39a262370381c6bc4f92

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761406852279,
+  "lastUpdate": 1761406867515,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -20526,50 +20526,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "29493a945500ccc5d1cf7ec202a4b8d925d5235d",
-          "message": "Merge pull request #2538 from f1shl3gs/dependabot/cargo/chrono-0.4.42\n\nchore(deps): bump chrono from 0.4.41 to 0.4.42",
-          "timestamp": "2025-09-09T14:26:45+08:00",
-          "tree_id": "00ee79fef14f7e5b17706a6de0dda86d67cd259e",
-          "url": "https://github.com/f1shl3gs/vertex/commit/29493a945500ccc5d1cf7ec202a4b8d925d5235d"
-        },
-        "date": 1757399362239,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "tags/insert/1",
-            "value": 21.346,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/5",
-            "value": 171.48,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/10",
-            "value": 311.73,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/20",
-            "value": 704.74,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "310fa925219700cfd523fd1c7d10d238823f7515",
           "message": "Merge pull request #2540 from f1shl3gs/dependabot/cargo/indexmap-2.11.1\n\nchore(deps): bump indexmap from 2.11.0 to 2.11.1",
           "timestamp": "2025-09-10T06:09:57+08:00",
@@ -24909,6 +24865,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 677.64,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03a5642aa2e3c46ffebc39a262370381c6bc4f92",
+          "message": "Merge pull request #2646 from f1shl3gs/lib/framework/add_env_expansion_comment\n\nlib/framework: add env expansion comment",
+          "timestamp": "2025-10-25T23:38:05+08:00",
+          "tree_id": "0c38c7bf282f3ff6c3ee9c6a7370e15179f0ab4f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/03a5642aa2e3c46ffebc39a262370381c6bc4f92"
+        },
+        "date": 1761406845544,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 21.236,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 179.08,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 335.57,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 677.81,
             "unit": "ns/op"
           }
         ]
