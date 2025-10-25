5e7f89e6eb67ff607450243417c5f9de92fdec87
github-action-benchmark <github@users.noreply.github.com>
2025-10-25 15:40:43 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 03a5642aa2e3c46ffebc39a262370381c6bc4f92

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761406293056,
+  "lastUpdate": 1761406843855,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "29493a945500ccc5d1cf7ec202a4b8d925d5235d",
-          "message": "Merge pull request #2538 from f1shl3gs/dependabot/cargo/chrono-0.4.42\n\nchore(deps): bump chrono from 0.4.41 to 0.4.42",
-          "timestamp": "2025-09-09T14:26:45+08:00",
-          "tree_id": "00ee79fef14f7e5b17706a6de0dda86d67cd259e",
-          "url": "https://github.com/f1shl3gs/vertex/commit/29493a945500ccc5d1cf7ec202a4b8d925d5235d"
-        },
-        "date": 1757399319074,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 495740,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 518780,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03a5642aa2e3c46ffebc39a262370381c6bc4f92",
+          "message": "Merge pull request #2646 from f1shl3gs/lib/framework/add_env_expansion_comment\n\nlib/framework: add env expansion comment",
+          "timestamp": "2025-10-25T23:38:05+08:00",
+          "tree_id": "0c38c7bf282f3ff6c3ee9c6a7370e15179f0ab4f",
+          "url": "https://github.com/f1shl3gs/vertex/commit/03a5642aa2e3c46ffebc39a262370381c6bc4f92"
+        },
+        "date": 1761406821340,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 519120,
             "unit": "ns/op"
           }
         ]
