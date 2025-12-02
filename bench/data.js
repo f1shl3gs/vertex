d54b3fa0b7a9ccbb455a6b0d140dc9b2ee419308
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 07:27:12 +0000

    add Build (customSmallerIsBetter) benchmark result for f05cee1ece5307f08de2a08ee8624348e5265ae9

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764660325395,
+  "lastUpdate": 1764660432328,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "8ddfea108e6bf8902f78c8ee341b2dfe717c50fa",
-          "message": "Merge pull request #2599 from f1shl3gs/cargo/update\n\ncargo update",
-          "timestamp": "2025-10-15T04:48:34+08:00",
-          "tree_id": "f70546fbec00eb413418d8a6e2716cb2a65c4751",
-          "url": "https://github.com/f1shl3gs/vertex/commit/8ddfea108e6bf8902f78c8ee341b2dfe717c50fa"
-        },
-        "date": 1760475552963,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 562,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 39593.4,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "665b23d29a6252f3a58b259e0c7c1b4638945568",
           "message": "Merge pull request #2598 from f1shl3gs/sources/kafka/fix_deserializing_for_fetch_wait_max\n\nsources/kafka: fix deserializing for fetch_wait_max",
           "timestamp": "2025-10-15T04:49:04+08:00",
@@ -16605,6 +16571,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary size",
             "value": 41258.1,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f05cee1ece5307f08de2a08ee8624348e5265ae9",
+          "message": "Merge pull request #2699 from f1shl3gs/extensions/healthcheck/switch_to_framework_authorizer\n\nextensions/healthcheck: switch to framework::http::Authorizer",
+          "timestamp": "2025-12-02T15:15:48+08:00",
+          "tree_id": "2d48f252a6e3083211b39bb9b7f9c21c81d940ae",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f05cee1ece5307f08de2a08ee8624348e5265ae9"
+        },
+        "date": 1764660409699,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 579,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 41257.1,
             "unit": "KiB"
           }
         ]
