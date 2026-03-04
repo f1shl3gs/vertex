155f47e47a806950ec89534af52d3aee71bd3027
github-action-benchmark <github@users.noreply.github.com>
2026-03-04 17:53:17 +0000

    add prometheus (customSmallerIsBetter) benchmark result for f701b9d908b47aad4fbc402fc1e2278e4dcb4477

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772646794327,
+  "lastUpdate": 1772646797959,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "f937f8e20ab11968dc34cf6b299f9caf0abb92c8",
-          "message": "Merge pull request #2635 from f1shl3gs/sources/syslog/unify_listen\n\nsources/syslog: unify listen",
-          "timestamp": "2025-10-23T05:36:46+08:00",
-          "tree_id": "991ab10df134aea14afffe4f98c2ff09bc6a435f",
-          "url": "https://github.com/f1shl3gs/vertex/commit/f937f8e20ab11968dc34cf6b299f9caf0abb92c8"
-        },
-        "date": 1761169143328,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 505520,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 527080,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f701b9d908b47aad4fbc402fc1e2278e4dcb4477",
+          "message": "Merge pull request #2825 from f1shl3gs/sources/node/lnstat\n\nsources/node/: add lnstat collector",
+          "timestamp": "2026-03-05T01:50:41+08:00",
+          "tree_id": "fce2c7f7ec7bc8009e2c8b74fe6ccd542e656f76",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f701b9d908b47aad4fbc402fc1e2278e4dcb4477"
+        },
+        "date": 1772646774466,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 524240,
             "unit": "ns/op"
           }
         ]
