99195791ee737618d070854165e45816ad47a114
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 02:31:51 +0000

    add metrics (customSmallerIsBetter) benchmark result for 03aa6663c132910ce6559a597d1308eed034f65e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779330686809,
+  "lastUpdate": 1779330711009,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "23936397bfb91ac555a044269df2a892e24dbf05",
-          "message": "Merge pull request #2810 from f1shl3gs/lib/framework/use_cow_for_endpoint_typ_field\n\nlib/framework: use Cow<'static, str> for Endpoint's typ field",
-          "timestamp": "2026-03-01T11:21:53+08:00",
-          "tree_id": "dae377a0a4255d535377066fa92f35fe97c27ab0",
-          "url": "https://github.com/f1shl3gs/vertex/commit/23936397bfb91ac555a044269df2a892e24dbf05"
-        },
-        "date": 1772335450228,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 16.059,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 101.53,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 173.87,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 161.41,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03aa6663c132910ce6559a597d1308eed034f65e",
+          "message": "Merge pull request #2923 from f1shl3gs/dependabot/cargo/mimalloc-0.1.51\n\nchore(deps): bump mimalloc from 0.1.50 to 0.1.51",
+          "timestamp": "2026-05-21T10:29:09+08:00",
+          "tree_id": "34946b0dcffa0316b9d699ab61188a75e206c03d",
+          "url": "https://github.com/f1shl3gs/vertex/commit/03aa6663c132910ce6559a597d1308eed034f65e"
+        },
+        "date": 1779330687945,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 16.98,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 90.398,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 170.71,
             "unit": "ns/op"
           }
         ]
