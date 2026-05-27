a2dd063223e2e7f6df87ade50395682f5b649ebd
github-action-benchmark <github@users.noreply.github.com>
2026-05-27 14:26:59 +0000

    add Build (customSmallerIsBetter) benchmark result for 1306c6c38e6f3977dc211a18a93c00af8ee996e6

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779891973435,
+  "lastUpdate": 1779892019349,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -13222,40 +13222,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "eeddbd4783ae657d44934ab7da7dcb64da94b2fb",
-          "message": "Merge pull request #2826 from f1shl3gs/sources/node/cgroups\n\nsources/node: add cgroups collector",
-          "timestamp": "2026-03-05T02:11:36+08:00",
-          "tree_id": "8bfc60493612b223cec24a348ba41890e7ec7599",
-          "url": "https://github.com/f1shl3gs/vertex/commit/eeddbd4783ae657d44934ab7da7dcb64da94b2fb"
-        },
-        "date": 1772648578133,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Baseline",
-            "value": 581,
-            "unit": "s"
-          },
-          {
-            "name": "Binary size",
-            "value": 40526,
-            "unit": "KiB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "2dde8fed3656350d291f3e7df5f9097752ec8614",
           "message": "Merge pull request #2827 from f1shl3gs/dependabot/github_actions/docker/login-action-4\n\nchore(ci): bump docker/login-action from 3 to 4",
           "timestamp": "2026-03-05T11:09:40+08:00",
@@ -16600,6 +16566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Baseline",
             "value": 567,
+            "unit": "s"
+          },
+          {
+            "name": "Binary size",
+            "value": 40173.8,
+            "unit": "KiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1306c6c38e6f3977dc211a18a93c00af8ee996e6",
+          "message": "Merge pull request #2931 from f1shl3gs/deps/bump_jemalloc_to_0.7.0\n\ndeps: bump tikv-jemalloc to 0.7.0",
+          "timestamp": "2026-05-27T22:15:07+08:00",
+          "tree_id": "41871f15b088c5e14ae9e47733460a2acf4a7c8c",
+          "url": "https://github.com/f1shl3gs/vertex/commit/1306c6c38e6f3977dc211a18a93c00af8ee996e6"
+        },
+        "date": 1779891995657,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Baseline",
+            "value": 602,
             "unit": "s"
           },
           {
