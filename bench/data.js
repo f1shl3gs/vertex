bab38fe2304ec03e59a7f288688abba9cc6c80e5
github-action-benchmark <github@users.noreply.github.com>
2026-06-18 13:24:20 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 06a240144e4201b0d797dbb615b0a165e084d88e

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781789049595,
+  "lastUpdate": 1781789060718,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "1c6e150bcca39b6887ad25984e8a5d6d6918b96e",
-          "message": "Merge pull request #2837 from f1shl3gs/toolchain/1.94.0\n\ntoolchain: bump to 1.94.0",
-          "timestamp": "2026-03-06T13:58:11+08:00",
-          "tree_id": "73388af9582fcfa4ac128fd3e3c683bac5a7bbfe",
-          "url": "https://github.com/f1shl3gs/vertex/commit/1c6e150bcca39b6887ad25984e8a5d6d6918b96e"
-        },
-        "date": 1772776822804,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 527480,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 540800,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06a240144e4201b0d797dbb615b0a165e084d88e",
+          "message": "Merge pull request #2947 from f1shl3gs/deps/bump_bytes_to_1.12.0\n\ndeps: bump bytes to 1.12.0",
+          "timestamp": "2026-06-18T21:21:43+08:00",
+          "tree_id": "e9e6fa48e5f045ec99eeaffcfb6f21897b8cba08",
+          "url": "https://github.com/f1shl3gs/vertex/commit/06a240144e4201b0d797dbb615b0a165e084d88e"
+        },
+        "date": 1781789036019,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 541640,
             "unit": "ns/op"
           }
         ]
