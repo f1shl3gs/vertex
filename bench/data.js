832a128ce6bf93782d954b55ac19e3290c6d5500
github-action-benchmark <github@users.noreply.github.com>
2026-07-05 15:30:49 +0000

    add vertex (customSmallerIsBetter) benchmark result for 13eb8b53a026449fa5da4d6953a62b3221fd77ae

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783265121037,
+  "lastUpdate": 1783265449378,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -16624,45 +16624,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "28820eedf1e6767d2539e1e5fdd8673d3914c661",
-          "message": "Merge pull request #2850 from f1shl3gs/sinks/prometheus/wrap_const_labels_with_arc\n\nsinks/prometheus: wrap const_labels with Arc, which reduce some clones(allocation)",
-          "timestamp": "2026-03-13T02:01:12+08:00",
-          "tree_id": "d3ca7a8b8716bec79de29051bdd04f369494d9a4",
-          "url": "https://github.com/f1shl3gs/vertex/commit/28820eedf1e6767d2539e1e5fdd8673d3914c661"
-        },
-        "date": 1773339096469,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 7407600,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 679800,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 51.99,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "90831cda7b1a75d1c229852d30c3316fbb57c172",
           "message": "Merge pull request #2852 from f1shl3gs/sinks/prometheus/reimplement_resp_writer\n\nsinks/prometheus_exporter: re-implement RespWriter to reduce allocations and CPU usage",
           "timestamp": "2026-03-13T06:15:22+08:00",
@@ -20507,6 +20468,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 52.038,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "13eb8b53a026449fa5da4d6953a62b3221fd77ae",
+          "message": "Merge pull request #2960 from f1shl3gs/cargo/update\n\nmisc: cargo update",
+          "timestamp": "2026-07-05T23:21:30+08:00",
+          "tree_id": "ed0cfb4232ff822f78abae1ae1ee64be3174c654",
+          "url": "https://github.com/f1shl3gs/vertex/commit/13eb8b53a026449fa5da4d6953a62b3221fd77ae"
+        },
+        "date": 1783265429169,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 6090800,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 642530,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 42.662,
             "unit": "ns/op"
           }
         ]
