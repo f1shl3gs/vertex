5b30c545acc18198486afe8ff90aabcf4f6fb104
github-action-benchmark <github@users.noreply.github.com>
2026-07-21 13:41:06 +0000

    add vertex (customSmallerIsBetter) benchmark result for ee97a5d92a7b6799ce7cccd049ab0ae9c721c104

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784641251508,
+  "lastUpdate": 1784641266949,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -16624,45 +16624,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "d184c05a84b0a00524f06364e4c493dd8fe63a88",
-          "message": "Merge pull request #2872 from f1shl3gs/sources/node/refactor_and_reduce_reallocs\n\nsources/node: refactor and reduce some allocations",
-          "timestamp": "2026-03-29T03:32:54+08:00",
-          "tree_id": "1dcb2968ad5975ea16b73d0ec1faace679aa4f29",
-          "url": "https://github.com/f1shl3gs/vertex/commit/d184c05a84b0a00524f06364e4c493dd8fe63a88"
-        },
-        "date": 1774726987183,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "haproxy/parse_csv",
-            "value": 10346000,
-            "unit": "ns/op"
-          },
-          {
-            "name": "hwmon_gather",
-            "value": 506850,
-            "unit": "ns/op"
-          },
-          {
-            "name": "loki/valid_label_name",
-            "value": 59.125,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "b74445bd722dd8932bac960cfcc56138d0e789f1",
           "message": "Merge pull request #2873 from f1shl3gs/dependabot/cargo/inventory-0.3.23\n\nchore(deps): bump inventory from 0.3.22 to 0.3.23",
           "timestamp": "2026-03-29T03:52:41+08:00",
@@ -20507,6 +20468,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "loki/valid_label_name",
             "value": 52.586,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee97a5d92a7b6799ce7cccd049ab0ae9c721c104",
+          "message": "Merge pull request #2994 from f1shl3gs/dependabot/cargo/tokio-1.53.1\n\nchore(deps): bump tokio from 1.53.0 to 1.53.1",
+          "timestamp": "2026-07-21T21:29:48+08:00",
+          "tree_id": "24b1fca8f1485fbb72e1426e972d459a95a0b6f1",
+          "url": "https://github.com/f1shl3gs/vertex/commit/ee97a5d92a7b6799ce7cccd049ab0ae9c721c104"
+        },
+        "date": 1784641240663,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "haproxy/parse_csv",
+            "value": 8125600,
+            "unit": "ns/op"
+          },
+          {
+            "name": "hwmon_gather",
+            "value": 861100,
+            "unit": "ns/op"
+          },
+          {
+            "name": "loki/valid_label_name",
+            "value": 52.21,
             "unit": "ns/op"
           }
         ]
