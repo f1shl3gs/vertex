abb8bd8aa1e470671284bd1da9b1f8d579763e81
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 13:22:41 +0000

    add metrics (customSmallerIsBetter) benchmark result for c5752797c944c181c6dedf4e5fe20ed4169146dd

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785331343989,
+  "lastUpdate": 1785331361300,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "40899a0aa1494b7be6be80a7032c570968467e43",
-          "message": "Merge pull request #2881 from f1shl3gs/dependabot/cargo/indexmap-2.13.1\n\nchore(deps): bump indexmap from 2.13.0 to 2.13.1",
-          "timestamp": "2026-04-03T23:32:49+08:00",
-          "tree_id": "70d2e027bf97dac99845d0645a6a1833a91ea325",
-          "url": "https://github.com/f1shl3gs/vertex/commit/40899a0aa1494b7be6be80a7032c570968467e43"
-        },
-        "date": 1775230511002,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 16.264,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 103.43,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 179.4,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 127.09,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c5752797c944c181c6dedf4e5fe20ed4169146dd",
+          "message": "Merge pull request #3005 from f1shl3gs/dependabot/cargo/pem-4.0.0\n\nchore(deps): bump pem from 3.0.6 to 4.0.0",
+          "timestamp": "2026-07-29T21:19:52+08:00",
+          "tree_id": "99cfb53d418d8141b19930c866244f35c8fa037b",
+          "url": "https://github.com/f1shl3gs/vertex/commit/c5752797c944c181c6dedf4e5fe20ed4169146dd"
+        },
+        "date": 1785331335001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 15.603,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 84.744,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 162.98,
             "unit": "ns/op"
           }
         ]
