09c0741e84f3466d503c50a788c48db0ccda003f
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 20:18:14 +0000

    add event (customSmallerIsBetter) benchmark result for f6dbfde100a46366a5604e40a6f138a493ee515a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785356276996,
+  "lastUpdate": 1785356294925,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -20526,50 +20526,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "ff6cd7870a1fbb880143b8dff7658f40892b5ee3",
-          "message": "Merge pull request #2883 from f1shl3gs/sources/node/fix_kernel_hung\n\nsources/node: fix kernel_hung",
-          "timestamp": "2026-04-06T14:01:15+08:00",
-          "tree_id": "d9ad242e7dc7bc6c2a2a0b235497d11f04ce8067",
-          "url": "https://github.com/f1shl3gs/vertex/commit/ff6cd7870a1fbb880143b8dff7658f40892b5ee3"
-        },
-        "date": 1775455428517,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "tags/insert/1",
-            "value": 22.859,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/5",
-            "value": 177.72,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/10",
-            "value": 329.37,
-            "unit": "ns/op"
-          },
-          {
-            "name": "tags/insert/20",
-            "value": 694.93,
-            "unit": "ns/op"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "5585ea376b35202c7f37e27ff0ebae6fbff2fed4",
           "message": "Merge pull request #2884 from f1shl3gs/dependabot/cargo/tokio-1.51.1\n\nchore(deps): bump tokio from 1.51.0 to 1.51.1",
           "timestamp": "2026-04-09T09:58:06+08:00",
@@ -24909,6 +24865,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "tags/insert/20",
             "value": 610.77,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6dbfde100a46366a5604e40a6f138a493ee515a",
+          "message": "Merge pull request #3007 from f1shl3gs/dependabot/cargo/rustls-0.23.43\n\nchore(deps): bump rustls from 0.23.42 to 0.23.43",
+          "timestamp": "2026-07-30T04:15:12+08:00",
+          "tree_id": "e6209d8e48831d8252a307b1de662ea73350e363",
+          "url": "https://github.com/f1shl3gs/vertex/commit/f6dbfde100a46366a5604e40a6f138a493ee515a"
+        },
+        "date": 1785356270555,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tags/insert/1",
+            "value": 21.927,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/5",
+            "value": 182.35,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/10",
+            "value": 349.83,
+            "unit": "ns/op"
+          },
+          {
+            "name": "tags/insert/20",
+            "value": 725.04,
             "unit": "ns/op"
           }
         ]
