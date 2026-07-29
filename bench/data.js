6e9a2a4e8a133d7ee98f8e5dfaff0bafcbce504f
github-action-benchmark <github@users.noreply.github.com>
2026-07-29 20:16:37 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 13863f899211b22791131ba1eeae67c76e535766

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785331879802,
+  "lastUpdate": 1785356197894,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "83fe81167a6731058bc5f2790dc2530cb3f60617",
-          "message": "Merge pull request #2882 from f1shl3gs/sources/node/default_enable_kernel_hung\n\nsources/node: enable kernel_hung by default",
-          "timestamp": "2026-04-05T20:20:37+08:00",
-          "tree_id": "4fd52bc0479b619a4195dcbc1de34e12d1f4949f",
-          "url": "https://github.com/f1shl3gs/vertex/commit/83fe81167a6731058bc5f2790dc2530cb3f60617"
-        },
-        "date": 1775391753646,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 531710,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 389760,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "13863f899211b22791131ba1eeae67c76e535766",
+          "message": "Merge pull request #3006 from f1shl3gs/dependabot/cargo/http-1.5.0\n\nchore(deps): bump http from 1.4.2 to 1.5.0",
+          "timestamp": "2026-07-30T04:14:06+08:00",
+          "tree_id": "d8145d481a4af05626973d08a1f3ff742616019a",
+          "url": "https://github.com/f1shl3gs/vertex/commit/13863f899211b22791131ba1eeae67c76e535766"
+        },
+        "date": 1785356172832,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 535550,
             "unit": "ns/op"
           }
         ]
