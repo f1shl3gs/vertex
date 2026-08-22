9fab4548d58405aa98a81e62222e8729aae7e5db
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 05:31:21 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 921dcde50b1e17cde0db02cdf8d285cc83bc873a

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786682967623,
+  "lastUpdate": 1787376681469,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "a7e255b23eb833c5ea48b06ea0ae1c40c9e9e460",
-          "message": "Merge pull request #2892 from f1shl3gs/lib/buffer/fix_race_condition_to_prevent_hanging_forever\n\nlib/buffer: fix race condition to prevent receiver hanging forever",
-          "timestamp": "2026-04-14T23:07:51+08:00",
-          "tree_id": "fc612bdc266edf43527861578de6a9ac908730a1",
-          "url": "https://github.com/f1shl3gs/vertex/commit/a7e255b23eb833c5ea48b06ea0ae1c40c9e9e460"
-        },
-        "date": 1776179401952,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 534580,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 527080,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "921dcde50b1e17cde0db02cdf8d285cc83bc873a",
+          "message": "Merge pull request #3019 from f1shl3gs/extensions/heartbeat\n\nextensions: add a simple heartbeat extension",
+          "timestamp": "2026-08-22T13:28:20+08:00",
+          "tree_id": "14e0273567151db7619a40e7afca39e2c5885ae1",
+          "url": "https://github.com/f1shl3gs/vertex/commit/921dcde50b1e17cde0db02cdf8d285cc83bc873a"
+        },
+        "date": 1787376657886,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 529210,
             "unit": "ns/op"
           }
         ]
