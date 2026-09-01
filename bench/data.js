3ee3d3837d4e9ebe0d12aa5d783e609ea19e45b4
github-action-benchmark <github@users.noreply.github.com>
2026-09-01 10:54:11 +0000

    add prometheus (customSmallerIsBetter) benchmark result for 101f0019de8850b917636b57576f5f78902741d1

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788260050391,
+  "lastUpdate": 1788260051872,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "5e4f47f868b6392adcb8863a33bf2e2744d1c70c",
-          "message": "Merge pull request #2900 from f1shl3gs/dependabot/cargo/rustls-webpki-0.103.13\n\nchore(deps): bump rustls-webpki from 0.103.10 to 0.103.13",
-          "timestamp": "2026-04-23T00:02:43+08:00",
-          "tree_id": "3d1f56ced3f3a74c56aea04c9d19279e7cc6066b",
-          "url": "https://github.com/f1shl3gs/vertex/commit/5e4f47f868b6392adcb8863a33bf2e2744d1c70c"
-        },
-        "date": 1776873893344,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 514260,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 522620,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "101f0019de8850b917636b57576f5f78902741d1",
+          "message": "Merge pull request #3026 from f1shl3gs/dependabot/cargo/flate2-1.1.10\n\nchore(deps): bump flate2 from 1.1.9 to 1.1.10",
+          "timestamp": "2026-09-01T18:51:54+08:00",
+          "tree_id": "a2d7d0289bb740c146b3abf492cdf48336c085b0",
+          "url": "https://github.com/f1shl3gs/vertex/commit/101f0019de8850b917636b57576f5f78902741d1"
+        },
+        "date": 1788260029063,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 543040,
             "unit": "ns/op"
           }
         ]
