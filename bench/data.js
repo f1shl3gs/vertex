1fb8e11634bca69e7c1cd785f0c3fca53e29d19d
github-action-benchmark <github@users.noreply.github.com>
2026-09-03 12:03:26 +0000

    add prometheus (customSmallerIsBetter) benchmark result for abcc5e6223d1cf34fe9d7c5285374ae55279b4d2

--- bench/data.js
+++ bench/data.js
@@ -1,37 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788325342883,
+  "lastUpdate": 1788437006015,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "820d7cb0d2a970292c796b2215ba0e89db3a3422",
-          "message": "Merge pull request #2902 from f1shl3gs/dependabot/cargo/rustls-0.23.39\n\nchore(deps): bump rustls from 0.23.38 to 0.23.39",
-          "timestamp": "2026-04-23T18:14:30+08:00",
-          "tree_id": "1195564bc2ebffcaebe973b73ad3927ea495ad88",
-          "url": "https://github.com/f1shl3gs/vertex/commit/820d7cb0d2a970292c796b2215ba0e89db3a3422"
-        },
-        "date": 1776939381621,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "prometheus/parse_text",
-            "value": 525220,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2899,6 +2870,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "prometheus/parse_text",
             "value": 500370,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "abcc5e6223d1cf34fe9d7c5285374ae55279b4d2",
+          "message": "Merge pull request #3028 from f1shl3gs/toolchain/1.98.0\n\nbump to toolchain 1.98.0",
+          "timestamp": "2026-09-03T20:00:45+08:00",
+          "tree_id": "0d1af23607ca1a7112ebfdbeb16808dc2ef153c1",
+          "url": "https://github.com/f1shl3gs/vertex/commit/abcc5e6223d1cf34fe9d7c5285374ae55279b4d2"
+        },
+        "date": 1788436982773,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prometheus/parse_text",
+            "value": 525030,
             "unit": "ns/op"
           }
         ]
