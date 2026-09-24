bc7ad6ba66d1ca3a0fc94fddbbb1678f206b6212
github-action-benchmark <github@users.noreply.github.com>
2026-09-24 03:57:49 +0000

    add metrics (customSmallerIsBetter) benchmark result for fa6924528e6e6b10c2e4ca4ab9a4954613a9f29f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790222266894,
+  "lastUpdate": 1790222269809,
   "repoUrl": "https://github.com/f1shl3gs/vertex",
   "entries": {
     "prometheus": [
@@ -2905,45 +2905,6 @@ window.BENCHMARK_DATA = {
       }
     ],
     "metrics": [
-      {
-        "commit": {
-          "author": {
-            "email": "26081140+f1shl3gs@users.noreply.github.com",
-            "name": "f1shl3gs",
-            "username": "f1shl3gs"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "7970eea52d262d1948d6cdbf24a70fdef8af1b97",
-          "message": "Merge pull request #2914 from f1shl3gs/dependabot/cargo/tonic-e49de9fdaa\n\nchore(deps): bump the tonic group with 4 updates",
-          "timestamp": "2026-05-08T00:39:44+08:00",
-          "tree_id": "bcfcd8b72790b3fda27e431615ebb61594b2d399",
-          "url": "https://github.com/f1shl3gs/vertex/commit/7970eea52d262d1948d6cdbf24a70fdef8af1b97"
-        },
-        "date": 1778172138122,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "metrics/without_labels",
-            "value": 16.994,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_2_labels",
-            "value": 89.74,
-            "unit": "ns/op"
-          },
-          {
-            "name": "metrics/with_4_labels",
-            "value": 170.79,
-            "unit": "ns/op"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -6801,6 +6762,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "metrics/with_4_labels",
             "value": 154.21,
+            "unit": "ns/op"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26081140+f1shl3gs@users.noreply.github.com",
+            "name": "f1shl3gs",
+            "username": "f1shl3gs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa6924528e6e6b10c2e4ca4ab9a4954613a9f29f",
+          "message": "Merge pull request #3041 from f1shl3gs/dependabot/cargo/hyper-rustls-0.27.10\n\nchore(deps): bump hyper-rustls from 0.27.9 to 0.27.10",
+          "timestamp": "2026-09-24T11:55:17+08:00",
+          "tree_id": "9a65f6dd29b48d530a6708bb6919cec0e7b3a887",
+          "url": "https://github.com/f1shl3gs/vertex/commit/fa6924528e6e6b10c2e4ca4ab9a4954613a9f29f"
+        },
+        "date": 1790222246049,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "metrics/without_labels",
+            "value": 15.279,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_2_labels",
+            "value": 87.088,
+            "unit": "ns/op"
+          },
+          {
+            "name": "metrics/with_4_labels",
+            "value": 165.54,
             "unit": "ns/op"
           }
         ]
